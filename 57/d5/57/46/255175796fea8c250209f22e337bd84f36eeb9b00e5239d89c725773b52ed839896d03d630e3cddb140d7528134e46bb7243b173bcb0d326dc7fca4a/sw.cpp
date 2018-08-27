b2f6281d35e9a44088ea9d76411004a8341b13b7
CppanBot <cppanbot@gmail.com>
2018-08-27 20:50:35 +0300

    Update.

--- 57/d5/57/46/255175796fea8c250209f22e337bd84f36eeb9b00e5239d89c725773b52ed839896d03d630e3cddb140d7528134e46bb7243b173bcb0d326dc7fca4a/sw.cpp
+++ 57/d5/57/46/255175796fea8c250209f22e337bd84f36eeb9b00e5239d89c725773b52ed839896d03d630e3cddb140d7528134e46bb7243b173bcb0d326dc7fca4a/sw.cpp
@@ -8,6 +8,10 @@ void build(Solution &s)
         yaml_cpp.Private += sw::Shared, "yaml_cpp_EXPORTS"_d;
         yaml_cpp.Public += sw::Shared, "YAML_CPP_DLL"_d;
     }
+    else
+        yaml_cpp.replaceInFileOnce("include/yaml-cpp/dll.h",
+            "#define YAML_CPP_API",
+            "#define  YAML_CPP_API __attribute__((visibility (\"default\")))");
 
     yaml_cpp.Public += "org.sw.demo.boost.iterator-*"_dep;
     yaml_cpp.Public += "org.sw.demo.boost.smart_ptr-*"_dep;
