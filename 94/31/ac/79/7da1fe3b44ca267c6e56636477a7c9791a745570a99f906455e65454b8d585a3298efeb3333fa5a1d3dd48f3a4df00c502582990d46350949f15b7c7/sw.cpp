d92c4f133aa24a01f1fc8b5ec9f960af9cb2ed2b
CppanBot <cppanbot@gmail.com>
2018-08-29 13:08:35 +0300

    Update.

---
+++ 94/31/ac/79/7da1fe3b44ca267c6e56636477a7c9791a745570a99f906455e65454b8d585a3298efeb3333fa5a1d3dd48f3a4df00c502582990d46350949f15b7c7/sw.cpp
@@ -0,0 +1,119 @@
+void build(Solution &s)
+{
+    auto &tcl = s.addProject("tcl", "8.6.8");
+    tcl += RemoteFile("https://prdownloads.sourceforge.net/tcl/tcl{v}-src.tar.gz");
+
+    auto &stub = tcl.addTarget<StaticLibraryTarget>("stub");
+    auto &lib = tcl.addTarget<LibraryTarget>("lib");
+    auto &sh = tcl.addTarget<ExecutableTarget>("sh");
+
+    //
+    stub +=
+        "generic/tclOOStubLib.c",
+        "generic/tclStubLib.c",
+        "generic/tclTomMathStubLib.c";
+    stub.Private += sw::Static, "STATIC_BUILD"_d;
+    stub.Private += sw::Shared, "TCL_SHARED_BUILD"_d;
+    auto d = stub.Public + sh;
+    d->IncludeDirectoriesOnly = true;
+    d = stub.Public + lib;
+    d->IncludeDirectoriesOnly = true;
+
+    //
+    lib +=
+        "compat/[^/]*\\.[h]"_rr,
+        "generic/[^/]*\\.[hc]"_rr,
+        "generic/[^/]*\\.decls"_rr,
+        "libtommath/[^/]*\\.[hc]"_rr,
+        "unix/[^/]*\\.[hc]"_rr,
+        "win/[^/]*\\.[hc]"_rr;
+    lib -=
+        ".*manifest$"_rr,
+        ".*rc$"_rr,
+        "generic/.*None.c"_rr,
+        "generic/.*StubLib.c"_rr,
+        "generic/regc_.*"_rr,
+        "generic/rege_.*"_rr,
+        "generic/regfronts.c",
+        "unix/tclAppInit.c",
+        "unix/tclLoadAix.c",
+        "unix/tclLoadDyld.c",
+        "unix/tclLoadNext.c",
+        "unix/tclLoadOSF.c",
+        "unix/tclLoadShl.c",
+        "unix/tclXtNotify.c",
+        "unix/tclXtTest.c",
+        "win/nmakehlp.c",
+        "win/tclAppInit.c",
+        "win/tclWinDde.c",
+        "win/tclWinReg.c";
+    lib.Public +=
+        "libtommath"_id,
+        "generic"_id;
+    lib.Private += "BUILD_tcl"_d;
+    lib.Private += "CFG_INSTALL_BINDIR=\"\""_d;
+    lib.Private += "CFG_INSTALL_DOCDIR=\"\""_d;
+    lib.Private += "CFG_INSTALL_INCDIR=\"\""_d;
+    lib.Private += "CFG_INSTALL_LIBDIR=\"\""_d;
+    lib.Private += "CFG_INSTALL_SCRDIR=\"\""_d;
+    lib.Private += "CFG_RUNTIME_BINDIR=\"\""_d;
+    lib.Private += "CFG_RUNTIME_DOCDIR=\"\""_d;
+    lib.Private += "CFG_RUNTIME_INCDIR=\"\""_d;
+    lib.Private += "CFG_RUNTIME_LIBDIR=\"\""_d;
+    lib.Private += "CFG_RUNTIME_SCRDIR=\"\""_d;
+    lib.Private += "HAVE_ZLIB"_d;
+    lib.Private += "TCL_CFGVAL_ENCODING=\"\""_d;
+    lib.Private += "TCL_TOMMATH"_d;
+    if (s.Settings.TargetOS.Type != OSType::Windows)
+    {
+        lib.Public += "TCL_LIBRARY=\"\""_d;
+        lib.Public += "TCL_PACKAGE_PATH=\"\""_d;
+    }
+    if (s.Settings.TargetOS.Type == OSType::Windows)
+    {
+        lib.Public += "UNICODE"_d;
+        lib.Public += "_UNICODE"_d;
+    }
+    if (s.Settings.TargetOS.Type == OSType::Windows)
+    {
+        lib.Private += "Netapi32.lib"_lib, "user32.lib"_lib;
+    }
+    lib.Private += sw::Shared, "TCL_SHARED_BUILD"_d;
+    lib.Public += sw::Static, "STATIC_BUILD"_d;
+
+    d = lib.Public + sh;
+    d->IncludeDirectoriesOnly = true;
+    lib.Public += stub;
+    lib.Public += "org.sw.demo.madler.zlib-1"_dep;
+
+    if (s.Settings.TargetOS.is(OSType::Windows))
+    {
+        auto m2 = lib.BinaryDir / "tclMain1.c";
+        lib.configureFile("generic/tclMain.c", m2);
+        lib += m2;
+        lib[m2].args.push_back("-DTCL_ASCII_MAIN=1");
+        lib -= "unix/.*"_rr;
+        lib -= "compat/.*"_rr;
+    }
+    else
+        lib -= "win/.*"_rr;
+
+    //
+    sh +=
+        "library/.*"_rr,
+        "unix/.*\\.h"_rr,
+        "unix/tclAppInit.c",
+        "win/.*\\.h"_rr,
+        "win/tclAppInit.c";
+    sh -=
+        ".*manifest$"_rr,
+        ".*rc$"_rr;
+    sh.Public +=
+        "unix"_id,
+        "win"_id;
+    if (s.Settings.TargetOS.is(OSType::Windows))
+        sh -= "unix/.*"_rr;
+    else
+        sh -= "win/.*"_rr;
+    sh.Public += lib;
+}
