ad6d63beb25776a4602eddcda3330b70ac543644
CppanBot <cppanbot@gmail.com>
2018-10-11 18:12:34 +0300

    Update.

--- e1/6a/9d/41/54e3741be4ec4bcb083f27b61e993fa59abe89a2f597d39ef9eb6c5fc41d77a106d3b6840eaffb5e681e334c746f0359cebf486f4d4148e8c7788a0a/sw.cpp
+++ e1/6a/9d/41/54e3741be4ec4bcb083f27b61e993fa59abe89a2f597d39ef9eb6c5fc41d77a106d3b6840eaffb5e681e334c746f0359cebf486f4d4148e8c7788a0a/sw.cpp
@@ -3,16 +3,37 @@ void build(Solution &s)
     auto &ns = s.addStaticLibrary("google.nsync", "1.20.1");
     ns += Git("https://github.com/google/nsync", "{v}");
 
+    auto &apply_files = [](auto &t)
+    {
+        t -=
+            ".*test.*.cc"_rr,
+            ".*/test_.*.cc"_rr
+            ;
+    };
+
     ns += "internal/.*"_rr;
     ns += "public/.*"_rr;
 
-    ns += "internal"_idir;
-    ns.Public += "public"_idir;
-
     ns -= "platform/.*"_rr;
+    ns += "platform/c\\+\\+11/.*"_rr;
+
+    /*ns +=
+        "platform/posix/src/nsync_panic.c",
+        "platform/posix/src/per_thread_waiter.c",
+        "platform/posix/src/time_rep.c",
+        "platform/posix/src/yield.c";
+    ns +=
+        "platform/c++11/src/per_thread_waiter.cc",
+        "platform/c++11/src/yield.cc",
+        "platform/c++11/src/time_rep_timespec.cc",
+        "platform/c++11/src/nsync_panic.cc";*/
+
     if (s.Settings.TargetOS.Type == OSType::Windows)
     {
         ns += "platform/win32/.*"_rr;
+        ns += "platform/c++11/src/nsync_semaphore_mutex.cc";
+        ns -= "platform/win32/src/pthread_cond_timedwait_win32.c";
+        ns += "platform/c++11"_idir;
         ns += "platform/win32"_idir;
     }
     if (s.Settings.Native.CompilerType == CompilerType::MSVC)
@@ -30,7 +51,16 @@ void build(Solution &s)
         ns += "platform/x86_64/.*"_rr;
         ns += "platform/x86_64"_idir;
     }
-    
+    apply_files(ns);
+
+    ns += "platform/posix"_idir;
+    ns.Public += "public"_idir;
+    ns += "internal"_idir;
+
+    ns +=
+        "NSYNC_USE_CPP11_TIMEPOINT"_def,
+        "NSYNC_ATOMIC_CPP11"_def;
+
     for (auto *f : ns.gatherSourceFiles())
         f->BuildAs = NativeSourceFile::CPP;
 }
