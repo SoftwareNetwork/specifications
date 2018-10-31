b54cf3777d4799b391e4c8a2426d68260485a3d3
CppanBot <cppanbot@gmail.com>
2018-10-31 15:28:41 +0300

    Update.

---
+++ 64/8d/b7/8c/2632a6e0caf45d614094eefc6f03846236a873db94de591572ae83fc7a99d73ce12ff9b3ea938256722ad7065dfdce80668a8f2d1c53d3d698594e8c/sw.cpp
@@ -0,0 +1,83 @@
+void build(Solution &s)
+{
+    auto &nasm = s.addTarget<ExecutableTarget>("nasm", "2.13.3");
+    nasm += RemoteFile("https://www.nasm.us/pub/nasm/releasebuilds/{M}.{m}.0{p}/nasm-{M}.{m}.0{p}.tar.xz");
+
+    nasm.setChecks("nasm");
+
+    nasm +=
+        ".*\\.h"_rr,
+        "asm/.*\\.c"_rr,
+        "output/.*\\.c"_rr,
+        "nasmlib/.*\\.c"_rr,
+        "common/.*\\.c"_rr,
+        "macros/.*\\.c"_rr,
+        "stdlib/.*\\.c"_rr,
+        "x86/.*\\.c"_rr
+        ;
+    nasm +=
+        "."_idir,
+        "include"_idir,
+        "x86"_idir,
+        "asm"_idir,
+        "output"_idir
+        ;
+
+    nasm.Public +=
+        "include"_id;
+
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("nasm");
+    s.checkFunctionExists("canonicalize_file_name");
+    s.checkFunctionExists("getgid");
+    s.checkFunctionExists("getuid");
+    s.checkFunctionExists("pathconf");
+    s.checkFunctionExists("realpath");
+    s.checkFunctionExists("strcasecmp");
+    s.checkFunctionExists("strcspn");
+    s.checkFunctionExists("stricmp");
+    s.checkFunctionExists("strlcpy");
+    s.checkFunctionExists("strncasecmp");
+    s.checkFunctionExists("strnicmp");
+    s.checkFunctionExists("strsep");
+    s.checkFunctionExists("strspn");
+    s.checkFunctionExists("vsnprintf");
+    s.checkFunctionExists("_fullpath");
+    s.checkFunctionExists("_snprintf");
+    s.checkFunctionExists("_vsnprintf");
+    s.checkIncludeExists("inttypes.h");
+    s.checkIncludeExists("limits.h");
+    s.checkIncludeExists("memory.h");
+    s.checkIncludeExists("stdbool.h");
+    s.checkIncludeExists("stddef.h");
+    s.checkIncludeExists("stdint.h");
+    s.checkIncludeExists("stdlib.h");
+    s.checkIncludeExists("strings.h");
+    s.checkIncludeExists("string.h");
+    s.checkIncludeExists("sys/param.h");
+    s.checkIncludeExists("sys/stat.h");
+    s.checkIncludeExists("sys/types.h");
+    s.checkIncludeExists("unistd.h");
+    s.checkTypeSize("size_t");
+    s.checkTypeSize("void *");
+    {
+        auto &c = s.checkSymbolExists("snprintf");
+        c.Parameters.Includes.push_back("stdio.h");
+    }
+    s.checkSourceCompiles("STDC_HEADERS", R"sw_xxx(
+    #include <stdlib.h>
+    #include <stdarg.h>
+    #include <string.h>
+    #include <float.h>
+    int main() {return 0;}
+    )sw_xxx");
+    s.checkDeclarationExists("decl");
+    s.checkDeclarationExists("decl");
+    s.checkDeclarationExists("decl");
+    s.checkDeclarationExists("decl");
+    s.checkDeclarationExists("decl");
+    s.checkDeclarationExists("decl");
+}
