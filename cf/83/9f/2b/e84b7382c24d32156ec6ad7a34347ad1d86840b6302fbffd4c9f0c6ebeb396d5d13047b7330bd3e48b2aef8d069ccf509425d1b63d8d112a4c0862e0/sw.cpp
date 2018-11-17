42fff09b173a76916646b30b8914611a643bd327
CppanBot <cppanbot@gmail.com>
2018-11-17 23:25:34 +0000

    Update.

---
+++ cf/83/9f/2b/e84b7382c24d32156ec6ad7a34347ad1d86840b6302fbffd4c9f0c6ebeb396d5d13047b7330bd3e48b2aef8d069ccf509425d1b63d8d112a4c0862e0/sw.cpp
@@ -0,0 +1,34 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("amazon.awslabs.c_common", "master");
+    t += Git("https://github.com/awslabs/aws-c-common", "", "{v}");
+
+    t +=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t -=
+        "include/.*\\.c"_rr,
+        "source/windows/.*"_rr,
+        "source/posix/.*"_rr;
+
+    if (s.Settings.Native.CompilerType == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+        //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_COMMON_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_COMMON_USE_IMPORT_EXPORT"_d;
+    if (s.Settings.TargetOS.Type == OSType::Windows)
+    {
+        t += "source/windows/.*"_rr;
+        t.Public += "USE_WINDOWS_DLL_SEMANTICS"_d;
+        t.Public += "WIN32"_d;
+        t += "HAVE_MSVC_CPUIDEX"_d;
+    }
+    else
+    {
+        t += "source/posix/.*"_rr;
+        t += "HAVE_BUILTIN_CPU_SUPPORTS"_d;
+    }
+}
+
