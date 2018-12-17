7ee6051b611d3ddcbdcce7d5971854d4aa3573c7
CppanBot <cppanbot@gmail.com>
2018-12-17 09:33:51 +0000

    Update.

---
+++ 22/af/50/d4/9eef3c24b9cf52b7645c4bf8e460d23ca59eff411f42be38cc27abc6ac70371d2caaef1036a2bcd96fece7967e86d8ed7fbeb3ad55d07414796bb8f0/sw.cpp
@@ -0,0 +1,32 @@
+#pragma sw require header pub.egorpugin.primitives.tools.syncqt-master
+#pragma sw require header org.sw.demo.qtproject.qt.base.widgets-5
+
+void build(Solution &s)
+{
+    auto &qt = s.addProject("qtproject.qt", "5.11.3");
+
+    auto &svg = qt.addLibrary("svg");
+    svg += Git("git://code.qt.io/qt/qtsvg.git", "v{v}");
+
+    {
+        String module = "QtSvg";
+        syncqt(svg, { module });
+        auto &t = svg;
+
+        svg += "src/svg/[^/]*"_rr;
+        svg.Private += "QT_BUILD_SVG_LIB"_d;
+        svg.Public += "org.sw.demo.qtproject.qt.base.widgets-5"_dep;
+        if (s.Settings.TargetOS.Type == OSType::Windows)
+        {
+            svg.Private += "UNICODE"_d;
+        }
+        automoc("org.sw.demo.qtproject.qt.base.tools.moc-5"_dep, svg);
+        t.writeFileOnce(module + "/" + module + "Depends", R"xxx(
+            #ifdef __cplusplus
+            #include <QtCore/QtCore>
+            #include <QtGui/QtGui>
+            #include <QtWidgets/QtWidgets>
+            #endif
+)xxx");
+    }
+}
