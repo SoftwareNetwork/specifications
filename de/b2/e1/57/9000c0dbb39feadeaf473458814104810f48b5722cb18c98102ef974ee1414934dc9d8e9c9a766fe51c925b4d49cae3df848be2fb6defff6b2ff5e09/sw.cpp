9f498167b8f5263456c2fa1714d4c4be4b353a11
CppanBot <cppanbot@gmail.com>
2018-12-19 18:14:02 +0000

    Update.

---
+++ de/b2/e1/57/9000c0dbb39feadeaf473458814104810f48b5722cb18c98102ef974ee1414934dc9d8e9c9a766fe51c925b4d49cae3df848be2fb6defff6b2ff5e09/sw.cpp
@@ -0,0 +1,33 @@
+#pragma sw require header pub.egorpugin.primitives.tools.syncqt-master
+#pragma sw require header org.sw.demo.qtproject.qt.base.widgets-5
+
+void build(Solution &s)
+{
+    auto &qt = s.addProject("qtproject.qt", "5.12.0");
+
+    auto &winextras = qt.addLibrary("winextras");
+    winextras += Git("https://github.com/qt/qtwinextras.git", "v{v}");
+
+    {
+        String module = "QtWinExtras";
+        syncqt(winextras, { module });
+        auto &t = winextras;
+
+        winextras.SourceDir /= "src/winextras";
+        winextras += "[^/]*"_rr;
+        winextras.Private += "QT_BUILD_WINEXTRAS_LIB"_d;
+        winextras.Public += "org.sw.demo.qtproject.qt.base.gui-5"_dep;
+        if (s.Settings.TargetOS.Type == OSType::Windows)
+        {
+            winextras.Private += "UNICODE"_d;
+            winextras += "dwmapi.lib"_lib;
+        }
+        automoc("org.sw.demo.qtproject.qt.base.tools.moc-5"_dep, winextras);
+        t.writeFileOnce(module + "/" + module + "Depends", R"xxx(
+            #ifdef __cplusplus
+            #include <QtCore/QtCore>
+            #include <QtGui/QtGui>
+            #endif
+)xxx");
+    }
+}
