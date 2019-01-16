66b023bc5a14a93557e9a928c92afc1b4c3b4b28
CppanBot <cppanbot@gmail.com>
2019-01-16 20:59:15 +0000

    Update.

---
+++ 55/d5/7d/97/37bba19c0867f80a0675c7ad2c2b61e3b208f9653adf5811491977f61673a0e4c74a085aa923e12eaf02affaa9ffca17ca2cd938141bf7bc633e4b1a/sw.cpp
@@ -0,0 +1,56 @@
+#pragma sw require header org.sw.demo.qtproject.qt.base.tools.moc-*
+#pragma sw require header org.sw.demo.qtproject.qt.tools.linguist.release-*
+
+#pragma sw header on
+
+static Files qt_translations_create_qm_files(const DependencyPtr &base, TargetBase &t)
+{
+    auto ts = std::make_shared<Dependency>(base->package);
+    ts->package.ppath /= "translations";
+
+    auto qt_trs_r = FileRegex(ts->getPackage().resolve().getDirSrc2() / "translations", std::regex(".*\\.ts"));
+    auto &qt_trs = t.addStaticLibrary("qt_translations");
+    qt_trs += qt_trs_r;
+    return qt_tr(base, qt_trs);
+}
+
+static void qt_translations_rcc(const DependencyPtr &base, NativeExecutedTarget &t, path in, const Files &qms)
+{
+    if (!in.is_absolute())
+        in = t.SourceDir / in;
+
+    auto r = std::make_shared<Dependency>(base->package);
+    r->package.ppath /= "base.tools.rcc";
+
+    for (auto &q : qms)
+    {
+        auto o = t.BinaryDir / q.filename();
+        SW_MAKE_EXECUTE_BUILTIN_COMMAND_AND_ADD(copy_cmd, t, "sw_copy_file");
+        copy_cmd->args.push_back(normalize_path(q));
+        copy_cmd->args.push_back(normalize_path(o));
+        copy_cmd->addInput(q);
+        copy_cmd->addOutput(o);
+        copy_cmd->name = "copy: " + normalize_path(o);
+        copy_cmd->maybe_unused = builder::Command::MU_ALWAYS;
+    }
+
+    t.configureFile(in,
+        t.BinaryDir / in.filename(),
+        ConfigureFlags::CopyOnly);
+    rcc(r, t,
+        t.BinaryDir / in.filename())
+        .c->working_directory = t.BinaryDir;
+}
+
+#pragma sw header off
+
+void build(Solution &s)
+{
+    auto &qt = s.addProject("qtproject.qt", "5.12.0");
+
+    auto &translations = qt.addTarget<LibraryTarget>("translations");
+    translations += Git("https://github.com/qt/qttranslations", "v{v}");
+
+    //translations.Empty = true;
+    translations += "translations/.*\\.ts"_rr;
+}
