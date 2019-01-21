c17f335acd5a10db6e024ebc2ebbedef4ff24853
CppanBot <cppanbot@gmail.com>
2019-01-21 17:59:15 +0000

    Update.

--- 57/d5/57/46/255175796fea8c250209f22e337bd84f36eeb9b00e5239d89c725773b52ed839896d03d630e3cddb140d7528134e46bb7243b173bcb0d326dc7fca4a/sw.cpp
+++ 57/d5/57/46/255175796fea8c250209f22e337bd84f36eeb9b00e5239d89c725773b52ed839896d03d630e3cddb140d7528134e46bb7243b173bcb0d326dc7fca4a/sw.cpp
@@ -1,18 +1,11 @@
 void build(Solution &s)
 {
     auto &yaml_cpp = s.addTarget<LibraryTarget>("jbeder.yaml_cpp", "master");
-    yaml_cpp += Git("https://github.com/jbeder/yaml-cpp", "", "master");
+    yaml_cpp += Git("https://github.com/jbeder/yaml-cpp", "", "{v}");
 
+    yaml_cpp.ApiName = "YAML_CPP_API";
     yaml_cpp.CPPVersion = CPPLanguageStandard::CPP11;
-    if (s.Settings.TargetOS.Type == OSType::Windows)
-    {
-        yaml_cpp.Private += sw::Shared, "yaml_cpp_EXPORTS"_d;
-        yaml_cpp.Public += sw::Shared, "YAML_CPP_DLL"_d;
-    }
-    else
-        yaml_cpp.replaceInFileOnce("include/yaml-cpp/dll.h",
-            "#define YAML_CPP_API",
-            "#define  YAML_CPP_API __attribute__((visibility (\"default\")))");
+    yaml_cpp.writeFileOnce("include/yaml-cpp/dll.h");
 
     yaml_cpp.Public += "org.sw.demo.boost.iterator-*"_dep;
     yaml_cpp.Public += "org.sw.demo.boost.smart_ptr-*"_dep;
