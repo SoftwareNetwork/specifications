4c8ab76dc5acfb4f85baa1dc622516f74bf92f23
CppanBot <cppanbot@gmail.com>
2019-01-23 11:59:19 +0000

    Update.

--- db/a5/0f/7b/9e3600e9bbeaea287279be5541fabd570ef1b82a002a4ecb34afc5b25b32a17df1f8f5ad6cb3c25e831380413d190e014973e1caa3cd594f1bb5caf1/sw.cpp
+++ db/a5/0f/7b/9e3600e9bbeaea287279be5541fabd570ef1b82a002a4ecb34afc5b25b32a17df1f8f5ad6cb3c25e831380413d190e014973e1caa3cd594f1bb5caf1/sw.cpp
@@ -1,32 +1,58 @@
-#pragma sw require header pub.egorpugin.primitives.tools.syncqt-master
 #pragma sw require header org.sw.demo.qtproject.qt.base.widgets-5
 
 void build(Solution &s)
 {
-    auto &qt = s.addProject("qtproject.qt", "5.11.3");
+    auto &qt = s.addProject("qtproject.qt", "5.12.0");
 
-    auto &svg = qt.addLibrary("svg");
-    svg += Git("git://code.qt.io/qt/qtsvg.git", "v{v}");
+    auto &plugins = qt.addDirectory("plugins");
+
+    auto &imageformats = plugins.addProject("imageformats");
+    imageformats += Git("git://code.qt.io/qt/qtimageformats.git", "v{v}");
 
     {
-        String module = "QtSvg";
-        syncqt(svg, { module });
-        auto &t = svg;
-
-        svg += "src/svg/[^/]*"_rr;
-        svg.Private += "QT_BUILD_SVG_LIB"_d;
-        svg.Public += "org.sw.demo.qtproject.qt.base.widgets-5"_dep;
-        if (s.Settings.TargetOS.Type == OSType::Windows)
+        auto add_imageformat1 = [&s](auto &t, const String &name)
         {
-            svg.Private += "UNICODE"_d;
-        }
-        automoc("org.sw.demo.qtproject.qt.base.tools.moc-5"_dep, svg);
-        t.writeFileOnce(module + "/" + module + "Depends", R"xxx(
-            #ifdef __cplusplus
-            #include <QtCore/QtCore>
-            #include <QtGui/QtGui>
-            #include <QtWidgets/QtWidgets>
-            #endif
-)xxx");
+            t.setOutputDir("plugins/imageformats");
+            t.SourceDir /= "src/plugins/imageformats";
+            t.SourceDir /= name;
+            t += ".*\\.cpp"_rr, ".*\\.h"_rr, ".*\\.json"_rr;
+            if (s.Settings.TargetOS.Type == OSType::Windows)
+            {
+                t.Private += "UNICODE"_d;
+            }
+            t.Private += sw::Static, "QT_STATICPLUGIN"_d;
+
+            automoc("org.sw.demo.qtproject.qt.base.tools.moc-5"_dep, t);
+
+            t.Public += "org.sw.demo.qtproject.qt.base.gui-5"_dep;
+        };
+
+        auto add_imageformat = [&s, &imageformats, &add_imageformat1](const String &name) -> decltype(auto)
+        {
+            auto &t = imageformats.addLibrary("q" + name);
+            add_imageformat1(t, name);
+            return t;
+        };
+
+        auto add_imageformat_base = [&imageformats, &add_imageformat1](const String &name) -> decltype(auto)
+        {
+            auto &t = imageformats.addLibrary("q" + name);
+            t += Git("git://code.qt.io/qt/qtbase.git", "v{v}");
+            add_imageformat1(t, name);
+            return t;
+        };
+
+        add_imageformat_base("gif");
+        add_imageformat_base("ico");
+        add_imageformat_base("jpeg") += "org.sw.demo.jpeg-*"_dep;
+
+        add_imageformat("dds");
+        add_imageformat("icns");
+        add_imageformat("tga");
+        add_imageformat("tiff") += "org.sw.demo.tiff-*"_dep;
+        add_imageformat("wbmp");
+        add_imageformat("webp") += "org.sw.demo.webmproject.webp-*"_dep;
+        add_imageformat("jp2") += "org.sw.demo.mdadams.jasper-*"_dep;
+        add_imageformat("mng") += "org.sw.demo.mng-*"_dep;
     }
 }
