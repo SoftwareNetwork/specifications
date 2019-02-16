00304aed4cbd8a751d61ff5334bebbad50fa653d
CppanBot <cppanbot@gmail.com>
2019-02-16 12:06:45 +0000

    "Update."

---
+++ b8/50/d6/d1/99e38d15f0f949a9550bb57bb7cb3b49ba6bb544d216802526023d2bb2ac617de3ed8c1c8222868edd37c74d8de3d7e9713a0069f34a692b5380c897/sw.cpp
@@ -0,0 +1,7 @@
+void build(Solution &s)
+{
+	auto &sqlpp11_connector_sqlite3 = s.addTarget<StaticLibraryTarget>("rbock.sqlpp11_connector_sqlite3", "develop");
+	sqlpp11_connector_sqlite3 += Git("https://github.com/rbock/sqlpp11-connector-sqlite3", "", "{v}");
+	sqlpp11_connector_sqlite3.Public += "org.sw.demo.rbock.sqlpp11-master"_dep;
+	sqlpp11_connector_sqlite3.Public += "org.sw.demo.sqlite3-3"_dep;
+}
