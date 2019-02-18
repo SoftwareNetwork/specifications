6ce893835f21e73e72cdcef25ef8a8e501172f34
CppanBot <cppanbot@gmail.com>
2019-02-18 21:51:45 +0000

    "Update."

---
+++ e5/1d/16/cb/3c9c8bdd992b3f4f12ab5ded7af8fca240888d8a0a7f7067ffbd0a527fc37f1a840ddce65c16619a47512b9378706cde0f387ea24eebd927fa370f95/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &sqlpp11 = s.addTarget<LibraryTarget>("rbock.sqlpp11", "0.56.0");
+    sqlpp11 += Git("https://github.com/rbock/sqlpp11", "{M}.{m}");
+    sqlpp11.Public += "org.sw.demo.howardhinnant.date.date-2"_dep;
+    sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "Input", "s");
+    sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "(&s)[N]", "*s");
+}
