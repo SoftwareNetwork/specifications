268e04c849a041eefa8c43965d935928b4471685
CppanBot <cppanbot@gmail.com>
2019-02-25 15:19:09 +0000

    Update.

---
+++ c6/2d/ba/fd/bca661b83ae47d9a40326450608d20c6f4bb4d899669891ba672d2f0d5dc6560cea200639fe96eb852f61055671bbb47b3953ed01f4942f8101ab329/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &eigen = s.addTarget<LibraryTarget>("eigen", "3.3.6");
+    eigen += RemoteFile("https://bitbucket.org/eigen/eigen/get/{v}.tar.gz");
+
+    eigen +=
+        "Eigen/.*"_rr,
+        "unsupported/Eigen/.*"_rr;
+}
