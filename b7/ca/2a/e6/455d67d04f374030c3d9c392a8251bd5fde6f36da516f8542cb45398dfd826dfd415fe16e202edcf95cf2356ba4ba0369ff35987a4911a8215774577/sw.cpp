0ef7fe970f816a35cb446996ee73104240077899
CppanBot <cppanbot@gmail.com>
2019-02-25 16:19:09 +0000

    Update.

---
+++ b7/ca/2a/e6/455d67d04f374030c3d9c392a8251bd5fde6f36da516f8542cb45398dfd826dfd415fe16e202edcf95cf2356ba4ba0369ff35987a4911a8215774577/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &spdlog = s.addTarget<LibraryTarget>("gabime.spdlog", "1.3.1");
+    spdlog += Git("https://github.com/gabime/spdlog", "v{v}");
+
+    spdlog += "include/.*"_rr;
+    spdlog -= "include/spdlog/fmt/.*"_rr;
+    spdlog.Public += "org.sw.demo.fmt-*"_dep;
+}
