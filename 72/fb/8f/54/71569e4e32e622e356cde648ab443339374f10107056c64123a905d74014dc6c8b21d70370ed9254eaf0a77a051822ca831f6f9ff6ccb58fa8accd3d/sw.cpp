697fd8e177b854c181f47e9c7b66bf1cf4cfd688
CppanBot <cppanbot@gmail.com>
2019-02-27 20:34:09 +0000

    Update.

---
+++ 72/fb/8f/54/71569e4e32e622e356cde648ab443339374f10107056c64123a905d74014dc6c8b21d70370ed9254eaf0a77a051822ca831f6f9ff6ccb58fa8accd3d/sw.cpp
@@ -0,0 +1,145 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("intel", "2019.3");
+    p += RemoteFile("https://github.com/01org/tbb/archive/{M}_U{m}.tar.gz");
+
+    String arch;
+
+    auto make_ver = [](auto &t)
+    {
+        t.writeFileOnce("version_string.ver", R"(#define __TBB_VERSION_STRINGS(N) \
+#N": BUILD_HOST         @CMAKE_SYSTEM_NAME@" ENDL \
+#N": BUILD_OS           @CMAKE_SYSTEM@" ENDL \
+#N": BUILD_KERNEL       @CMAKE_SYSTEM_VERSION@" ENDL \
+#N": BUILD_GCC          @CMAKE_CXX_COMPILER_ID@" ENDL \
+#N": BUILD_LIBC         Unknown" ENDL \
+#N": BUILD_LD           Unknown" ENDL \
+#N": BUILD_TARGET       Unknown" ENDL \
+#N": BUILD_COMMAND      Unknown" ENDL
+
+#define __TBB_DATETIME "@_configure_date@"
+)");
+    };
+
+    auto make_def = [&s](auto &t, const String &name, const String &arch)
+    {
+        auto cc = std::static_pointer_cast<VisualStudioCompiler>(s.findProgramByExtension(".cpp")->clone());
+        cc->CompileAsC = true;
+        cc->PreprocessToStdout = true;
+        cc->IncludeDirectories.insert(t.SourceDir / "include");
+        cc->InputFile = t.SourceDir / ("src/" + name + "/" + arch + "-" + name + "-export.def");
+
+        auto c = cc->getCommand(t);
+        c->redirectStdout(t.BinaryDir / ("" + name + ".def"));
+        t.Storage.push_back(c);
+        t += t.BinaryDir / ("" + name + ".def");
+    };
+
+    auto &tbb = p.addTarget<LibraryTarget>("tbb");
+    {
+        tbb +=
+            "include/.*"_rr,
+            "src/old/.*"_rr,
+            "src/old/.*\\.h"_rr,
+            "src/rml/.*\\.h"_rr,
+            "src/rml/client/rml_tbb.cpp",
+            "src/tbb/.*\\.asm"_rr,
+            "src/tbb/.*\\.cpp"_rr,
+            "src/tbb/.*\\.def"_rr,
+            "src/tbb/.*\\.h"_rr,
+            "src/tbb/.*\\.lst"_rr;
+
+        tbb.Private +=
+            "src"_id,
+            "src/rml/include"_id;
+
+        tbb.Public += "include"_id;
+
+        tbb -= "src/tbb/.*\\.asm"_rr;
+        tbb -= "src/tbb/.*\\.def"_rr;
+        tbb -= "src/.*test.*"_rr;
+
+        tbb.Private += "__TBB_BUILD"_d;
+        tbb.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        if (s.Settings.TargetOS.Type != OSType::Windows)
+        {
+            tbb.Public += "USE_PTHREAD"_d;
+        }
+        if (s.Settings.TargetOS.Type == OSType::Windows)
+        {
+            arch += "win";
+            if (s.Settings.TargetOS.is(ArchType::x86_64))
+            {
+                tbb += "src/tbb/intel64-masm/.*"_rr;
+                arch += "64";
+            }
+            else
+            {
+                tbb += "src/tbb/ia32-masm/.*"_rr;
+                arch += "32";
+            }
+            tbb.Public += "USE_WINTHREAD"_d;
+
+            make_def(tbb, "tbb", arch);
+        }
+
+        make_ver(tbb);
+    }
+
+    auto &tbbmalloc = tbb.addTarget<LibraryTarget>("malloc");
+    {
+        tbbmalloc +=
+            "include/.*"_rr,
+            "src/tbb/.*\\.h"_rr,
+            "src/tbbmalloc/.*\\.cpp"_rr,
+            "src/tbbmalloc/.*\\.def"_rr,
+            "src/tbbmalloc/.*\\.h"_rr;
+
+        tbbmalloc -=
+            "src/tbbmalloc/proxy.*"_rr,
+            "src/tbbmalloc/tbb_function_replacement.*"_rr;
+
+        tbbmalloc.Private += "src"_id;
+
+        tbbmalloc.Public += "include"_id;
+
+        tbbmalloc.Private += "__TBBMALLOC_BUILD"_d;
+        tbbmalloc.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        tbbmalloc.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        if (s.Settings.TargetOS.Type != OSType::Windows)
+        {
+            tbbmalloc.Public += "USE_PTHREAD"_d;
+        }
+        if (s.Settings.TargetOS.Type == OSType::Windows)
+        {
+            tbbmalloc.Public += "USE_WINTHREAD"_d;
+
+            make_def(tbbmalloc, "tbbmalloc", arch);
+        }
+
+        make_ver(tbbmalloc);
+    }
+
+    auto &tbbmalloc_proxy = tbbmalloc.addTarget<LibraryTarget>("proxy");
+    {
+        tbbmalloc_proxy +=
+            "include/.*"_rr,
+            "src/tbb/.*\\.h"_rr,
+            "src/tbbmalloc/.*\\.h"_rr,
+            "src/tbbmalloc/proxy.*"_rr,
+            "src/tbbmalloc/tbb_function_replacement.*"_rr;
+
+        tbbmalloc_proxy.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        tbbmalloc_proxy.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        if (s.Settings.TargetOS.Type != OSType::Windows)
+        {
+            tbbmalloc_proxy.Public += "USE_PTHREAD"_d;
+        }
+        if (s.Settings.TargetOS.Type == OSType::Windows)
+        {
+            tbbmalloc_proxy.Public += "USE_WINTHREAD"_d;
+        }
+
+        tbbmalloc_proxy.Public += tbbmalloc;
+    }
+}
