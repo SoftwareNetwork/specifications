ad06b28c11e54e60e698aef44f1be0a07980d28f
CppanBot <cppanbot@gmail.com>
2019-02-27 15:49:09 +0000

    Update.

---
+++ 8f/f4/74/c7/b50d0fb7199bc846c913b02dbdffaca8f8b43bbf7259fac81308f1b7f7a3e32858748acbda2ed0fd83acd37be05689995afbf7798d2d8da0f534539c/sw.cpp
@@ -0,0 +1,17 @@
+void build(Solution &s)
+{
+    auto &pqxx = s.addTarget<LibraryTarget>("jtv.pqxx", "6.3.2");
+    pqxx += Git("https://github.com/jtv/libpqxx", "{v}");
+
+    pqxx.CPPVersion = CPPLanguageStandard::CPP17;
+    pqxx.ApiName = "PQXX_LIBEXPORT";
+
+    if (s.Settings.TargetOS.Type == OSType::Windows)
+        pqxx.Public += "ws2_32.lib"_slib;
+
+    pqxx.Public += "org.sw.demo.find.libpq-master"_dep;
+
+    pqxx.writeFileOnce("pqxx/config-public-compiler.h");
+    pqxx.writeFileOnce("pqxx/config-internal-libpq.h");
+    pqxx.writeFileOnce("pqxx/config-internal-compiler.h");
+}
