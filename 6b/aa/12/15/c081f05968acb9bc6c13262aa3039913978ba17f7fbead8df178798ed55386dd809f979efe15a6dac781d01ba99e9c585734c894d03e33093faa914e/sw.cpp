5fc8c7b8255eb8a6e2226ec35f136b00f1893e32
CppanBot <cppanbot@gmail.com>
2019-02-27 16:04:09 +0000

    Update.

---
+++ 6b/aa/12/15/c081f05968acb9bc6c13262aa3039913978ba17f7fbead8df178798ed55386dd809f979efe15a6dac781d01ba99e9c585734c894d03e33093faa914e/sw.cpp
@@ -0,0 +1,11 @@
+void build(Solution &s)
+{
+    auto &mach7 = s.addTarget<LibraryTarget>("mach7", "master");
+    mach7 += Git("https://github.com/solodon4/Mach7", "", "{v}");
+
+    mach7 +=
+        "code/mach7/.*"_rr;
+
+    mach7.Public +=
+        "code"_id;
+}
