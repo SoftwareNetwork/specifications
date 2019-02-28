00016563f997f8cc677c5e92b4bf81759d887358
CppanBot <cppanbot@gmail.com>
2019-02-28 18:19:09 +0000

    Update.

---
+++ 60/0f/b5/e3/e5f93d467eca6519b253e41f408768c8050ae19f550970e07c341daffd583a6463aaac9145175d3538c5a5732afafc21c77d0c1e557886df1fe7726b/sw.cpp
@@ -0,0 +1,36 @@
+void build(Solution &s)
+{
+    auto &p = s.addTarget<StaticLibrary>("facebook.folly", "2019.2.25.0");
+    p += Git("https://github.com/facebook/folly", "v{M}.{m:02}.{p:02}.{t:02}");
+
+    auto& t = p;
+    //t.CPPVersion = CPPLanguageStandard::CPP14;
+    t.CPPVersion = CPPLanguageStandard::CPP17;
+
+    t += "folly/.*\\.cpp"_rr;
+    t += "folly/.*\\.h"_rr;
+
+    t -= ".*tools.*"_rr;
+    t -= ".*experimental.*"_rr;
+    t -= ".*exercises.*"_rr;
+    t -= ".*[tT]est.*"_rr;
+
+    t.Public += "_ENABLE_EXTENDED_ALIGNED_STORAGE=1"_def;
+    t.Public += "FOLLY_HAVE_LIBGFLAGS=1"_def;
+
+    t.Public += "org.sw.demo.google.glog-master"_dep;
+    t.Public += "org.sw.demo.google.double_conversion"_dep;
+    t.Public += "org.sw.demo.boost.crc"_dep;
+    t.Public += "org.sw.demo.boost.intrusive"_dep;
+    t.Public += "org.sw.demo.boost.thread"_dep;
+    t.Public += "org.sw.demo.boost.multi_index"_dep;
+    t.Public += "org.sw.demo.boost.context"_dep;
+    t.Public += "org.sw.demo.openssl.ssl-*.*.*.*"_dep;
+    t.Public += "org.sw.demo.libevent-master"_dep;
+    t.Public += "org.sw.demo.python.lib"_dep;
+
+    if (s.Settings.TargetOS.Type == OSType::Windows)
+        t += "NOMINMAX"_d;
+
+    t.writeFileOnce("folly/folly-config.h");
+}
