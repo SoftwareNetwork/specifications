8a45d391ad8d0f33e353c00d7b0ea876da5bed2b
CppanBot <cppanbot@gmail.com>
2019-03-03 12:34:09 +0000

    Update.

---
+++ 0a/14/43/77/343a3ed3be40fc03251bf9d65b7318cc6ba0b50eae470ccfd56fc970a7228a386314571ab4d8e91a99710854df1f72d6370ab8fba23ed8da169f27e8/sw.cpp
@@ -0,0 +1,16 @@
+void build(Solution &s)
+{
+    auto &zxing_cpp = s.addTarget<StaticLibraryTarget>("glassechidna.zxing_cpp", "master");
+    zxing_cpp += Git("https://github.com/glassechidna/zxing-cpp", "", "master");
+
+    zxing_cpp +=
+        "core/src/zxing/.*"_rr;
+
+    zxing_cpp.Public +=
+        "core/src"_id;
+
+    zxing_cpp.Public += "org.sw.demo.gnu.iconv.libiconv-1"_dep;
+    zxing_cpp.Public += "org.sw.demo.mattmccutchen.bigint-master"_dep;
+
+    zxing_cpp.patch("core/src/zxing/aztec/decoder/Decoder.cpp", "char* ss = s;", "const char* ss = s;");
+}
---
+++ 37/82/0d/39/1b087825038e1948ffc12a82a8afbdfe684b9bddbbec97107f30c46e50f53464c409f2c48faa07dad271698bd65751bd20e87a446322db179e6f30ce/sw.cpp
@@ -0,0 +1,14 @@
+void build(Solution &s)
+{
+    auto &bigint = s.addTarget<StaticLibraryTarget>("mattmccutchen.bigint", "master");
+    bigint += Git("https://mattmccutchen.net/bigint/bigint.git", "", "master");
+
+    bigint +=
+        "BigInteger.[hc][hc]"_r,
+        "BigIntegerAlgorithms.[hc][hc]"_r,
+        "BigIntegerLibrary.hh",
+        "BigIntegerUtils.[hc][hc]"_r,
+        "BigUnsigned.[hc][hc]"_r,
+        "BigUnsignedInABase.[hc][hc]"_r,
+        "NumberlikeArray.hh";
+}
