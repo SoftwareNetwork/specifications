028084007ed4af3572b1ccea5dbda9db1121e9ec
CppanBot <cppanbot@gmail.com>
2019-03-05 12:49:09 +0000

    Update.

---
+++ c5/6d/9a/aa/12812d433e31584d5ee8e3bff85369e679856494465dda5359e4131a5ad4e838bd3a1a5b8031d77f0af8453a85ad719a75987a3b489b1d2f7937081d/sw.cpp
@@ -0,0 +1,35 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<Library>("apache.log4cxx", "master");
+	t += Git("https://github.com/apache/logging-log4cxx", "", "{v}");
+
+	t.setRootDirectory("src/main");
+
+	t += "include/.*"_rr;
+	t += "cpp/.*"_rr;
+
+	t -= "cpp/asyncappender_nonblocking.cpp";
+
+	t += "LOG4CXX"_d;
+	t.Public += sw::Static, "LOG4CXX_STATIC"_d;
+
+	if (s.Settings.TargetOS.Type == OSType::Windows)
+		t += "odbc32.lib"_slib;
+
+	/*t.Variables["LOGCHAR_IS_UNICHAR"] = 0;
+	t.Variables["LOGCHAR_IS_UTF8"] = 1;
+	t.Variables["LOGCHAR_IS_WCHAR"] = 0;
+
+	t.Variables["CHAR_API"] = 0;
+	t.Variables["WCHAR_T_API"] = 0;
+	t.Variables["UNICHAR_API"] = 0;
+	t.Variables["CFSTRING_API"] = 0;*/
+
+	//t.configureFile("include/log4cxx/log4cxx.h.in", "log4cxx/log4cxx.h");
+	t.configureFile("include/log4cxx/log4cxx.hw", "log4cxx/log4cxx.h");
+
+	//t.configureFile("include/log4cxx/private/log4cxx_private.h.in", "log4cxx/private/log4cxx_private.h");
+	t.configureFile("include/log4cxx/private/log4cxx_private.hw", "log4cxx/private/log4cxx_private.h");
+
+	t.Public += "org.sw.demo.apache.apr.util"_dep;
+}
