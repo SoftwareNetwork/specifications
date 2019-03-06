93062111ea839871050e4cb3089004008b7bae8a
CppanBot <cppanbot@gmail.com>
2019-03-06 16:55:44 +0000

    Update.

---
+++ 49/50/ef/46/57c40e6347b32054d64e175026e03cc7c285423eff3efe1456bb954ff6d8c24ee01a7a70769c0027f0c479f4d28d95d4711881c8ec2aae6889c41a92/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &charls = s.addTarget<LibraryTarget>("vbaderks.charls", "master");
+    charls += Git("https://github.com/team-charls/charls", "", "{v}");
+
+    charls.Public += sw::Static, "CHARLS_STATIC"_def;
+    charls += "CHARLS_LIBRARY_BUILD"_def;
+}
