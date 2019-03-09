bb3fd710c79597fe99655882c53c098a0d320176
CppanBot <cppanbot@gmail.com>
2019-03-09 11:18:54 +0000

    Update.

---
+++ 03/2a/e3/0c/bff032485fd6f4d3eb8be7d40500813c127977bc91aeb56b9037df3df9d3afd9031b05bea23ba7f23d729796cd742d408a07352c49312c9dd8a5cf9a/sw.cpp
@@ -0,0 +1,11 @@
+void build(Solution &s)
+{
+    auto &cds = s.addTarget<LibraryTarget>("khizmax.cds", "2.3.3");
+    cds += Git("https://github.com/khizmax/libcds", "v{v}");
+
+    cds +=
+        "cds/.*"_rr,
+        "src/.*"_rr;
+
+    cds += "CDS_BUILD_LIB"_d;
+}
