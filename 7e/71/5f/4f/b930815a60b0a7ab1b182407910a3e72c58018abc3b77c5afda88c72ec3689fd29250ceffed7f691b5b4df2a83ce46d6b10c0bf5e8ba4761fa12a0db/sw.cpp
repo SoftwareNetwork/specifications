f2b362cae8656e6368e3e554a5850657c44af1ff
CppanBot <cppanbot@gmail.com>
2019-03-14 22:48:54 +0000

    Update.

--- 7e/71/5f/4f/b930815a60b0a7ab1b182407910a3e72c58018abc3b77c5afda88c72ec3689fd29250ceffed7f691b5b4df2a83ce46d6b10c0bf5e8ba4761fa12a0db/sw.cpp
+++ 7e/71/5f/4f/b930815a60b0a7ab1b182407910a3e72c58018abc3b77c5afda88c72ec3689fd29250ceffed7f691b5b4df2a83ce46d6b10c0bf5e8ba4761fa12a0db/sw.cpp
@@ -1,5 +1,5 @@
 void build(Solution &s)
 {
-    auto &range_v3 = s.addTarget<LibraryTarget>("ericniebler.range_v3", "master");
+    auto &range_v3 = s.addTarget<LibraryTarget>("ericniebler.range_v3", "0.3.6");
     range_v3 += Git("https://github.com/ericniebler/range-v3");
 }
