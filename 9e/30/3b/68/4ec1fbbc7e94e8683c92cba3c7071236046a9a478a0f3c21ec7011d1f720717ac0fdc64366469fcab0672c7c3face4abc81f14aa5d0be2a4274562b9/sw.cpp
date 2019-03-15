eb5aa895a0fa895bb60b01139e4f305ef50e03d4
CppanBot <cppanbot@gmail.com>
2019-03-15 20:33:54 +0000

    Update.

---
+++ 9e/30/3b/68/4ec1fbbc7e94e8683c92cba3c7071236046a9a478a0f3c21ec7011d1f720717ac0fdc64366469fcab0672c7c3face4abc81f14aa5d0be2a4274562b9/sw.cpp
@@ -0,0 +1,22 @@
+void build(Solution &s)
+{
+    auto &jasper = s.addTarget<LibraryTarget>("mdadams.jasper", "2.0.16");
+    jasper += Git("https://github.com/mdadams/jasper", "version-{v}");
+
+    jasper.SourceDir /= "src/libjasper";
+    jasper +=
+        ".*\\.c"_rr,
+        ".*\\.h"_rr,
+        ".*\\.in"_rr;
+
+    jasper -=
+        "jpg/jpg_dummy.c";
+
+    jasper.Private += sw::Shared, "JAS_BUILDING_DLL"_d;
+    jasper.Public += sw::Shared, "JAS_DLL"_d;
+    jasper.Public += sw::Shared, "JAS_HAVE_VISIBILITY"_d;
+
+    jasper.Public += "org.sw.demo.jpeg-9"_dep;
+
+    jasper.configureFile("include/jasper/jas_config.h.in", "jasper/jas_config.h");
+}
