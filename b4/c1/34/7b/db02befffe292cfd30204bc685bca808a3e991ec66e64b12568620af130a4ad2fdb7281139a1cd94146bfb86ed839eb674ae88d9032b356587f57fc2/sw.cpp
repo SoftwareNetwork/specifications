14a4b8df0583284b0296ccd8e616f387d9198d87
CppanBot <cppanbot@gmail.com>
2019-03-16 13:48:56 +0000

    Update.

--- b4/c1/34/7b/db02befffe292cfd30204bc685bca808a3e991ec66e64b12568620af130a4ad2fdb7281139a1cd94146bfb86ed839eb674ae88d9032b356587f57fc2/sw.cpp
+++ b4/c1/34/7b/db02befffe292cfd30204bc685bca808a3e991ec66e64b12568620af130a4ad2fdb7281139a1cd94146bfb86ed839eb674ae88d9032b356587f57fc2/sw.cpp
@@ -2,4 +2,7 @@ void build(Solution &s)
 {
     auto &json = s.addTarget<LibraryTarget>("nlohmann.json", "3.5.0");
     json += Git("https://github.com/nlohmann/json", "v{v}");
+
+    json += "include/.*"_rr;
+    json.Public += "nlohmann_json.natvis";
 }
