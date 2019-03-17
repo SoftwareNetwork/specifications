e1b47fac225e91c3d7523a100d4a0981463b5721
CppanBot <cppanbot@gmail.com>
2019-03-17 22:03:54 +0000

    Update.

---
+++ 36/b8/a9/ef/1117c1aee3dfce21dce85c3d7944c6cef008dd558a52e88441f3a9fe831f7441cf3bb4dab45320f7841c27f88e79cf5c20c224e89bf2cfb8b1ead20d/sw.cpp
@@ -0,0 +1,75 @@
+void build(Solution &s)
+{
+    // sdl cannot load vk as static lib
+    auto &t = s.addTarget<SharedLibrary>("khronos.vulkan.loader", "1.1.102");
+    t += Git("https://github.com/KhronosGroup/Vulkan-Loader", "v{v}");
+
+    t.setChecks("vk");
+
+    t += "loader/.*"_r;
+    t += "scripts/.*"_r;
+    t -= "loader/vk_loader_extensions.c";
+    t -= "loader/asm_offset.c";
+    t -= "loader/.*\\.asm"_r;
+
+    t += "loader"_idir;
+
+    t += "API_NAME=\"Vulkan\""_def;
+
+    if (s.Settings.TargetOS.Type == OSType::Windows)
+    {
+        t += "VK_USE_PLATFORM_WIN32_KHR"_def;
+
+        t += "Cfgmgr32.lib"_slib;
+        t += "advapi32.lib"_slib;
+
+        t.patch("loader/vulkan-1.def", "LIBRARY vulkan-1.dll", "");
+
+        // only when shared
+        t.Interface += Definition("SW_VK_LIBRARY_NAME=\"" + t.getOutputFile().filename().u8string() + "\"");
+    }
+
+    auto GenerateFromVkXml = [&t](const path &output, const path &dep)
+    {
+        auto c = t.addCommand();
+        c << cmd::prog("org.sw.demo.python.exe-3"_dep)
+            << cmd::wdir(t.BinaryDir)
+            << cmd::in("scripts/loader_genvk.py")
+            << "-registry"
+            << cmd::in(t.getFile("org.sw.demo.khronos.vulkan"_dep, "registry/vk.xml"))
+            << "-scripts"
+            << t.getFile("org.sw.demo.khronos.vulkan"_dep, "registry")
+            << output
+            << cmd::end()
+            << cmd::in(path("scripts") / dep)
+            << cmd::out(output)
+            // deps:
+            // ${VulkanRegistry_DIR}/generator.py
+            // ${VulkanRegistry_DIR}/reg.py
+            ;
+    };
+
+    GenerateFromVkXml("vk_layer_dispatch_table.h", "loader_extension_generator.py");
+    GenerateFromVkXml("vk_dispatch_table_helper.h", "dispatch_table_helper_generator.py");
+    GenerateFromVkXml("vk_safe_struct.h", "helper_file_generator.py");
+    GenerateFromVkXml("vk_safe_struct.cpp", "helper_file_generator.py");
+    GenerateFromVkXml("vk_enum_string_helper.h", "helper_file_generator.py");
+    GenerateFromVkXml("vk_object_types.h", "helper_file_generator.py");
+    GenerateFromVkXml("vk_extension_helper.h", "helper_file_generator.py");
+    GenerateFromVkXml("vk_typemap_helper.h", "helper_file_generator.py");
+
+    GenerateFromVkXml("vk_loader_extensions.h", "loader_extension_generator.py");
+    GenerateFromVkXml("vk_loader_extensions.c", "loader_extension_generator.py");
+    t -= "vk_loader_extensions.c";
+
+    t.configureFile("loader/loader_cmake_config.h.in", "loader_cmake_config.h");
+
+    t.Public += "org.sw.demo.khronos.vulkan"_dep;
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("vk");
+    s.checkFunctionExists("secure_getenv");
+    s.checkFunctionExists("__secure_getenv");
+}
