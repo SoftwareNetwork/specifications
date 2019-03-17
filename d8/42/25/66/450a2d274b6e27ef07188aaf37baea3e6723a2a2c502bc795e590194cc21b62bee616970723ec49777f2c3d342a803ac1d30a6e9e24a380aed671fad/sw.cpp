38409660e50f3987e89b1d6d7c3f07fa3e67f597
CppanBot <cppanbot@gmail.com>
2019-03-17 20:03:54 +0000

    Update.

--- d8/42/25/66/450a2d274b6e27ef07188aaf37baea3e6723a2a2c502bc795e590194cc21b62bee616970723ec49777f2c3d342a803ac1d30a6e9e24a380aed671fad/sw.cpp
+++ d8/42/25/66/450a2d274b6e27ef07188aaf37baea3e6723a2a2c502bc795e590194cc21b62bee616970723ec49777f2c3d342a803ac1d30a6e9e24a380aed671fad/sw.cpp
@@ -1,5 +1,5 @@
 void build(Solution &s)
 {
     auto &vulkan = s.addTarget<LibraryTarget>("khronos.vulkan", "1.1.103");
-    vulkan += Git("https://github.com/KhronosGroup/Vulkan-Docs", "v{v}");
+    vulkan += Git("https://github.com/KhronosGroup/Vulkan-Headers", "v{v}");
 }
