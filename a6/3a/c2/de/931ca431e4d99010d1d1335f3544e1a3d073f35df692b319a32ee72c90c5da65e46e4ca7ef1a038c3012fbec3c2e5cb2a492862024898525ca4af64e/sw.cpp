2af7d4159f7fe56b65ca827268c04c424232b78e
CppanBot <cppanbot@gmail.com>
2019-03-20 01:33:54 +0000

    Update.

---
+++ a6/3a/c2/de/931ca431e4d99010d1d1335f3544e1a3d073f35df692b319a32ee72c90c5da65e46e4ca7ef1a038c3012fbec3c2e5cb2a492862024898525ca4af64e/sw.cpp
@@ -0,0 +1,13 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("brechtsanders.xlsxio", "0.2.21");
+    t += Git("https://github.com/brechtsanders/xlsxio");
+
+    t.ApiName = "DLL_EXPORT_XLSXIO";
+
+    t += "include/.*"_rr;
+    t += "lib/.*"_rr;
+
+    t += "org.sw.demo.expat"_dep;
+    t += "org.sw.demo.nih_at.libzip"_dep;
+}
