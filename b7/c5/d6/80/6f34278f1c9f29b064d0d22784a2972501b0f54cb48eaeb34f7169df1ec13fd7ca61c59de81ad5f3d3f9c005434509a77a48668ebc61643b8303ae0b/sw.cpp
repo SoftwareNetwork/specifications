ec0af2e6e18cdba683ae512cb3e20e254938a168
CppanBot <cppanbot@gmail.com>
2019-03-23 19:56:21 +0000

    Update.

--- b7/c5/d6/80/6f34278f1c9f29b064d0d22784a2972501b0f54cb48eaeb34f7169df1ec13fd7ca61c59de81ad5f3d3f9c005434509a77a48668ebc61643b8303ae0b/sw.cpp
+++ b7/c5/d6/80/6f34278f1c9f29b064d0d22784a2972501b0f54cb48eaeb34f7169df1ec13fd7ca61c59de81ad5f3d3f9c005434509a77a48668ebc61643b8303ae0b/sw.cpp
@@ -5,4 +5,10 @@ void build(Solution &s)
     sqlpp11.Public += "org.sw.demo.howardhinnant.date.date-2"_dep;
     sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "Input", "s");
     sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "(&s)[N]", "*s");
+
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "-> result_t<decltype(db.select(composite)), _result_row_t<Db>>", "//->  result_t<decltype(db.select(composite)), _result_row_t<Db>>");
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "return {db.select(composite), get_dynamic_names()};", "return result_t<decltype(db.select(composite)), _result_row_t<Db>>{db.select(composite), get_dynamic_names()};");
+
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "-> result_t<decltype(db.select(std::declval<_statement_t>())), _result_row_t<Db>>", "//->  result_t<decltype(db.select(std::declval<_statement_t>())), _result_row_t<Db>>");
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "return {db.select(_get_statement()), get_dynamic_names()};", "return result_t<decltype(db.select(std::declval<_statement_t>())), _result_row_t<Db>>{db.select(_get_statement()), get_dynamic_names()};");
 }
