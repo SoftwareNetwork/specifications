e08e166822cd1d6f0e0e412504b4cc0e593ec244
CppanBot <cppanbot@gmail.com>
2019-03-23 20:41:21 +0000

    Update.

--- 68/00/3e/70/e8ea709ebbf107ae96462388aa650ec569f4405459c5076e56162a45eee97e58b46072606cb51e0a86b247a494a6cda379edfcd01d7318db5d7b8d45/sw.cpp
+++ 68/00/3e/70/e8ea709ebbf107ae96462388aa650ec569f4405459c5076e56162a45eee97e58b46072606cb51e0a86b247a494a6cda379edfcd01d7318db5d7b8d45/sw.cpp
@@ -1,8 +1,14 @@
 void build(Solution &s)
 {
-	auto &sqlpp11 = s.addTarget<LibraryTarget>("rbock.sqlpp11", "develop");
-	sqlpp11 += Git("https://github.com/rbock/sqlpp11", "", "{v}");
-	sqlpp11.Public += "org.sw.demo.howardhinnant.date.date-2"_dep;
-	sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "Input", "s");
-	sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "(&s)[N]", "*s");
+    auto &sqlpp11 = s.addTarget<LibraryTarget>("rbock.sqlpp11", "develop");
+    sqlpp11 += Git("https://github.com/rbock/sqlpp11");
+    sqlpp11.Public += "org.sw.demo.howardhinnant.date.date-2"_dep;
+    sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "Input", "s");
+    sqlpp11.replaceInFileOnce("include/sqlpp11/char_sequence.h", "(&s)[N]", "*s");
+
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "-> result_t<decltype(db.select(composite)), _result_row_t<Db>>", "//->  result_t<decltype(db.select(composite)), _result_row_t<Db>>");
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "return {db.select(composite), get_dynamic_names()};", "return result_t<decltype(db.select(composite)), _result_row_t<Db>>{db.select(composite), get_dynamic_names()};");
+
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "-> result_t<decltype(db.select(std::declval<_statement_t>())), _result_row_t<Db>>", "//->  result_t<decltype(db.select(std::declval<_statement_t>())), _result_row_t<Db>>");
+    sqlpp11.replaceInFileOnce("include/sqlpp11/select_column_list.h", "return {db.select(_get_statement()), get_dynamic_names()};", "return result_t<decltype(db.select(std::declval<_statement_t>())), _result_row_t<Db>>{db.select(_get_statement()), get_dynamic_names()};");
 }
