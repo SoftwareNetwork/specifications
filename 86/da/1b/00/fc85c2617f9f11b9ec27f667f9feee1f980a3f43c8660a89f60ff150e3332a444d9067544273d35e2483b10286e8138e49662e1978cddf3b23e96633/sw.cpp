af71890f8db81d7fcff9bc4a35e24c75877bf1ed
CppanBot <cppanbot@gmail.com>
2019-04-05 09:11:21 +0000

    Update.

---
+++ 86/da/1b/00/fc85c2617f9f11b9ec27f667f9feee1f980a3f43c8660a89f60ff150e3332a444d9067544273d35e2483b10286e8138e49662e1978cddf3b23e96633/sw.cpp
@@ -0,0 +1,28 @@
+void build(Solution &s)
+{
+    auto &cryptopp = s.addTarget<LibraryTarget>("weidai11.cryptopp", "8.1.0");
+    cryptopp += Git("https://github.com/weidai11/cryptopp", "CRYPTOPP_{M}_{m}_{p}");
+
+    cryptopp +=
+        "[^/]*\\.asm"_rr,
+        "[^/]*\\.cpp"_rr,
+        "[^/]*\\.[hs]"_rr;
+
+    cryptopp -=
+        "bench.*\\.cpp"_rr,
+        "fipsalgt.cpp",
+        ".*test.*"_rr,
+        "validat.*\\.cpp"_rr
+        ;
+
+    cryptopp.Interface += sw::Shared, "CRYPTOPP_IMPORTS"_d;
+    cryptopp += sw::Shared, "CRYPTOPP_EXPORTS"_d;
+
+    // for asm
+    if (s.Settings.TargetOS.is(ArchType::x86_64))
+        cryptopp += "_M_X64"_def;
+    else if (s.Settings.TargetOS.is(ArchType::x86))
+        cryptopp += "_M_X86"_def;
+
+    cryptopp.patch("iterhash.cpp", "#if defined(__GNUC__) || defined(__clang__)", "#if defined(__GNUC__)  || defined(__clang__) || defined(_WIN32)");
+}
