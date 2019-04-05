a55fabd5aaebe891679df5836286b84d481f7cac
CppanBot <cppanbot@gmail.com>
2019-04-05 09:26:21 +0000

    Update.

---
+++ a2/10/07/69/40e170900b073ba1b2f0286efe24b7b9439cf46650ac3d7efe605e3e2fc837f5a8cac8226f5597ed252ee9cc7559d3fd80b132d1d1e551a4c1bcf6db/sw.cpp
@@ -0,0 +1,7 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibraryTarget>("vincentlaucsb.csv_parser", "1.1.3");
+    t += Git("https://github.com/vincentlaucsb/csv-parser");
+
+    t += "include/.*"_rr;
+}
