5416f6904482eb61d8b1566890be7317876efece
CppanBot <cppanbot@gmail.com>
2019-04-07 20:11:21 +0000

    Update.

---
+++ 12/de/46/32/f2509e7b65b99b8a154b5edf0d02c93f23722e2bdc3b263a60d0826a9022ba80bb0f15ff6cbb504c9fcf29ce0841acacf0e84676023fd3f339ed8e50/sw.cpp
@@ -0,0 +1,5 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibraryTarget>("therocode.meta_enum", "master");
+    t += Git("https://github.com/therocode/meta_enum");
+}
---
+++ db/24/cf/93/c61f05b458035931716afc906964c3296e7383ca7d5e478ea42559025194ba9924a72ff47a873890fc706804074b27ab0dc09b8445962fb2c7c6d38b/sw.cpp
@@ -0,0 +1,10 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibraryTarget>("google.cctz", "2.3");
+    t += Git("https://github.com/google/cctz", "v{M}.{m}");
+
+    t += "include/.*"_rr;
+    t += "src/.*"_rr;
+    t -= ".*test.*"_rr;
+    t -= ".*bench.*"_rr;
+}
