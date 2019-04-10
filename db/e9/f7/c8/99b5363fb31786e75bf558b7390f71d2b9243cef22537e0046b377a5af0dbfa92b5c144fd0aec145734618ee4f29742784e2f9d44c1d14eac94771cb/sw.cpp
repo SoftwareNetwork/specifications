c4b700a8bc5979b6aa6817fe9ac92fd6f48890a0
CppanBot <cppanbot@gmail.com>
2019-04-10 12:41:21 +0000

    Update.

---
+++ db/e9/f7/c8/99b5363fb31786e75bf558b7390f71d2b9243cef22537e0046b377a5af0dbfa92b5c144fd0aec145734618ee4f29742784e2f9d44c1d14eac94771cb/sw.cpp
@@ -0,0 +1,19 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("objectcomputing.mFAST", "master");
+    t += Git("https://github.com/objectcomputing/mFAST");
+
+    t.ApiNames.insert("MFAST_EXPORT");
+    t.ApiNames.insert("MFAST_XML_PARSER_EXPORT");
+    t.ApiNames.insert("MFAST_SQLITE3_EXPORT");
+    t.ApiNames.insert("MFAST_JSON_EXPORT");
+    t.ApiNames.insert("MFAST_CODER_EXPORT");
+
+    t.Public += "NOMINMAX"_def;
+
+    t.Public += "org.sw.demo.boost.assign"_dep;
+    t.Public += "org.sw.demo.sqlite3"_dep;
+    t.Public += "org.sw.demo.leethomason.tinyxml2"_dep;
+
+    t.patch("src/mfast/xml_parser/templates_builder.cpp", "XMLDocument", "tinyxml2::XMLDocument");
+}
