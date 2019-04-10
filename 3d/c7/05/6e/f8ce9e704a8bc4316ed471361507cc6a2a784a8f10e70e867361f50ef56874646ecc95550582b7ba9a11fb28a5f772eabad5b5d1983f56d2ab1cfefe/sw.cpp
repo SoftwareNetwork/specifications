f2c56010c02f98d7ddc56aff03b650d783a1dfc4
CppanBot <cppanbot@gmail.com>
2019-04-10 12:11:21 +0000

    Update.

---
+++ 3d/c7/05/6e/f8ce9e704a8bc4316ed471361507cc6a2a784a8f10e70e867361f50ef56874646ecc95550582b7ba9a11fb28a5f772eabad5b5d1983f56d2ab1cfefe/sw.cpp
@@ -0,0 +1,15 @@
+void build(Solution &s)
+{
+    auto &tinyxml = s.addTarget<StaticLibraryTarget>("leethomason.tinyxml", "2.6.2");
+    tinyxml += RemoteFile("http://freefr.dl.sourceforge.net/project/tinyxml/tinyxml/{v}/tinyxml_{M}_{m}_{p}.zip");
+
+    tinyxml +=
+        "tinystr.cpp",
+        "tinystr.h",
+        "tinyxml.cpp",
+        "tinyxml.h",
+        "tinyxmlerror.cpp",
+        "tinyxmlparser.cpp";
+
+    tinyxml.Public += "TIXML_USE_STL"_d;
+}
---
+++ 55/1e/11/b0/13e3cb1b76647df28555ac6c0580e4a48ddc3453a92147e69dc103b6a688ac1506a69a323e0111917e0644d8b7606eef778f3c90fc6ad9735342dd09/sw.cpp
@@ -0,0 +1,12 @@
+void build(Solution &s)
+{
+    auto &tinyxml2 = s.addTarget<LibraryTarget>("leethomason.tinyxml2", "7.0.1");
+    tinyxml2 += Git("https://github.com/leethomason/tinyxml2");
+
+    tinyxml2 +=
+        "tinyxml2.cpp",
+        "tinyxml2.h";
+
+    tinyxml2.Interface += sw::Shared, "TINYXML2_IMPORT"_d;
+    tinyxml2 += sw::Shared, "TINYXML2_EXPORT"_d;
+}
