6727324cdde57966e12ba2f28281d1865ebcd6df
CppanBot <cppanbot@gmail.com>
2019-04-18 14:03:20 +0000

    Update.

---
+++ 6d/d5/dc/37/179ecd8e1dc461894bd9fe16f0a0dce68ec878477dbd481f606178fd90360193488f5091cb3484e7053b85ff0cfe0102469f4db166baa964bac0511b/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &sqlite3 = s.addTarget<LibraryTarget>("sqlite3", "3.28.0");
+    sqlite3 += RemoteFile("http://www.sqlite.org/2019/sqlite-amalgamation-{M}{m:02}{p:02}00.zip");
+    sqlite3 += "sqlite3.[hc]"_r;
+    sqlite3 += "sqlite3ext.h";
+    sqlite3.ApiName = "SQLITE_API";
+}
