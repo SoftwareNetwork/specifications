1938b9fd812ca3ee68d80a3ffb16802915c320a6
CppanBot <cppanbot@gmail.com>
2019-04-18 17:03:20 +0000

    Update.

---
+++ 79/62/be/e2/3e93cda953d022a854b5229bcca39fbb4366bb5944dccbf4445b89a86c417de9c7b28eae4ba21e18c664bbf7827d6562dfed9f7fd1b9f20d48a667bb/sw.cpp
@@ -0,0 +1,30 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibraryTarget>("amazon.awslabs.aws_c_io", "0.3.0");
+    t += Git("https://github.com/awslabs/aws-c-io", "v{v}");
+
+    t -=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t += "source/.*"_r;
+
+    if (s.Settings.Native.CompilerType == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_IO_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_IO_USE_IMPORT_EXPORT"_d;
+
+    if (s.Settings.TargetOS.Type == OSType::Windows)
+    {
+        t += "source/windows/.*"_rr;
+        t.Public += "AWS_USE_IO_COMPLETION_PORTS"_d;
+    }
+    else
+    {
+        t += "source/posix/.*"_rr;
+    }
+
+    t += "org.sw.demo.amazon.awslabs.c_common"_dep;
+}
