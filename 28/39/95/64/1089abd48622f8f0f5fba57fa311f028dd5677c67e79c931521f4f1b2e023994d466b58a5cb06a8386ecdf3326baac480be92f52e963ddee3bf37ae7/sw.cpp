6135e099e4f2c2db1f106d3a855f12502bf48882
CppanBot <cppanbot@gmail.com>
2019-05-01 14:48:13 +0000

    Update.

---
+++ 28/39/95/64/1089abd48622f8f0f5fba57fa311f028dd5677c67e79c931521f4f1b2e023994d466b58a5cb06a8386ecdf3326baac480be92f52e963ddee3bf37ae7/sw.cpp
@@ -0,0 +1,33 @@
+void build(Solution &s)
+{
+    auto &png = s.addTarget<LibraryTarget>("glennrp.png", "1.6.37");
+    png += Git("https://github.com/glennrp/libpng", "v{v}");
+
+    png.ApiName = "PNG_IMPEXP";
+    png +=
+        "png.c",
+        "png.h",
+        "pngconf.h",
+        "pngdebug.h",
+        "pngerror.c",
+        "pngget.c",
+        "pnginfo.h",
+        "pngmem.c",
+        "pngpread.c",
+        "pngpriv.h",
+        "pngread.c",
+        "pngrio.c",
+        "pngrtran.c",
+        "pngrutil.c",
+        "pngset.c",
+        "pngstruct.h",
+        "pngtrans.c",
+        "pngwio.c",
+        "pngwrite.c",
+        "pngwtran.c",
+        "pngwutil.c",
+        "scripts/pnglibconf.h.prebuilt";
+
+    png.Public += "org.sw.demo.madler.zlib-1"_dep;
+    png.configureFile("scripts/pnglibconf.h.prebuilt", "pnglibconf.h");
+}
