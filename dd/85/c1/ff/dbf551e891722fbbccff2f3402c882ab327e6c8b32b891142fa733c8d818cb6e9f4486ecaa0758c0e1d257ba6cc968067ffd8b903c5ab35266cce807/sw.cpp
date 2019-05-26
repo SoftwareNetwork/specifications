3e84fc871d1e9ada10a6484f2d0fa1d34c70102b
CppanBot <cppanbot@gmail.com>
2019-05-26 19:51:46 +0000

    Update.

---
+++ dd/85/c1/ff/dbf551e891722fbbccff2f3402c882ab327e6c8b32b891142fa733c8d818cb6e9f4486ecaa0758c0e1d257ba6cc968067ffd8b903c5ab35266cce807/sw.cpp
@@ -0,0 +1,17 @@
+void build(Solution &s)
+{
+    auto &lzma_sdk = s.addTarget<LibraryTarget>("_7zip.lzma_sdk", "18.1.0");
+    lzma_sdk += RemoteFile("https://www.7-zip.org/a/lzma{M}0{m}.7z");
+
+    lzma_sdk.setRootDirectory("C");
+    lzma_sdk +=
+        ".*\\.h"_rr,
+        "Alloc.c",
+        "LzFind.c",
+        "LzFindMt.c",
+        "LzmaDec.c",
+        "LzmaEnc.c",
+        "LzmaLib.c",
+        "Threads.c",
+        "Util/LzmaLib/LzmaLib.def";
+}
