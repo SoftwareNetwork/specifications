fbf45d5b17ccefd6c0b2a22b36848dddf2c06cf2
CppanBot <cppanbot@gmail.com>
2019-06-02 00:21:47 +0000

    Update.

---
+++ 7a/88/eb/6d/a5fbe5fd066afde12fd2697ebb086ca6a7bf62cb7beafb4ff220bf49176398abaebe50df4b35a9b3feecf833af50225a2bea029888ab137615cbe0cf/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &libtomcrypt = s.addTarget<StaticLibraryTarget>("libtom.libtomcrypt", "1.18.2");
+    libtomcrypt += Git("https://github.com/libtom/libtomcrypt", "v{v}");
+
+    libtomcrypt += "LTC_SOURCE"_def;
+    libtomcrypt.Public += "src/headers"_idir;
+}
