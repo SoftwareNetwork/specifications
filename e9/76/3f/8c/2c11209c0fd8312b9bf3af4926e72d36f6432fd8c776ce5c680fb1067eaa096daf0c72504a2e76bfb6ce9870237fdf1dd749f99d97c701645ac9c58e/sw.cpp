f63128f9c55363915e204ba94268a25f6ed58c43
CppanBot <cppanbot@gmail.com>
2019-06-10 09:42:21 +0000

    Update.

---
+++ e9/76/3f/8c/2c11209c0fd8312b9bf3af4926e72d36f6432fd8c776ce5c680fb1067eaa096daf0c72504a2e76bfb6ce9870237fdf1dd749f99d97c701645ac9c58e/sw.cpp
@@ -0,0 +1,6 @@
+void build(Solution &s)
+{
+    auto &sqlite3pp = s.addTarget<StaticLibraryTarget>("iwongu.sqlite3pp", "1.0.8");
+    sqlite3pp += Git("https://github.com/iwongu/sqlite3pp", "v{v}");
+    sqlite3pp.Public += "org.sw.demo.sqlite3-3"_dep;
+}
