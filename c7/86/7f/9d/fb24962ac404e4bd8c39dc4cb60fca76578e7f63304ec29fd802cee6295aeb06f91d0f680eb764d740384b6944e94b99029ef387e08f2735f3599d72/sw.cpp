a5e6c36e5b29be2277b509d0866bef25d9e7ffdd
CppanBot <cppanbot@gmail.com>
2019-06-11 22:12:21 +0000

    Update.

---
+++ c7/86/7f/9d/fb24962ac404e4bd8c39dc4cb60fca76578e7f63304ec29fd802cee6295aeb06f91d0f680eb764d740384b6944e94b99029ef387e08f2735f3599d72/sw.cpp
@@ -0,0 +1,15 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("optimad.bitpit", "master");
+    t += Git("https://github.com/optimad/bitpit");
+
+    if (!t.DryRun)
+    {
+        for (auto &f : fs::directory_iterator(t.SourceDir / "src"))
+        {
+            if (!fs::is_directory(f))
+                continue;
+            t += IncludeDirectory(normalize_path(f));
+        }
+    }
+}
