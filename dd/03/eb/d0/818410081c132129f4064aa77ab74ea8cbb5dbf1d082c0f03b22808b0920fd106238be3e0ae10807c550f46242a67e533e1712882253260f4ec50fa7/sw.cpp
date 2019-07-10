fb3be8f1b30aa4813d1fe6036c3a826317b7898f
CppanBot <cppanbot@gmail.com>
2019-07-10 10:51:16 +0000

    Update.

---
+++ dd/03/eb/d0/818410081c132129f4064aa77ab74ea8cbb5dbf1d082c0f03b22808b0920fd106238be3e0ae10807c550f46242a67e533e1712882253260f4ec50fa7/sw.cpp
@@ -0,0 +1,19 @@
+void build(Solution &s)
+{
+    auto &lib = s.addTarget<LibraryTarget>("emcrisostomo.fswatch.lib", "1.14.0");
+    lib += Git("https://github.com/emcrisostomo/fswatch");
+
+    lib.setRootDirectory("libfswatch");
+    lib += "src/libfswatch"_id;
+
+    lib += "HAVE_CXX_MUTEX"_def;
+
+    lib += "HAVE_WINDOWS"_def;
+    lib -= "src/libfswatch/c++/poll_monitor.cpp";
+    lib -= "src/libfswatch/c++/fsevents_monitor.cpp";
+    lib -= "src/libfswatch/c++/inotify_monitor.cpp";
+    lib -= "src/libfswatch/c++/kqueue_monitor.cpp";
+
+    lib.Public += "org.sw.demo.tronkko.dirent-master"_dep;
+    lib.Public += "org.sw.demo.kimgr.getopt_port-master"_dep;
+}
