832fe122ab807a3c2d0224e079158914d2e990b7
CppanBot <cppanbot@gmail.com>
2019-07-12 17:51:16 +0000

    Update.

---
+++ fe/9c/e4/85/7a394d59c646fb9bd1a5bdc7f47ba71d1fefcfc7368766334b616e28d5e51d9b4a24b7b0069d52f4f01901b4b3bbe5af44c2faed35475c3e97c63424/sw.cpp
@@ -0,0 +1,107 @@
+#pragma sw header on
+
+static auto gen_protobuf(const DependencyPtr &base, NativeExecutedTarget &t,
+    path f, bool public_protobuf = false, const path &out_dir = {})
+{
+    auto protoc = std::make_shared<Dependency>(base->package);
+    protoc->package.ppath /= "protoc";
+
+    auto protobuf = std::make_shared<Dependency>(base->package);
+    protobuf->package.ppath /= "protobuf";
+
+    if (!f.is_absolute())
+        f = t.SourceDir / f;
+
+    auto n = f.filename().stem().u8string();
+    auto d = f.parent_path();
+
+    if (out_dir.is_absolute())
+        throw std::logic_error("Make out_dir relative");
+    auto bdir = t.BinaryDir / out_dir;
+
+    auto o = bdir / n;
+    auto ocpp = o;
+    ocpp += ".pb.cc";
+    auto oh = o;
+    oh += ".pb.h";
+
+    auto c = t.addCommand();
+    c << cmd::prog(protoc)
+        << cmd::wdir(bdir)
+        << "-I" << normalize_path(d) // must be normalized as f
+        << "-I" << [protoc, &t]() { return normalize_path(sw::LocalPackage(t.getSolution().swctx.getLocalStorage(), protoc->getResolvedPackage()).getDirSrc2() / "src"); }
+        << ("--cpp_out=" + normalize_path(bdir))
+        << cmd::in(normalize_path(f)) // must be normalized as d
+        << cmd::end()
+        << cmd::out(ocpp)
+        << cmd::out(oh);
+
+    t += protobuf;
+    if (public_protobuf)
+        t.Public += protobuf;
+
+    return std::tuple{ protoc, c };
+}
+
+#pragma sw header off
+
+void build(Solution &s)
+{
+    auto &p = s.addProject("google.protobuf", "3.9.0");
+    p += Git("https://github.com/protocolbuffers/protobuf", "v{v}");
+
+    auto &protobuf_lite = p.addTarget<LibraryTarget>("protobuf_lite");
+    protobuf_lite.ImportFromBazel = true;
+    if (protobuf_lite.getSettings().TargetOS.Type != OSType::Windows)
+        protobuf_lite.ExportAllSymbols = true;
+    protobuf_lite += "src/google/protobuf/.*\\.h"_rr;
+    protobuf_lite += "src/google/protobuf/.*\\.inc"_rr;
+    protobuf_lite += "src/google/protobuf/io/strtod.cc";
+    protobuf_lite += "src/google/protobuf/parse_context.cc";
+    protobuf_lite += sw::Shared, "LIBPROTOBUF_EXPORTS"_d;
+    protobuf_lite.Public += sw::Shared, "PROTOBUF_USE_DLLS"_d;
+    if (protobuf_lite.getSettings().TargetOS.Type != OSType::Windows)
+        protobuf_lite.Public += "HAVE_PTHREAD"_d;
+
+    auto &protobuf = p.addTarget<LibraryTarget>("protobuf");
+    protobuf.ImportFromBazel = true;
+    if (protobuf.getSettings().TargetOS.Type != OSType::Windows)
+        protobuf.ExportAllSymbols = true;
+    protobuf.BazelNames.insert("protobuf_lite");
+    protobuf += "src/.*\\.h"_rr;
+    protobuf += "src/.*\\.inc"_rr;
+    protobuf += "src/google/protobuf/io/strtod.cc";
+    protobuf += "src/google/protobuf/parse_context.cc";
+    protobuf.Private += sw::Shared, "LIBPROTOBUF_EXPORTS"_d;
+    protobuf.Public += sw::Shared, "PROTOBUF_USE_DLLS"_d;
+    protobuf.Public += "org.sw.demo.madler.zlib"_dep;
+    if (protobuf.getSettings().TargetOS.Type != OSType::Windows)
+        protobuf.Public += "HAVE_PTHREAD"_d;
+
+    auto &protoc_lib = p.addTarget<LibraryTarget>("protoc_lib");
+    protoc_lib.ImportFromBazel = true;
+    if (protoc_lib.getSettings().TargetOS.Type != OSType::Windows)
+        protoc_lib.ExportAllSymbols = true;
+    protoc_lib.Private += sw::Shared, "LIBPROTOC_EXPORTS"_d;
+    protoc_lib.Public += sw::Shared, "PROTOBUF_USE_DLLS"_d;
+    protoc_lib.Public += protobuf;
+
+    auto &protoc = p.addTarget<ExecutableTarget>("protoc");
+    protoc.ImportFromBazel = true;
+    if (protoc.getSettings().TargetOS.Type != OSType::Windows)
+        protoc.ExportAllSymbols = true;
+    protoc +=
+        "src/google/protobuf/any.proto",
+        "src/google/protobuf/api.proto",
+        "src/google/protobuf/compiler/plugin.proto",
+        "src/google/protobuf/descriptor.proto",
+        "src/google/protobuf/duration.proto",
+        "src/google/protobuf/empty.proto",
+        "src/google/protobuf/field_mask.proto",
+        "src/google/protobuf/source_context.proto",
+        "src/google/protobuf/struct.proto",
+        "src/google/protobuf/timestamp.proto",
+        "src/google/protobuf/type.proto",
+        "src/google/protobuf/wrappers.proto";
+    protoc.Public += protoc_lib;
+}
