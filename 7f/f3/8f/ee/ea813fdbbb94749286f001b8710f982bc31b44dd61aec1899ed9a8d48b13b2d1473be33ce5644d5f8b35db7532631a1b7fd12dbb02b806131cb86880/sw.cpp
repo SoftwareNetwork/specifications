5b458629ee253dff59821e54db5cdd9eb26edceb
CppanBot <cppanbot@gmail.com>
2019-07-20 20:21:19 +0000

    Update.

---
+++ 7f/f3/8f/ee/ea813fdbbb94749286f001b8710f982bc31b44dd61aec1899ed9a8d48b13b2d1473be33ce5644d5f8b35db7532631a1b7fd12dbb02b806131cb86880/sw.cpp
@@ -0,0 +1,15 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<Library>("microsoft.mimalloc", "1.0.5");
+    t += Git("https://github.com/microsoft/mimalloc", "v{v}");
+
+    t += "include/.*"_rr;
+    t -= "src/.*"_rr;
+    t += "src/static.c";
+
+    t.Public += sw::Shared, "MI_SHARED_LIB"_def;
+    t += sw::Shared, "MI_SHARED_LIB_EXPORT"_def;
+
+    if (t.getSettings().TargetOS.is(OSType::Windows))
+        t += "Advapi32.lib"_slib;
+}
