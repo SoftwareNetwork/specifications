eda030e5d07663b0f8a0cdbb8670cd98febe2238
CppanBot <cppanbot@gmail.com>
2019-08-16 17:37:10 +0000

    Update.

--- 15/a0/48/59/72e93fc9194f4a1d757bf22ba5bd2fd3c76d4dbdfc55f9c2a251dc514d67411b3e474b1d447fd4431f9cdf046cf99debea8c98d442db0193da722a53/sw.cpp
+++ 15/a0/48/59/72e93fc9194f4a1d757bf22ba5bd2fd3c76d4dbdfc55f9c2a251dc514d67411b3e474b1d447fd4431f9cdf046cf99debea8c98d442db0193da722a53/sw.cpp
@@ -1,6 +1,6 @@
 void build(Solution &s)
 {
-    auto &nanopb = s.addTarget<StaticLibraryTarget>("nanopb", "0.3.9");
+    auto &nanopb = s.addTarget<StaticLibraryTarget>("nanopb", "0.3.9.1");
     nanopb += Git("https://github.com/nanopb/nanopb", "{v}");
     nanopb += "[^/]*\\.[hc]"_rr;
     nanopb.Public += "PB_FIELD_32BIT"_d;
