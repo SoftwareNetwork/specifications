44d5be8467df3a39933a1e6b2383c428d79a16d5
CppanBot <cppanbot@gmail.com>
2019-08-16 07:37:10 +0000

    Update.

---
+++ 6c/65/db/43/6c086ea856d448928251d27520101a622862e138b64bc2c533dc1721e7038b532bd42373c40720dfc493ff8bb0cb2eb6a54e6cabf40279f5baaf4fb6/sw.cpp
@@ -0,0 +1,12 @@
+void build(Build &b)
+{
+    auto &t = b.addStaticLibrary("google.protocolbuffers.upb.upb", "master");
+    t += Git("https://github.com/protocolbuffers/upb");
+
+    t.ImportFromBazel = true;
+
+    /*t += "upb/.*"_r;
+    t += "upb/bindings/std.*"_rr;
+    t += "upb/json/.*"_r;
+    t += "upb/pb/.*"_r;*/
+}
