6b4518d10a7b993e25506295237a99f981a88d1e
CppanBot <cppanbot@gmail.com>
2019-08-17 18:22:10 +0000

    Update.

--- 08/48/3f/a1/649bc5dfb7efe970f69faa332a7f9b7aec333e11318c69d39eb7456d2e9316f5af59bd344489c02d17d8abdf89d49c0732eb66a487a231e8c4c907b6/sw.cpp
+++ 08/48/3f/a1/649bc5dfb7efe970f69faa332a7f9b7aec333e11318c69d39eb7456d2e9316f5af59bd344489c02d17d8abdf89d49c0732eb66a487a231e8c4c907b6/sw.cpp
@@ -2,9 +2,10 @@ void build(Solution &s)
 {
     auto &p = s.addProject("howardhinnant.date", "2.4.1");
     p += Git("https://github.com/HowardHinnant/date", "v{v}");
-    
+
     auto &date = p.addTarget<StaticLibraryTarget>("date");
     date += "include/date/date.h";
-    
+
     auto &date_full = p.addTarget<StaticLibraryTarget>("date_full");
+    date_full += "org.sw.demo.badger.curl.libcurl"_dep;
 }
