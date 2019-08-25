da916073b2b492003cfbdd9b671ce182ed2563e5
CppanBot <cppanbot@gmail.com>
2019-08-25 20:34:51 +0000

    Update.

--- f2/fc/93/f8/ef6d412425854beabc1efb369b9d157a06b827ee6f06c8e9d125054ab04a25e0238fae74b596319ca8d5cc12337bba86edabdac98d90784a1a10d01b/sw.cpp
+++ f2/fc/93/f8/ef6d412425854beabc1efb369b9d157a06b827ee6f06c8e9d125054ab04a25e0238fae74b596319ca8d5cc12337bba86edabdac98d90784a1a10d01b/sw.cpp
@@ -71,6 +71,7 @@ void build(Solution &s)
             "pub.egorpugin.primitives.log-master"_dep;
         ProtobufData d;
         d.public_protobuf = true;
+        d.addIncludeDirectory(protos.SourceDir / "src");
         for (auto &[p, _] : protos["src/sw/protocol/.*\\.proto"_rr])
             gen_grpc_cpp("org.sw.demo.google.protobuf"_dep, "org.sw.demo.google.grpc.cpp.plugin"_dep, protos, p, d);
     }
@@ -244,14 +245,16 @@ void build(Solution &s)
         embed("pub.egorpugin.primitives.tools.embedder-master"_dep, client, "src/sw/client/inserts/inserts.cpp.in");
         if (client.getCompilerType() == CompilerType::MSVC)
             client.CompileOptions.push_back("-bigobj");
-        if (client.getBuildSettings().TargetOS.Type == OSType::Linux)
+        if (client.getBuildSettings().TargetOS.Type != OSType::Windows)
         {
             //client.getSelectedTool()->LinkOptions.push_back("-static-libstdc++");
             //client.getSelectedTool()->LinkOptions.push_back("-static-libgcc");
 
             // needed to export all shared symbols
             // so dlopen will work for plugins
-            client.LinkOptions.push_back("-Wl,-export-dynamic");
+            //client.LinkOptions.push_back("-Wl,--export-dynamic");
+            //client.LinkOptions.push_back("-Wl,-export-dynamic");
+            client.LinkOptions.push_back("-rdynamic");
         }
 
         {
