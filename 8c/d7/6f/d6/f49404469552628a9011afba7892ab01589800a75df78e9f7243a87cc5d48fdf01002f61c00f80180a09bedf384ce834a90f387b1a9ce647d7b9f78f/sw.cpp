feeaa5ae4e5ae497a3fb8b5ff9aef7463f2b6194
CppanBot <cppanbot@gmail.com>
2019-10-10 13:01:25 +0000

    Update.

---
+++ 8c/d7/6f/d6/f49404469552628a9011afba7892ab01589800a75df78e9f7243a87cc5d48fdf01002f61c00f80180a09bedf384ce834a90f387b1a9ce647d7b9f78f/sw.cpp
@@ -0,0 +1,25 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("libraw", "0.19.5");
+    t += RemoteFile("https://www.libraw.org/data/LibRaw-{v}.tar.gz");
+
+    t += "internal/.*"_rr;
+    t += "libraw/.*"_rr;
+    t += "src/.*"_rr;
+    t -= "src/libraw_fuji_compressed.cpp";
+    t -= "internal/libraw_x3f.cpp";
+    t -= "internal/.*_demosaic.*"_rr;
+    t -= "internal/wf_filtering.cpp";
+
+    t.Public += "."_idir;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t.Public += "WIN32"_def;
+
+        t += "ws2_32.lib"_slib;
+    }
+
+    t.Public += sw::Static, "LIBRAW_NODLL"_def;
+    t.Public += "LIBRAW_BUILDLIB"_def;
+}
---
+++ d6/66/88/50/6d782faabe9e66b512c2e4a3d793d18a27bf77078c477ffe81c169703abb07972e71a8f03069934d40392ddb4c98b62b9b7992ffa6fed3aa79552b7f/sw.cpp
@@ -0,0 +1,55 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("freeimage", "3.18.0");
+    p += RemoteFile("https://downloads.sourceforge.net/freeimage/FreeImage{M}{m}{p}.zip");
+
+    auto &t = p.addLibrary("freeimage");
+    t += "Source/.*"_r;
+    t += "Source/FreeImage.*"_rr;
+    t += "Source/Metadata/.*"_rr;
+
+    t += "org.sw.demo.openexr.IlmImf"_dep;
+    t += "org.sw.demo.glennrp.png"_dep;
+    t += "org.sw.demo.tiff"_dep;
+    t += "org.sw.demo.uclouvain.openjpeg.openjp2"_dep;
+    t += "org.sw.demo.webmproject.webp"_dep;
+    t += "org.sw.demo.jpeg"_dep;
+    t += "org.sw.demo.4creators.jxrlib"_dep;
+    t += "org.sw.demo.libraw"_dep;
+
+    t.patch("Source/FreeImage/PluginPNG.cpp", "../LibPNG/png.h", "png.h");
+    t.patch("Source/FreeImage/PluginPNG.cpp", "../ZLib/zlib.h", "zlib.h");
+
+    t.patch("Source/FreeImage/ZLibInterface.cpp", "../ZLib/zlib.h", "zlib.h");
+    t.patch("Source/FreeImage/ZLibInterface.cpp", "../ZLib/zutil.h", "zutil.h");
+
+    t.patch("Source/FreeImage/PluginG3.cpp", "../LibTIFF4/tiffiop.h", "tiffiop.h");
+    t.patch("Source/Metadata/XTIFF.cpp", "../LibTIFF4/tiffiop.h", "tiffiop.h");
+    t.patch("Source/FreeImage/PluginTIFF.cpp", "../LibTIFF4/tiffiop.h", "tiffiop.h");
+
+    t.patch("Source/FreeImage/J2KHelper.cpp", "../LibOpenJPEG/openjpeg.h", "openjpeg.h");
+    t.patch("Source/FreeImage/PluginJP2.cpp", "../LibOpenJPEG/openjpeg.h", "openjpeg.h");
+    t.patch("Source/FreeImage/PluginJ2K.cpp", "../LibOpenJPEG/openjpeg.h", "openjpeg.h");
+
+    t.patch("Source/FreeImage/PluginWebP.cpp", "../LibWebP/src/webp/decode.h", "webp/decode.h");
+    t.patch("Source/FreeImage/PluginWebP.cpp", "../LibWebP/src/webp/encode.h", "webp/encode.h");
+    t.patch("Source/FreeImage/PluginWebP.cpp", "../LibWebP/src/webp/mux.h", "webp/mux.h");
+
+    for (auto s : { "../OpenEXR/IlmImf/","../OpenEXR/Iex/","../OpenEXR/Half/", })
+    {
+        t.patch("Source/FreeImage/PluginEXR.cpp", s, "");
+        t.patch("Source/FreeImage/PluginTIFF.cpp", s, "");
+    }
+
+    for (auto s : { "Source/FreeImageToolkit/JPEGTransform.cpp","Source/FreeImage/PluginJPEG.cpp" })
+    {
+        t.patch(s, "../LibJPEG/jinclude.h", "jinclude.h");
+        t.patch(s, "../LibJPEG/jpeglib.h", "jpeglib.h");
+        t.patch(s, "../LibJPEG/jerror.h", "jerror.h");
+        t.patch(s, "../LibJPEG/transupp.h", "transupp.h");
+    }
+
+    t.patch("Source/FreeImage/PluginJXR.cpp", "../LibJXR/jxrgluelib/JXRGlue.h", "jxrgluelib/JXRGlue.h");
+
+    t.patch("Source/FreeImage/PluginRAW.cpp", "../LibRawLite/libraw/libraw.h", "libraw/libraw.h");
+}
