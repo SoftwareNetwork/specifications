d0cb55df88771b9f63fbd2fd71e0c3ee6e4d8390
CppanBot <cppanbot@gmail.com>
2019-10-10 12:01:27 +0000

    Update.

---
+++ 0b/0e/13/5f/0dcb77bbfc05fa11f64373d47af5507366c7d5d9dafa98242c7a2a8a67870aeb9c1009816a0984d2cb32bc40472a1f6f7903e06a123bb64e6afa830f/sw.cpp
@@ -0,0 +1,166 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("intel", "2019.9.0");
+    p += RemoteFile("https://github.com/01org/tbb/archive/{M}_U{m}.tar.gz");
+
+    String arch;
+
+    auto make_ver = [](auto &t)
+    {
+        t.writeFileOnce("version_string.ver", R"(#define __TBB_VERSION_STRINGS(N) \
+#N": BUILD_HOST         @CMAKE_SYSTEM_NAME@" ENDL \
+#N": BUILD_OS           @CMAKE_SYSTEM@" ENDL \
+#N": BUILD_KERNEL       @CMAKE_SYSTEM_VERSION@" ENDL \
+#N": BUILD_GCC          @CMAKE_CXX_COMPILER_ID@" ENDL \
+#N": BUILD_LIBC         Unknown" ENDL \
+#N": BUILD_LD           Unknown" ENDL \
+#N": BUILD_TARGET       Unknown" ENDL \
+#N": BUILD_COMMAND      Unknown" ENDL
+
+#define __TBB_DATETIME "@_configure_date@"
+)");
+    };
+
+    auto make_def = [&s](auto &t, const String &name, const String &arch)
+    {
+        auto cc = std::static_pointer_cast<VisualStudioCompiler>(t.findProgramByExtension(".cpp")->clone());
+        cc->CompileAsC = true;
+        cc->PreprocessToStdout = true;
+        cc->IncludeDirectories.insert(t.SourceDir / "include");
+        cc->InputFile = t.SourceDir / ("src/" + name + "/" + arch + "-" + name + "-export.def");
+
+        auto c = cc->getCommand(t);
+        c->redirectStdout(t.BinaryDir / ("" + name + ".def"));
+        t.Storage.push_back(c);
+        t += t.BinaryDir / ("" + name + ".def");
+    };
+
+    auto make_def_simple = [&s](auto &t, const String &name, const String &arch)
+    {
+        t -= "src/.*\\.def"_rr;
+        t += path("src/" + name + "/" + arch + "-" + name + "-export.def");
+    };
+
+    auto &tbb = p.addTarget<LibraryTarget>("tbb");
+    {
+        tbb +=
+            "include/.*"_rr,
+            "src/old/.*"_rr,
+            "src/old/.*\\.h"_rr,
+            "src/rml/.*\\.h"_rr,
+            "src/rml/client/rml_tbb.cpp",
+            "src/tbb/.*\\.asm"_rr,
+            "src/tbb/.*\\.cpp"_rr,
+            "src/tbb/.*\\.def"_rr,
+            "src/tbb/.*\\.h"_rr,
+            "src/tbb/.*\\.lst"_rr;
+        tbb ^= "include/tbb/tbbmalloc_proxy.h";
+
+        tbb.Private +=
+            "src"_id,
+            "src/rml/include"_id;
+
+        tbb.Public += "include"_id;
+
+        tbb -= "src/tbb/.*\\.asm"_rr;
+        tbb -= "src/tbb/.*\\.def"_rr;
+        tbb -= "src/.*test.*"_rr;
+
+        tbb.Private += "__TBB_BUILD"_d;
+        tbb.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        tbb.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        if (tbb.getBuildSettings().TargetOS.Type != OSType::Windows)
+        {
+            tbb.Public += "USE_PTHREAD"_d;
+        }
+        if (tbb.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            arch += "win";
+            if (tbb.getBuildSettings().TargetOS.is(ArchType::x86_64))
+            {
+                tbb += "src/tbb/intel64-masm/.*"_rr;
+                arch += "64";
+            }
+            else
+            {
+                tbb += "src/tbb/ia32-masm/.*"_rr;
+                arch += "32";
+            }
+            tbb.Public += "USE_WINTHREAD"_d;
+
+            make_def(tbb, "tbb", arch);
+        }
+
+        make_ver(tbb);
+    }
+
+    auto &tbbmalloc = tbb.addTarget<SharedLibraryTarget>("malloc");
+    {
+        tbbmalloc +=
+            "include/.*"_rr,
+            "src/tbb/.*\\.h"_rr,
+            "src/tbbmalloc/.*\\.cpp"_rr,
+            "src/tbbmalloc/.*\\.def"_rr,
+            "src/tbbmalloc/.*\\.h"_rr;
+
+        tbbmalloc -=
+            "src/tbbmalloc/proxy.*"_rr,
+            "src/tbbmalloc/tbb_function_replacement.*"_rr;
+
+        // disable autolink
+        tbbmalloc.patch("include/tbb/tbbmalloc_proxy.h", "#pragma comment", "//#pragma  comment");
+
+        tbbmalloc.Private += "src"_id;
+        tbbmalloc.Public += "include"_id;
+
+        tbbmalloc.Private += "__TBBMALLOC_BUILD"_d;
+        tbbmalloc.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        tbbmalloc.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        if (tbbmalloc.getBuildSettings().Native.LibrariesType == LibraryType::Static)
+            tbbmalloc += "__TBB_SOURCE_DIRECTLY_INCLUDED"_def;
+        if (tbbmalloc.getBuildSettings().TargetOS.Type != OSType::Windows)
+        {
+            tbbmalloc.Public += "USE_PTHREAD"_d;
+        }
+        if (tbbmalloc.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            tbbmalloc.Public += "USE_WINTHREAD"_d;
+
+            make_def_simple(tbbmalloc, "tbbmalloc", arch);
+        }
+
+        make_ver(tbbmalloc);
+    }
+
+    auto &tbbmalloc_proxy = tbbmalloc.addTarget<SharedLibraryTarget>("proxy");
+    {
+        tbbmalloc_proxy +=
+            "include/.*"_rr,
+            "src/tbb/.*\\.h"_rr,
+            "src/tbbmalloc/.*\\.h"_rr,
+            "src/tbbmalloc/proxy.*"_rr,
+            "src/tbbmalloc/tbb_function_replacement.*"_rr;
+        tbbmalloc_proxy ^= "include/tbb/tbbmalloc_proxy.h";
+
+        tbbmalloc_proxy.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        tbbmalloc_proxy.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        if (tbbmalloc_proxy.getBuildSettings().TargetOS.Type != OSType::Windows)
+        {
+            tbbmalloc_proxy.Public += "USE_PTHREAD"_d;
+        }
+        if (tbbmalloc_proxy.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            tbbmalloc_proxy.Public += "USE_WINTHREAD"_d;
+        }
+
+        tbbmalloc_proxy.Public += tbbmalloc;
+
+        if (tbbmalloc_proxy.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            if (tbbmalloc_proxy.getBuildSettings().TargetOS.is(ArchType::x86_64))
+                tbbmalloc_proxy.Interface.LinkOptions.push_back("-INCLUDE:__TBB_malloc_proxy");
+            else if (tbbmalloc_proxy.getBuildSettings().TargetOS.is(ArchType::x86))
+                tbbmalloc_proxy.Interface.LinkOptions.push_back("-INCLUDE:___TBB_malloc_proxy");
+        }
+    }
+}
---
+++ 61/7c/8f/1a/d4b2a98377da3fc82e105c66c2f0567bc10153562cc0da1f2f68673a61ef383e9061a9afbb18e8f4341877674b320d1cd0da97db111a4bf4e4cdd608/sw.cpp
@@ -0,0 +1,22 @@
+void build(Solution &s)
+{
+    auto &jxrlib = s.addTarget<StaticLibraryTarget>("4creators.jxrlib", "2019.10.9");
+    jxrlib += Git("https://github.com/4creators/jxrlib", "v{v}");
+
+    jxrlib +=
+        "common/include/.*"_rr,
+        "image/decode/.*"_rr,
+        "image/encode/.*"_rr,
+        "image/sys/.*"_rr,
+        "image/x86/.*"_rr,
+        "jxrgluelib/.*\\.[hc]"_rr;
+
+    jxrlib.Public +=
+        "common/include"_idir,
+        "jxrgluelib"_idir,
+        "image/sys"_idir;
+    jxrlib.Public += "."_idir;
+
+    if (jxrlib.getBuildSettings().TargetOS.Type == OSType::Windows)
+        jxrlib.Public += "WIN32"_def;
+}
