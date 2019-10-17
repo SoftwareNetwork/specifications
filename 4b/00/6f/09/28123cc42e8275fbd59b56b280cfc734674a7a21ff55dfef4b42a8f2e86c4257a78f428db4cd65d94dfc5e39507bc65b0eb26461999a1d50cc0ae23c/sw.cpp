3abbaab169d41eff5d7da2cc68d4df675f7beb52
CppanBot <cppanbot@gmail.com>
2019-10-17 23:11:25 +0000

    Update.

---
+++ 4b/00/6f/09/28123cc42e8275fbd59b56b280cfc734674a7a21ff55dfef4b42a8f2e86c4257a78f428db4cd65d94dfc5e39507bc65b0eb26461999a1d50cc0ae23c/sw.cpp
@@ -0,0 +1,11 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("google.gumbo_parser", "0.10.1");
+    t += Git("https://github.com/google/gumbo-parser", "v{v}");
+
+    t += "src/.*"_rr;
+    t += "visualc/include/.*"_rr;
+
+    t.Public += "src"_idir;
+    t += "visualc/include"_idir;
+}
---
+++ 96/5f/c4/3a/95e1637b44ee01ce4e5f2298ec09b2e68e398c198238cb4126caca540da12efd128b9afd0605971f7e9cd1e5bc55fa30133819ef61a1096f98690b87/sw.cpp
@@ -0,0 +1,35 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("amazon.awslabs.c_common", "0.4.13");
+    t += Git("https://github.com/awslabs/aws-c-common", "v{v}");
+
+    t +=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t -=
+        "source/windows/.*"_rr,
+        "source/posix/.*"_rr;
+    t -= "source/arch/encoding_avx2.c";
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (t.getSettings().Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_COMMON_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_COMMON_USE_IMPORT_EXPORT"_d;
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t += "source/windows/.*"_rr;
+        t.Public += "USE_WINDOWS_DLL_SEMANTICS"_d;
+        t.Public += "WIN32"_d;
+        t += "HAVE_MSVC_CPUIDEX"_d;
+        t.Public += "bcrypt.lib"_lib;
+    }
+    else
+    {
+        t += "source/posix/.*"_rr;
+        t += "HAVE_BUILTIN_CPU_SUPPORTS"_d;
+    }
+    t.writeFileOnce("aws/common/config.h");
+}
