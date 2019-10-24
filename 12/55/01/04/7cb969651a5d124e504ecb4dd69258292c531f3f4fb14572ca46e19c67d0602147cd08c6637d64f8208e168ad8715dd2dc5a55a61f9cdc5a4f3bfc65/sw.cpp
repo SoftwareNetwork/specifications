6a187d7cdbb0933b6d4cb767d9847960a3f4b192
CppanBot <cppanbot@gmail.com>
2019-10-24 18:11:26 +0000

    Update.

---
+++ 12/55/01/04/7cb969651a5d124e504ecb4dd69258292c531f3f4fb14572ca46e19c67d0602147cd08c6637d64f8208e168ad8715dd2dc5a55a61f9cdc5a4f3bfc65/sw.cpp
@@ -0,0 +1,10 @@
+void build(Solution &s)
+{
+    auto &catch2 = s.addTarget<LibraryTarget>("catchorg.catch2", "2.10.2");
+    catch2 += Git("https://github.com/catchorg/Catch2", "v{v}");
+
+    catch2 +=
+        "single_include/.*"_rr;
+    catch2.Public +=
+        "single_include"_id;
+}
