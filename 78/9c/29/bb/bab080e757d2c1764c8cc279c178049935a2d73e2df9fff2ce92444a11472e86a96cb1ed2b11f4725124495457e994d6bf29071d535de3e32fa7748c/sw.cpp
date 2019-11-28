c388435ea7fd36133412f40e73d222246f88dfa1
CppanBot <cppanbot@gmail.com>
2019-11-28 23:38:32 +0000

    Update.

--- 78/9c/29/bb/bab080e757d2c1764c8cc279c178049935a2d73e2df9fff2ce92444a11472e86a96cb1ed2b11f4725124495457e994d6bf29071d535de3e32fa7748c/sw.cpp
+++ 78/9c/29/bb/bab080e757d2c1764c8cc279c178049935a2d73e2df9fff2ce92444a11472e86a96cb1ed2b11f4725124495457e994d6bf29071d535de3e32fa7748c/sw.cpp
@@ -4,6 +4,8 @@ void build(Solution &s)
     fmt += Git("https://github.com/fmtlib/fmt", "{v}");
     //fmt.ExportAllSymbols = true;
 
+    fmt += cpp11;
+
     fmt.Private += sw::Shared, "FMT_EXPORT"_d;
     fmt.Public += sw::Shared, "FMT_SHARED"_d;
 
