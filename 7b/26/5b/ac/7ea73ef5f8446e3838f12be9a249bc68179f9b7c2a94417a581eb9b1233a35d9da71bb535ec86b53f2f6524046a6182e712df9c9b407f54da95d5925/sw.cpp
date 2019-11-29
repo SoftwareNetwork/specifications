dd2510b7a2ef393cb05bfee661c44772be07de81
CppanBot <cppanbot@gmail.com>
2019-11-29 15:38:33 +0000

    Update.

--- 7b/26/5b/ac/7ea73ef5f8446e3838f12be9a249bc68179f9b7c2a94417a581eb9b1233a35d9da71bb535ec86b53f2f6524046a6182e712df9c9b407f54da95d5925/sw.cpp
+++ 7b/26/5b/ac/7ea73ef5f8446e3838f12be9a249bc68179f9b7c2a94417a581eb9b1233a35d9da71bb535ec86b53f2f6524046a6182e712df9c9b407f54da95d5925/sw.cpp
@@ -3,6 +3,8 @@ void build(Solution &s)
     auto &pqxx = s.addTarget<LibraryTarget>("jtv.pqxx", "6.4.5");
     pqxx += Git("https://github.com/jtv/libpqxx", "{v}");
 
+    pqxx.setChecks("pqxx", true);
+
     pqxx.CPPVersion = CPPLanguageStandard::CPP17;
     pqxx.ApiName = "PQXX_LIBEXPORT";
 
@@ -14,4 +16,10 @@ void build(Solution &s)
     pqxx.writeFileOnce("pqxx/config-public-compiler.h");
     pqxx.writeFileOnce("pqxx/config-internal-libpq.h");
     pqxx.writeFileOnce("pqxx/config-internal-compiler.h");
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("pqxx");
+    s.checkFunctionExists("poll");
 }
