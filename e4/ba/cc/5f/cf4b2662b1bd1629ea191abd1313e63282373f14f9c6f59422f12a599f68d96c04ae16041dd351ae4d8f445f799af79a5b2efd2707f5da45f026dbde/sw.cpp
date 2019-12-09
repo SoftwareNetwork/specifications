39093cecbb8862b5c5dc733c830da12c45d3c104
CppanBot <cppanbot@gmail.com>
2019-12-09 12:16:54 +0000

    Update.

--- e4/ba/cc/5f/cf4b2662b1bd1629ea191abd1313e63282373f14f9c6f59422f12a599f68d96c04ae16041dd351ae4d8f445f799af79a5b2efd2707f5da45f026dbde/sw.cpp
+++ e4/ba/cc/5f/cf4b2662b1bd1629ea191abd1313e63282373f14f9c6f59422f12a599f68d96c04ae16041dd351ae4d8f445f799af79a5b2efd2707f5da45f026dbde/sw.cpp
@@ -3,11 +3,13 @@ void build(Solution &s)
     auto &t = s.addStaticLibrary("litehtml", "master");
     t += Git("https://github.com/litehtml/litehtml");
 
-    t.Public += "include"_idir;
-    t += "include/litehtml"_idir;
-
     t += "include/.*"_rr;
     t += "src/.*"_r;
+
+    t.Public += "LITEHTML_UTF8"_def;
+
+    t.Public += "include"_idir;
+    t += "include/litehtml"_idir;
 
     t.Public += "org.sw.demo.google.gumbo_parser"_dep;
 }
