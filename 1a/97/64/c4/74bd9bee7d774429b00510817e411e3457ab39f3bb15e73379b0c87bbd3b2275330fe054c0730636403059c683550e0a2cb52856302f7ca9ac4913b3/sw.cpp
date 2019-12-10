0e27f48b710b7c7346c9cbb5ba88f940b963c856
CppanBot <cppanbot@gmail.com>
2019-12-10 00:16:57 +0000

    Update.

---
+++ 1a/97/64/c4/74bd9bee7d774429b00510817e411e3457ab39f3bb15e73379b0c87bbd3b2275330fe054c0730636403059c683550e0a2cb52856302f7ca9ac4913b3/sw.cpp
@@ -0,0 +1,10 @@
+void build(Solution &s)
+{
+    const String uv_ver = "1.31";
+    auto uv = "org.sw.demo.libuv"_dep;
+    uv->package.range = uv_ver;
+
+    auto &p = s.addLibrary("skypjack.uvw", "2.0.0");
+    p += Git("https://github.com/skypjack/uvw", "v{v}_libuv-v" + uv_ver);
+    p.Public += uv;
+}
