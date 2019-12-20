b273e915c2829652943edbd79ca711d72bd9949c
CppanBot <cppanbot@gmail.com>
2019-12-20 17:16:58 +0000

    Update.

--- c1/34/30/93/7ef863dc59d03089bcae2bcd57610c976f82cd7187255c10752be8dd923050acab652f1e2abf992896e9d4fd4d94b1969c510620b3a78e17eb570ffd/sw.cpp
+++ c1/34/30/93/7ef863dc59d03089bcae2bcd57610c976f82cd7187255c10752be8dd923050acab652f1e2abf992896e9d4fd4d94b1969c510620b3a78e17eb570ffd/sw.cpp
@@ -1,8 +1,9 @@
 void build(Solution &s)
 {
-    auto &sqlite3 = s.addTarget<LibraryTarget>("sqlite3", "3.30.1");
-    sqlite3 += RemoteFile("http://www.sqlite.org/2019/sqlite-amalgamation-{M}{m:02}{p:02}00.zip");
-    sqlite3 += "sqlite3.[hc]"_r;
-    sqlite3 += "sqlite3ext.h";
-    sqlite3.ApiName = "SQLITE_API";
+    auto &t = s.addTarget<LibraryTarget>("sqlite3", "3.30.1");
+    t += RemoteFile("http://www.sqlite.org/2019/sqlite-amalgamation-{M}{m:02}{p:02}00.zip");
+    t += "sqlite3.[hc]"_r;
+    t += "sqlite3ext.h";
+    t.ApiName = "SQLITE_API";
+    t += "SQLITE_ENABLE_COLUMN_METADATA"_def;
 }
