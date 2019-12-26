57fbbf8b074b5f72a0a4d25ee27b577fdec4d473
CppanBot <cppanbot@gmail.com>
2019-12-26 21:18:31 +0000

    Update.

---
+++ cb/f0/7b/7a/5955a83fe3c219c179fdb11356fe8cd1ea63cb0512eb5feda1116f65a18922c4faccb1aaedc5d7448d271f9a1822cc822191888c7a9e8ade615248a2/sw.cpp
@@ -0,0 +1,42 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("Microsoft", "master");
+    p += Git("https://github.com/microsoft/STL");
+
+    auto &stl = p.addLibrary("STL");
+    {
+        stl.setRootDirectory("stl");
+
+        stl -= "src/locale_implib_pure.cpp";
+        stl -= "src/locale0_implib.cpp";
+        stl -= "src/mpiostream.cpp";
+        stl -= "src/mexcptptr.cpp";
+        stl -= "src/ulocale.cpp";
+
+        stl += cpp17;
+
+        stl += "_CRTBLD"_def;
+        stl += sw::Shared, "CRTDLL2"_def;
+        stl += sw::Static, "_STATIC_CPPLIB"_def;
+
+        stl += "_VCRT_ALLOW_INTERNALS"_def;
+        stl += "_HAS_OLD_IOSTREAMS_MEMBERS=1"_def;
+        stl += "_STL_CONCRT_SUPPORT"_def;
+
+        if (stl.getBuildSettings().TargetOS.is(ArchType::x86_64))
+            stl += "_AMD64_"_def;
+        else if (stl.getBuildSettings().TargetOS.is(ArchType::x86))
+            stl += "_X86_"_def;
+        else if (stl.getBuildSettings().TargetOS.is(ArchType::aarch64))
+            stl += "_ARM64_"_def;
+        else if (stl.getBuildSettings().TargetOS.is(ArchType::arm))
+            stl += "_ARM_"_def;
+        else
+            throw SW_RUNTIME_ERROR("Unsupported arch");
+
+        stl += "com.Microsoft.VisualStudio.VC.concrt"_dep;
+        stl += "com.Microsoft.VisualStudio.VC.runtime"_dep;
+        stl += "com.Microsoft.Windows.SDK.winrt"_dep;
+        stl += "org.sw.demo.boost.math"_dep;
+    }
+}
