3e9b666cf6756f8e6959cbfa644db8cec764f1a9
CppanBot <cppanbot@gmail.com>
2020-01-03 18:18:32 +0000

    Update.

---
+++ 0c/9c/fc/27/f883a1eecc8ecfcd8e5c4bc5c0a0d4f9b33e18feadb780e4237b11473036c67ac203cdc29ab4db36c3621dc437a9279136aee6ab6f1fb5996c31c08f/sw.cpp
@@ -0,0 +1,21 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("libusb", "1.0.23");
+    t += Git("https://github.com/libusb/libusb", "v{v}");
+    {
+        t += "libusb/.*"_r;
+        t -= "libusb/os/.*"_rr;
+        t ^= "libusb/libusb-1.0.rc";
+        t -= "msvc/.*\\.h"_rr;
+
+        if (t.getBuildSettings().TargetOS.is(OSType::Windows))
+        {
+            t += "msvc"_idir;
+            t += "libusb/os/.*windows.*"_rr;
+            t += "Advapi32.lib"_slib;
+            t.patch("libusb/libusb-1.0.def", "LIBRARY \"libusb-1.0.dll\"", "LIBRARY");
+        }
+
+        t.Public += "libusb"_idir;
+    }
+}
