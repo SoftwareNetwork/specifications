52a875e8e1bee65a6d08e944d12f8b3157c28138
CppanBot <cppanbot@gmail.com>
2020-01-17 13:05:20 +0000

    Update.

--- 6b/b8/7a/05/3beced3e11cd507c656e9ca5052d6f2b024bb36c05854459382e8c8e839e9d44b5282410e6333491fa75f8a49cd7cbc546b49d623c316ae49e79431d/sw.cpp
+++ 6b/b8/7a/05/3beced3e11cd507c656e9ca5052d6f2b024bb36c05854459382e8c8e839e9d44b5282410e6333491fa75f8a49cd7cbc546b49d623c316ae49e79431d/sw.cpp
@@ -9,8 +9,10 @@ void build(Solution &s)
         t +=
             FileRegex("include/SFML/" + name, ".*", true),
             FileRegex("src/SFML/" + name, ".*", false);
-        if (s.Settings.TargetOS.is(OSType::Windows))
+        t.AllowEmptyRegexes = true;
+        if (t.getBuildSettings().TargetOS.is(OSType::Windows))
             t += FileRegex("src/SFML/" + name + "/Win32", ".*", true);
+        t.AllowEmptyRegexes = false;
         t += path("include/SFML/" + name + ".hpp");
         t.Private += sw::Shared, Definition("SFML_" + boost::to_upper_copy(name) + "_EXPORTS");
         t.Public += sw::Static, "SFML_STATIC"_d;
@@ -20,38 +22,40 @@ void build(Solution &s)
     auto &system = add_library("System");
     system += "include/SFML/Config.hpp";
     system += "include/SFML/Main.hpp";
-    if (s.Settings.TargetOS.is(OSType::Windows))
+    if (system.getBuildSettings().TargetOS.is(OSType::Windows))
         system.Protected += "winmm.lib"_slib;
 
     auto &window = add_library("Window");
     window += "include/SFML/OpenGL.hpp";
     window += "include/SFML/GpuPreference.hpp";
-    if (s.Settings.TargetOS.is(OSType::Windows))
+    if (window.getBuildSettings().TargetOS.is(OSType::Windows))
     {
         window.Protected += "UNICODE"_def;
-        window.Protected += "gdi32.lib"_slib,
+        window.Protected +=
+            "gdi32.lib"_slib,
             "user32.lib"_slib,
             "Opengl32.lib"_slib,
             "ws2_32.lib"_slib,
-            "advapi32.lib"_slib;
+            "advapi32.lib"_slib
+            ;
     }
     window -= "src/SFML/Window/E[gG][lL].*"_r;
     window.Public += system;
 
     auto &network = add_library("Network");
-    if (s.Settings.TargetOS.is(OSType::Windows))
+    if (network.getBuildSettings().TargetOS.is(OSType::Windows))
         network.Protected += "ws2_32.lib"_slib;
     network.Public += system;
 
     auto &audio = add_library("Audio");
     audio.Public += system;
-    audio += "org.sw.demo.kcat.openal-1"_dep;
-    audio += "org.sw.demo.xiph.vorbis.vorbisfile-1"_dep;
-    audio += "org.sw.demo.xiph.flac.libflac-1"_dep;
+    audio += "org.sw.demo.kcat.openal"_dep;
+    audio += "org.sw.demo.xiph.vorbis.vorbisfile"_dep;
+    audio += "org.sw.demo.xiph.flac.libflac"_dep;
 
     auto &graphics = add_library("Graphics");
     graphics.Public += window;
-    graphics += "org.sw.demo.freetype-*"_dep;
+    graphics += "org.sw.demo.freetype"_dep;
     graphics += "org.sw.demo.stb.image-master"_dep;
     graphics += "org.sw.demo.stb.image_write-master"_dep;
 }
