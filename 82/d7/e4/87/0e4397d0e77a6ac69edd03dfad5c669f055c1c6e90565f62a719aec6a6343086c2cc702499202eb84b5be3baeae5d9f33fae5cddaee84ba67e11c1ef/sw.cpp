1dc4b72b3b5784552016ba7c7b42e314b60bc3aa
CppanBot <cppanbot@gmail.com>
2020-01-18 17:05:22 +0000

    Update.

---
+++ 82/d7/e4/87/0e4397d0e77a6ac69edd03dfad5c669f055c1c6e90565f62a719aec6a6343086c2cc702499202eb84b5be3baeae5d9f33fae5cddaee84ba67e11c1ef/sw.cpp
@@ -0,0 +1,21 @@
+void build(Solution &s)
+{
+    auto &imgui = s.addTarget<LibraryTarget>("ocornut.imgui", "1.73.0");
+    imgui += Git("https://github.com/ocornut/imgui", "v{M}.{m}");
+
+    imgui.ApiNames.insert("IMGUI_API");
+    imgui +=
+        "examples/imgui_impl_glfw.*"_rr,
+        "examples/imgui_impl_opengl3.*"_rr,
+        "examples/imgui_impl_win32.*"_rr,
+        "im.*"_rr;
+
+    imgui -=
+        "examples/.*"_rr;
+    imgui.Public +=
+        "examples"_id;
+
+    imgui.Public += "org.sw.demo.stb.all-master"_dep;
+    imgui.Public += "org.sw.demo.glfw"_dep;
+    //imgui.Public += "org.sw.demo.gl3w-master"_dep;
+}
