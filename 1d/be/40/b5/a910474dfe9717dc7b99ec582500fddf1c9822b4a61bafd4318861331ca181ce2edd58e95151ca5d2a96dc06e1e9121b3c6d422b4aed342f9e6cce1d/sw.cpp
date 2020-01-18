cf821a3f8bf1a0f03c1645c75b52d16e5cf5229f
CppanBot <cppanbot@gmail.com>
2020-01-18 23:05:21 +0000

    Update.

---
+++ 1d/be/40/b5/a910474dfe9717dc7b99ec582500fddf1c9822b4a61bafd4318861331ca181ce2edd58e95151ca5d2a96dc06e1e9121b3c6d422b4aed342f9e6cce1d/sw.cpp
@@ -0,0 +1,46 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("xorg.pciaccess", "0.16.0");
+    t += Git("https://gitlab.freedesktop.org/xorg/lib/libpciaccess", "libpciaccess-0.16");
+
+    t.setChecks("x", true);
+
+    t += "include/.*"_rr;
+    t += "src/.*"_rr;
+
+    t -= "src/freebsd_pci.c";
+    t -= "src/netbsd_pci.c";
+    t -= "src/hurd_pci.c";
+    t -= "src/openbsd_pci.c";
+    t -= "src/solx_devfs.c";
+    t -= "src/x86_pci.c"; // cygwin
+
+    t -= "src/common_vgaarb_stub.c";
+
+    t += "PCIIDS_PATH=\".\""_def;
+
+    t += "HAVE_CONFIG_H"_def;
+    t.writeFileOnce(t.BinaryPrivateDir / "config.h", R"(
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+    )");
+
+    t.LinkOptions.push_back("--no-undefined");
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("x");
+    s.checkIncludeExists("err.h");
+    s.checkIncludeExists("machine/sysarch.h");
+    s.checkIncludeExists("machine/mtrr.h");
+    s.checkIncludeExists("asm/mtrr.h");
+
+    s.checkIncludeExists("string.h");
+    s.checkIncludeExists("strings.h");
+    s.checkIncludeExists("inttypes.h");
+    s.checkIncludeExists("stdint.h");
+    s.checkIncludeExists("stdlib.h");
+    s.checkIncludeExists("stddef.h");
+}
