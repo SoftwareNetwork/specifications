5daac352b19284abd5f53551a347a7ac32c4804f
CppanBot <cppanbot@gmail.com>
2020-01-19 14:05:25 +0000

    Update.

---
+++ 68/f5/e4/84/e8af697caa6f086dfcf31a1abcd2b5e86da69c7655c4c8997a70c6f65160122cc894f9e525a3f2f1011d50b4213db0215453f5ede0548800da592bb6/sw.cpp
@@ -0,0 +1,74 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<Library>("anholt.libepoxy", "1.5.4");
+    t += Git("https://github.com/anholt/libepoxy");
+
+    t.ApiName = "EPOXY_PUBLIC";
+    t += "include/.*"_rr;
+    t += "registry/.*"_rr;
+    t += "src/.*"_rr;
+
+    t.writeFileOnce(t.BinaryPrivateDir / "config.h");
+
+    auto output_dir = t.BinaryDir / "epoxy";
+
+    // hdr
+    for (auto [gs, r, deps] : {
+        std::tuple{"gl_generated.h","gl",Strings{"gl.h"}},
+        std::tuple{"egl_generated.h","egl",Strings{"egl.h"}},
+        std::tuple{"glx_generated.h","glx",Strings{"glx.h"}},
+        std::tuple{"wgl_generated.h","wgl",Strings{"wgl.h"}},
+        })
+    {
+        if (t.getBuildSettings().TargetOS.is(OSType::Windows) || t.getBuildSettings().TargetOS.is(OSType::Macos))
+        {
+            if (r == "egl" || r == "glx")
+                continue;
+        }
+        auto c = t.addCommand();
+        c << cmd::prog("org.sw.demo.python.exe-3"_dep)
+            << cmd::in("src/gen_dispatch.py")
+            << "--header"
+            << "--no-source"
+            << ("--outputdir="s + output_dir.u8string())
+            << cmd::in("registry/"s + r + ".xml")
+            << cmd::end()
+            << cmd::out(output_dir / gs)
+            ;
+        for (auto &f : deps)
+            c << cmd::in("include/epoxy/"s + f);
+    }
+
+    // src
+    for (auto [gs, r, deps] : {
+        std::tuple{"gl_generated_dispatch.c","gl",Strings{"dispatch_common.c","dispatch_common.h"}},
+        std::tuple{"egl_generated_dispatch.c","egl",Strings{"dispatch_egl.c"}},
+        std::tuple{"glx_generated_dispatch.c","glx",Strings{"dispatch_glx.c"}},
+        std::tuple{"wgl_generated_dispatch.c","wgl",Strings{"dispatch_wgl.c"}},
+        })
+    {
+        if (t.getBuildSettings().TargetOS.is(OSType::Windows) || t.getBuildSettings().TargetOS.is(OSType::Macos))
+        {
+            if (r == "egl" || r == "glx")
+                continue;
+        }
+        auto c = t.addCommand();
+        c << cmd::prog("org.sw.demo.python.exe-3"_dep)
+            << cmd::in("src/gen_dispatch.py")
+            << "--source"
+            << "--no-header"
+            << ("--outputdir="s + output_dir.u8string())
+            << cmd::in("registry/"s + r + ".xml")
+            << cmd::end()
+            << cmd::out(output_dir / gs)
+            ;
+        for (auto &f : deps)
+            c << cmd::in("src/"s + f);
+    }
+
+    if (t.getBuildSettings().TargetOS.is(OSType::Windows) || t.getBuildSettings().TargetOS.is(OSType::Macos))
+    {
+        t -= ".*egl.*"_rr;
+        t -= ".*glx.*"_rr;
+    }
+}
---
+++ b4/b2/3a/21/94893dd4c8b1603cdd8e233e5629dd29890c585aa64ea3db11952ba79ef647804ffb7ec6c2f4bdb11254995dd13402d848c28cc771c9f51326be7a52/sw.cpp
@@ -0,0 +1,46 @@
+void build(Solution &s)
+{
+    // sdl cannot load vk as static lib
+    auto &t = s.addTarget<SharedLibrary>("khronos.vulkan", "1.2.131");
+    t += Git("https://github.com/KhronosGroup/Vulkan-Loader", "v{v}");
+
+    t.setChecks("vk");
+
+    t += "loader/.*"_r;
+    t += "scripts/.*"_r;
+    t -= "loader/generated/.*"_rr;
+    t -= "loader/asm_offset.c";
+    t -= "loader/.*\\.asm"_r;
+
+    t += "loader"_idir;
+    t += "loader/generated"_idir;
+
+    t += "API_NAME=\"Vulkan\""_def;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t += "VK_USE_PLATFORM_WIN32_KHR"_def;
+
+        t += "Cfgmgr32.lib"_slib;
+        t += "advapi32.lib"_slib;
+
+        t.patch("loader/vulkan-1.def", "LIBRARY vulkan-1.dll", "");
+
+        // only when shared
+        t.Interface += Definition("SW_VK_LIBRARY_NAME=\"" + t.getOutputFile().filename().u8string() + "\"");
+    }
+
+    auto hdrs = "org.sw.demo.khronos.vulkan.headers"_dep;
+    hdrs->package.range = t.getPackage().getVersion();
+    t += hdrs; // add to pvt because dummy dep is addded below
+    t.Public += hdrs;
+
+    t.configureFile("loader/loader_cmake_config.h.in", "loader_cmake_config.h");
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("vk");
+    s.checkFunctionExists("secure_getenv");
+    s.checkFunctionExists("__secure_getenv");
+}
