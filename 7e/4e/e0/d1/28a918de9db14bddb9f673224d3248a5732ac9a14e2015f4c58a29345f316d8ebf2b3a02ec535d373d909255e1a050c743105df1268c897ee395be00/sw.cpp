301ba1ce370c5b9a2a398ef59be5bd6499e1e07d
CppanBot <cppanbot@gmail.com>
2020-01-20 08:05:19 +0000

    Update.

---
+++ 7e/4e/e0/d1/28a918de9db14bddb9f673224d3248a5732ac9a14e2015f4c58a29345f316d8ebf2b3a02ec535d373d909255e1a050c743105df1268c897ee395be00/sw.cpp
@@ -0,0 +1,48 @@
+void build(Solution &s)
+{
+    auto &glfw = s.addTarget<LibraryTarget>("glfw", "3.3.2");
+    glfw += Git("https://github.com/glfw/glfw", "{M}.{m}{po}");
+
+    glfw +=
+        "deps/glad/.*"_rr,
+        "include/.*"_rr,
+        "src/.*\\.c"_rr,
+        "src/.*\\.h"_rr,
+        "src/.*\\.h.in"_rr,
+        "src/.*\\.m"_rr;
+
+    glfw.Private +=
+        "src"_id;
+
+    glfw.Public +=
+        "include"_id,
+        "deps"_id;
+
+    glfw.ApiName = "GLAPI";
+    glfw.Private += "_GLFW_USE_CONFIG_H"_d;
+    glfw.Interface += sw::Shared, "GLFW_DLL"_d;
+    glfw.Private += sw::Shared, "_GLFW_BUILD_DLL"_d;
+
+    glfw.Public += "org.sw.demo.khronos.vulkan"_dep;
+
+    if (glfw.getBuildSettings().TargetOS.is(OSType::Windows))
+    {
+        glfw -= "src/posix.*"_rr;
+        glfw -= "src/cocoa.*"_rr;
+        glfw -= "src/x11.*"_rr;
+        glfw -= "src/linux.*"_rr;
+        glfw -= "src/wl.*"_rr;
+        glfw -= "src/glx.*"_rr;
+        glfw -= "src/null.*"_rr;
+
+        glfw += "User32.lib"_slib;
+        glfw += "gdi32.lib"_slib;
+        glfw += "Shell32.lib"_slib;
+
+        glfw.Variables["_GLFW_WIN32"] = 1;
+    }
+    //_GLFW_X11
+    //_GLFW_COCOA
+
+    glfw.configureFile("src/glfw_config.h.in", "glfw_config.h");
+}
