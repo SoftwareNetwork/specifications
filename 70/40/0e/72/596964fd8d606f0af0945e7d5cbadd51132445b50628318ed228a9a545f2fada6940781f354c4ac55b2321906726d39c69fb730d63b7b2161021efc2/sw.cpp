fc833e66a31941647d4744ba972994bc9233bbef
CppanBot <cppanbot@gmail.com>
2020-01-21 20:05:19 +0000

    Update.

---
+++ 70/40/0e/72/596964fd8d606f0af0945e7d5cbadd51132445b50628318ed228a9a545f2fada6940781f354c4ac55b2321906726d39c69fb730d63b7b2161021efc2/sw.cpp
@@ -0,0 +1,7 @@
+void build(Solution &s)
+{
+    auto &variant = s.addTarget<LibraryTarget>("mapbox.variant", "1.1.6");
+    variant += Git("https://github.com/mapbox/variant", "v{v}");
+    variant += "include/.*"_rr;
+    variant.Public += "include"_id;
+}
