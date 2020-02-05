51273401b6850a25446cf27bc92a715b40e6dc77
CppanBot <cppanbot@gmail.com>
2020-02-05 00:27:14 +0000

    Update.

--- aa/b2/18/4e/5681751421b67d00543e9f4cbbd1f871723c3a3f969fbbc05da03aa291c3d4145300ac2f8adbd3eb9a9a664c4e10a3816a2a3096a2ea663e5d086495/sw.cpp
+++ aa/b2/18/4e/5681751421b67d00543e9f4cbbd1f871723c3a3f969fbbc05da03aa291c3d4145300ac2f8adbd3eb9a9a664c4e10a3816a2a3096a2ea663e5d086495/sw.cpp
@@ -2,5 +2,19 @@ void build(Solution &s)
 {
     auto &t = s.addLibrary("facebook.experimental.libunifex", "master");
     t += Git("https://github.com/facebookexperimental/libunifex");
+
     t += cpplatest;
+
+    // the configured options and settings for unifex
+    /*#cmakedefine01 UNIFEX_NO_MEMORY_RESOURCE
+    #cmakedefine UNIFEX_MEMORY_RESOURCE_HEADER <@UNIFEX_MEMORY_RESOURCE_HEADER@>
+    #cmakedefine UNIFEX_MEMORY_RESOURCE_NAMESPACE @UNIFEX_MEMORY_RESOURCE_NAMESPACE@
+    #cmakedefine01 UNIFEX_NO_COROUTINES
+    #cmakedefine UNIFEX_COROUTINES_HEADER <@UNIFEX_COROUTINES_HEADER@>
+    #cmakedefine UNIFEX_COROUTINES_NAMESPACE @UNIFEX_COROUTINES_NAMESPACE@*/
+
+    t.Variables["libunifex_VERSION_MAJOR"] = 0;
+    t.Variables["libunifex_VERSION_MINOR"] = 1;
+    t.Variables["UNIFEX_NO_EPOLL"] = 1;
+    t.configureFile("include/unifex/config.hpp.in", "unifex/config.hpp");
 }
