b34515f635d3af6ea0e66d070681e4f405e8cb55
CppanBot <cppanbot@gmail.com>
2020-02-13 15:27:19 +0000

    Update.

--- 1b/f1/0a/a5/530db879cfb6fb63bb0c68f9e17a01457e02a31c2f4f66f552e6ca1379cc348dba3f233ec74b318e8237828c63353bc53fea4c0eb19a817eb1cf5ee9/sw.cpp
+++ 1b/f1/0a/a5/530db879cfb6fb63bb0c68f9e17a01457e02a31c2f4f66f552e6ca1379cc348dba3f233ec74b318e8237828c63353bc53fea4c0eb19a817eb1cf5ee9/sw.cpp
@@ -1,7 +1,7 @@
 void build(Solution &s)
 {
-	auto &sqlpp11_connector_sqlite3 = s.addTarget<StaticLibraryTarget>("rbock.sqlpp11_connector_sqlite3", "master");
-	sqlpp11_connector_sqlite3 += Git("https://github.com/rbock/sqlpp11-connector-sqlite3", "", "{v}");
-	sqlpp11_connector_sqlite3.Public += "org.sw.demo.rbock.sqlpp11-master"_dep;
-	sqlpp11_connector_sqlite3.Public += "org.sw.demo.sqlite3-3"_dep;
+    auto &sqlpp11_connector_sqlite3 = s.addTarget<StaticLibraryTarget>("rbock.sqlpp11_connector_sqlite3", "master");
+    sqlpp11_connector_sqlite3 += Git("https://github.com/rbock/sqlpp11-connector-sqlite3");
+    sqlpp11_connector_sqlite3.Public += "org.sw.demo.rbock.sqlpp11-develop"_dep;
+    sqlpp11_connector_sqlite3.Public += "org.sw.demo.sqlite3"_dep;
 }
