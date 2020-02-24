4c6ffe354839c8c81140f0b9aea81766fba19cf0
CppanBot <cppanbot@gmail.com>
2020-02-24 10:18:43 +0000

    Update.

---
+++ 76/ba/af/a9/19024bcadf2de2a85ffa883a76e8b54c5416f0c5fb7de8aa797b3fcfd1799a42bb7282f38f9bcaf9829e8d50004ec0623242b1388229cd5979032243/sw.cpp
@@ -0,0 +1,57 @@
+void build(Solution &s)
+{
+    auto &cpprestsdk = s.addTarget<LibraryTarget>("microsoft.cpprestsdk", "2.10.15");
+    cpprestsdk += Git("https://github.com/Microsoft/cpprestsdk", "v{v}");
+    cpprestsdk.setRootDirectory("Release");
+
+    cpprestsdk +=
+        "include/.*"_rr,
+        "src/http/.*"_rr,
+        "src/json/.*"_rr,
+        "src/pch/.*"_rr,
+        "src/pplx/.*"_rr,
+        "src/streams/.*"_rr,
+        "src/uri/.*"_rr,
+        "src/utilities/.*\\.cpp"_rr,
+        "src/websockets/.*"_rr;
+
+    cpprestsdk +=
+        "src/pch"_id;
+
+    cpprestsdk -= ".*winrt.*"_rr;
+    if (cpprestsdk.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        cpprestsdk -= ".*apple.*"_rr;
+        cpprestsdk -= ".*linux.*"_rr;
+        cpprestsdk -= ".*posix.*"_rr;
+        cpprestsdk -= "src/http/client/http_client_asio.cpp";
+        cpprestsdk -= "src/http/listener/http_server_asio.cpp";
+        cpprestsdk -= "src/pplx/threadpool.cpp";
+    }
+
+    cpprestsdk.Public +=
+        "include"_id;
+
+    cpprestsdk += "CPPREST_NO_SSL_LEAK_SUPPRESS"_d;
+    cpprestsdk += "SSL_R_SHORT_READ=boost::asio::ssl::error::stream_truncated"_d;
+    if (cpprestsdk.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        cpprestsdk += "UNICODE"_def;
+        cpprestsdk.Public += "bcrypt.lib"_slib;
+        cpprestsdk.Public += "httpapi.lib"_slib;
+        cpprestsdk.Public += "winhttp.lib"_slib;
+    }
+    cpprestsdk += sw::Shared, "_ASYNCRT_EXPORT"_d;
+    cpprestsdk += sw::Shared, "_PPLX_EXPORT"_d;
+    cpprestsdk.Public += sw::Static, "_NO_ASYNCRTIMP"_d;
+    cpprestsdk.Public += sw::Static, "_NO_PPLXIMP"_d;
+
+    cpprestsdk.Public += "org.sw.demo.boost.asio"_dep;
+    cpprestsdk.Public += "org.sw.demo.zaphoyd.websocketpp"_dep;
+    cpprestsdk.Public += "org.sw.demo.openssl.ssl"_dep;
+    cpprestsdk.Public += "org.sw.demo.madler.zlib"_dep;
+    cpprestsdk.Public += "org.sw.demo.google.brotli"_dep;
+
+    cpprestsdk -= "src/websockets/client/ws_client_wspp.cpp";
+    //cpprestsdk.patch("src/websockets/client/ws_client_wspp.cpp", "::SSL_COMP_free_compression_methods();", "");
+}
