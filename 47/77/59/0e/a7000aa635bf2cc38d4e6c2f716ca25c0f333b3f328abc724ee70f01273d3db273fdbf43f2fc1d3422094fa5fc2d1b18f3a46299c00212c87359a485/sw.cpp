35c536191fa6d8a525ca6a7fe83cc81f02544e26
CppanBot <cppanbot@gmail.com>
2020-02-26 19:39:11 +0000

    Update

---
+++ 47/77/59/0e/a7000aa635bf2cc38d4e6c2f716ca25c0f333b3f328abc724ee70f01273d3db273fdbf43f2fc1d3422094fa5fc2d1b18f3a46299c00212c87359a485/sw.cpp
@@ -0,0 +1,25 @@
+void build(Solution &s)
+{
+    auto &abseil = s.addStaticLibrary("google.abseil", "2020.2.25");
+    abseil += Git("https://github.com/abseil/abseil-cpp", "{M}{m:02}{p:02}");
+
+    {
+        auto &t = abseil;
+        t += cpp11;
+        SwapAndRestore sr(t.SourceDir, t.SourceDir / "absl");
+        t += ".*\\.cc"_rr;
+        t += ".*\\.h"_rr;
+        t += ".*\\.inc"_rr;
+        t -= ".*_benchmark.*cc"_rr;
+        t -= ".*_test.*cc"_rr;
+        t -= ".*_generator.*cc"_rr;
+        t -= ".*_gentables.*cc"_rr;
+        t -= ".*_nonprod.*cc"_rr;
+        t -= ".*/test_.*cc"_rr;
+        t -= "hash/internal/print_hash_of.cc";
+        t -= "random/benchmarks.cc";
+        t -= "synchronization/internal/waiter.cc";
+    }
+
+    abseil.Public += "NOMINMAX"_d;
+}
