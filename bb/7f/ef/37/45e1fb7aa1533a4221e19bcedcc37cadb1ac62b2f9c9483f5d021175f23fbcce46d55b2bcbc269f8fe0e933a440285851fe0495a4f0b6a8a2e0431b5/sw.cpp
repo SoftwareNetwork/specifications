cd60098f3035ca386af14cf6082c52784ff87a48
CppanBot <cppanbot@gmail.com>
2020-02-28 08:51:33 +0000

    Update

---
+++ bb/7f/ef/37/45e1fb7aa1533a4221e19bcedcc37cadb1ac62b2f9c9483f5d021175f23fbcce46d55b2bcbc269f8fe0e933a440285851fe0495a4f0b6a8a2e0431b5/sw.cpp
@@ -0,0 +1,7 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("cpp_taskflow", "2.3.0");
+    t += Git("https://github.com/cpp-taskflow/cpp-taskflow", "v{v}");
+
+    t += "taskflow/.*"_rr;
+}
