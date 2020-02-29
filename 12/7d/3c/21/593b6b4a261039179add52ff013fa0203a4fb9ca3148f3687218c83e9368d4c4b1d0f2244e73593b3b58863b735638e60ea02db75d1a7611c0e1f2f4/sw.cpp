f4280894898c3d08c3d9349148406cabc049d3fa
CppanBot <cppanbot@gmail.com>
2020-02-29 18:51:30 +0000

    Update

---
+++ 12/7d/3c/21/593b6b4a261039179add52ff013fa0203a4fb9ca3148f3687218c83e9368d4c4b1d0f2244e73593b3b58863b735638e60ea02db75d1a7611c0e1f2f4/sw.cpp
@@ -0,0 +1,25 @@
+void build(Solution &s)
+{
+    auto &pqxx = s.addTarget<LibraryTarget>("jtv.pqxx", "7.0.4");
+    pqxx += Git("https://github.com/jtv/libpqxx", "{v}");
+
+    pqxx.setChecks("pqxx", true);
+
+    pqxx.ApiName = "PQXX_LIBEXPORT";
+    pqxx += cpp17;
+
+    if (pqxx.getBuildSettings().TargetOS.Type == OSType::Windows)
+        pqxx.Public += "ws2_32.lib"_slib;
+
+    pqxx.Public += "org.sw.demo.find.libpq-master"_dep;
+
+    pqxx.writeFileOnce("pqxx/config-public-compiler.h");
+    pqxx.writeFileOnce("pqxx/config-internal-libpq.h");
+    pqxx.writeFileOnce("pqxx/config-internal-compiler.h");
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("pqxx");
+    s.checkFunctionExists("poll");
+}
