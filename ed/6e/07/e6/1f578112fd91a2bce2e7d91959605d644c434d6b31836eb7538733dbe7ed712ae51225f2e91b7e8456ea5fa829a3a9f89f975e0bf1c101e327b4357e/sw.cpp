c456f5cdbdaaab235eb8103f5b02e6e79281f079
CppanBot <cppanbot@gmail.com>
2020-03-13 23:51:41 +0000

    Update

---
+++ ed/6e/07/e6/1f578112fd91a2bce2e7d91959605d644c434d6b31836eb7538733dbe7ed712ae51225f2e91b7e8456ea5fa829a3a9f89f975e0bf1c101e327b4357e/sw.cpp
@@ -0,0 +1,35 @@
+void build(Solution &s)
+{
+    auto &googletest = s.addProject("google.googletest", "1.10.0");
+    googletest += Git("https://github.com/google/googletest", "release-{v}");
+
+    const auto api = "GTEST_API_"s;
+
+    auto add = [&googletest, &api](const String &name) -> decltype(auto)
+    {
+        const auto dirname = "google" + name;
+        const auto tname = "g" + name;
+
+        auto &t = googletest.addLibrary(tname);
+        t.ApiName = api;
+        t -= FileRegex(dirname, "include/.*", true);
+        t -= FileRegex(dirname, "src/.*", true);
+        t += path(dirname) / "src" / (tname + "-all.cc");
+
+        t += IncludeDirectory(dirname);
+        t.Public += IncludeDirectory(dirname + "/include");
+
+        auto &m = t.addStaticLibrary("main");
+        m.ApiName = api;
+        m += path(dirname) / "src" / (tname + "_main.cc");
+        m.Public += t;
+
+        return t;
+    };
+
+    auto &t = add("test");
+    auto &m = add("mock");
+    m.Public += t;
+
+    t.patch("googletest/include/gtest/internal/gtest-port.h", "bool ParseInt32", api + "\nbool  ParseInt32");
+}
--- fa/a3/ef/ed/17e6c635058a473a69fcf59a81d102a7d7a4d0787f2193bc3f093413f04a6bce74b536addf41d8677a8e8f4cfdc85e59060a5c98fb7454f2e1420345/sw.cpp
+++ fa/a3/ef/ed/17e6c635058a473a69fcf59a81d102a7d7a4d0787f2193bc3f093413f04a6bce74b536addf41d8677a8e8f4cfdc85e59060a5c98fb7454f2e1420345/sw.cpp
@@ -1,5 +1,5 @@
 void build(Solution &s)
 {
-    auto &p = s.addLibrary("CLIUtils.CLI11", "1.9.0");
-    p += Git("https://github.com/CLIUtils/CLI11", "v{v}");
+    auto &t = s.addLibrary("CLIUtils.CLI11", "1.9.0");
+    t += Git("https://github.com/CLIUtils/CLI11", "v{v}");
 }
