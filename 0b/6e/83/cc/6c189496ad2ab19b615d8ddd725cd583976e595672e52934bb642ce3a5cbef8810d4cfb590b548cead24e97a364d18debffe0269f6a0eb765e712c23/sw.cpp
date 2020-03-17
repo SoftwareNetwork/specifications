241dbd74aceb7d0422a14dee2b9ca065e89d758b
CppanBot <cppanbot@gmail.com>
2020-03-17 00:51:47 +0000

    Update

--- 0b/6e/83/cc/6c189496ad2ab19b615d8ddd725cd583976e595672e52934bb642ce3a5cbef8810d4cfb590b548cead24e97a364d18debffe0269f6a0eb765e712c23/sw.cpp
+++ 0b/6e/83/cc/6c189496ad2ab19b615d8ddd725cd583976e595672e52934bb642ce3a5cbef8810d4cfb590b548cead24e97a364d18debffe0269f6a0eb765e712c23/sw.cpp
@@ -7,8 +7,16 @@ void build(Solution &s)
         auto url = "https://github.com/Tessil/" + n;
         boost::replace_all(n, "-", "_");
         auto &t = p.addTarget<LibraryTarget>(n, v);
-        t += Git(url, branch ? "" : "v{v}", "{v}");
+        t += Git(url, branch ? "" : "v{v}", branch ? "{v}" : "");
     };
 
+    // do not use configs like this!
+    // split one config per library
+    // it was used for speed
+    add_repo("robin-map", "0.6.2");
+    add_repo("hopscotch-map", "2.2.1");
+    add_repo("ordered-map", "0.8.1");
+    add_repo("hat-trie", "0.6.0");
+    add_repo("sparse-map", "0.6.2");
     add_repo("array-hash", "0.7.0");
 }
