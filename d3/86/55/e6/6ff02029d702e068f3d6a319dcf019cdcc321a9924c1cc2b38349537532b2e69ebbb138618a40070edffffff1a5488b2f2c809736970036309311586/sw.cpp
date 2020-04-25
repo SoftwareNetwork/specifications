4af19763b4ff457e1dbe44d27f5e22c53e9fce00
CppanBot <cppanbot@gmail.com>
2020-04-25 00:21:33 +0000

    Update

--- d3/86/55/e6/6ff02029d702e068f3d6a319dcf019cdcc321a9924c1cc2b38349537532b2e69ebbb138618a40070edffffff1a5488b2f2c809736970036309311586/sw.cpp
+++ d3/86/55/e6/6ff02029d702e068f3d6a319dcf019cdcc321a9924c1cc2b38349537532b2e69ebbb138618a40070edffffff1a5488b2f2c809736970036309311586/sw.cpp
@@ -10,4 +10,15 @@ void build(Solution &s)
     t -= "Autest.c";
 
     t.Public += "org.sw.demo.xorg.proto"_dep;
+
+    if (t.getBuildSettings().TargetOS.is(OSType::Windows))
+    {
+        t.writeFileOnce("unistd.h", R"(
+#define R_OK    4       /* Test for read permission.  */
+#define W_OK    2       /* Test for write permission.  */
+//#define   X_OK    1       /* execute permission - unsupported in windows*/
+#define F_OK    0       /* Test for existence.  */
+)");
+        t.writeFileOnce("sys/time.h", "#include <time.h>");
+    }
 }
