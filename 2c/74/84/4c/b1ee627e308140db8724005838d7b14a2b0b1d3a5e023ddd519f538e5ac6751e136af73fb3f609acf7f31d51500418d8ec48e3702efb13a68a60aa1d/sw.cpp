4b4bc0c3ecc3e2d3690b2999a9470a9b19186b64
CppanBot <cppanbot@gmail.com>
2020-05-08 11:55:40 +0000

    Update

--- 2c/74/84/4c/b1ee627e308140db8724005838d7b14a2b0b1d3a5e023ddd519f538e5ac6751e136af73fb3f609acf7f31d51500418d8ec48e3702efb13a68a60aa1d/sw.cpp
+++ 2c/74/84/4c/b1ee627e308140db8724005838d7b14a2b0b1d3a5e023ddd519f538e5ac6751e136af73fb3f609acf7f31d51500418d8ec48e3702efb13a68a60aa1d/sw.cpp
@@ -1,3 +1,13 @@
+static int merge_sql(FilesOrdered files, path o)
+{
+    String out;
+    for (auto &f : files)
+        out += read_file(f);
+    write_file(o, out);
+    return 0;
+}
+SW_DEFINE_VISIBLE_FUNCTION_JUMPPAD2(merge_sql)
+
 void build(Solution &s)
 {
     auto &t = s.addLibrary("OSGeo.PROJ", "7.0.1");
@@ -13,6 +23,10 @@ void build(Solution &s)
         t += "src/projections/.*"_r;
         t += "src/transformations/.*"_r;
 
+        // data files
+        t += "data/sql/.*"_r;
+        t += "data/sql_filelist.cmake";
+
         t.Public += "include"_idir;
         t.Public += "src"_idir;
 
@@ -25,6 +39,40 @@ void build(Solution &s)
             t += "NOMINMAX"_def;
             t += "shell32.lib"_slib;
         }
+
+        // proj.db
+        //if (!t.DryRun)
+        {
+            auto datadir = t.SourceDir / "data";
+            auto f = read_file(datadir / "sql_filelist.cmake");
+            std::regex r(R"xxx(\"\$\{SQL_DIR\}/(.*?\.sql)\")xxx");
+            std::smatch m;
+            FilesOrdered files;
+            while (std::regex_search(f, m, r))
+            {
+                files.push_back(datadir / "sql" / m[1].str());
+                f = m.suffix().str();
+            }
+
+            {
+                auto c = t.addCommand(SW_VISIBLE_FUNCTION(merge_sql));
+                auto bc = std::dynamic_pointer_cast<BuiltinCommand>(c.getCommand());
+                bc->push_back(files);
+                for (auto &f : files)
+                    c->addInput(f);
+                c << cmd::out("all.sql");
+            }
+        }
+
+        {
+            auto c = t.addCommand();
+            c << cmd::prog("org.sw.demo.sqlite3.shell"_dep)
+                << "-init"
+                << cmd::in("all.sql")
+                << cmd::out("proj.db")
+                << ".quit";
+        }
+        t += Definition("PROJ_LIB=\"" + normalize_path(t.BinaryDir / "proj.db\""));
     }
 }
 
