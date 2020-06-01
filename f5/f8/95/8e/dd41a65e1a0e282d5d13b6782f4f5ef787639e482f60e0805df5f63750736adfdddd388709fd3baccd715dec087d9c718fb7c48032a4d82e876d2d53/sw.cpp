3d2fbd8c2c8d329fc35d235b805bea386495ad0d
CppanBot <cppanbot@gmail.com>
2020-06-01 09:30:28 +0000

    Update

---
+++ f5/f8/95/8e/dd41a65e1a0e282d5d13b6782f4f5ef787639e482f60e0805df5f63750736adfdddd388709fd3baccd715dec087d9c718fb7c48032a4d82e876d2d53/sw.cpp
@@ -0,0 +1,14 @@
+void build(Solution &s)
+{
+    auto &jsoncpp = s.addTarget<LibraryTarget>("open_source_parsers.jsoncpp", "1.9.3");
+    jsoncpp += Git("https://github.com/open-source-parsers/jsoncpp", "{v}");
+
+    jsoncpp +=
+        "include/.*\\.h"_rr,
+        "src/lib_json/.*\\.cpp"_rr,
+        "src/lib_json/.*\\.h"_rr,
+        "src/lib_json/.*\\.inl"_rr;
+
+    jsoncpp.Interface += sw::Shared, "JSON_DLL"_d;
+    jsoncpp += sw::Shared, "JSON_DLL_BUILD"_d;
+}
