21e3dc3cb5def01bc4ed6262aad93202ec2d78d9
CppanBot <cppanbot@gmail.com>
2020-06-03 10:30:28 +0000

    Update

---
+++ 1b/11/23/9f/2427b7eb061d4b9e8aafda2f2d8e4f659f0788aa3e2351bcc986525a15c7329b12b2b11010a330c1906112f0a449a8ee71b892afb7dfa19aa0164831/sw.cpp
@@ -0,0 +1,30 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("amazon.awslabs.checksums", "0.1.7");
+    t += Git("https://github.com/awslabs/aws-checksums", "v{v}");
+
+    t +=
+        "include/.*"_rr,
+        "source/.*"_rr;
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_CHECKSUMS_EXPORTS"_d;
+    t.Public += sw::Shared, "USE_IMPORT_EXPORT"_d;
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t.Public += "USE_WINDOWS_DLL_SEMANTICS"_d;
+        t.Public += "WIN32"_d;
+    }
+    else
+    {
+        t.ExportAllSymbols = true;
+        t -= "source/visualc/.*"_rr;
+    }
+
+    t.replaceInFileOnce("source/intel/crc32c_sse42_asm.c", "+c", "+g");
+    t.replaceInFileOnce("source/intel/crc32c_sse42_asm.c", "+d", "+g");
+}
