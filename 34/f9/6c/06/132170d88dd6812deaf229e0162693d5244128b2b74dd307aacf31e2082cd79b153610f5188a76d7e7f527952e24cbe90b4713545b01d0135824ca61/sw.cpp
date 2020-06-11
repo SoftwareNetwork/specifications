ed2b111e3f4a32cb5d5076caed6cd265de07810b
CppanBot <cppanbot@gmail.com>
2020-06-11 16:31:34 +0000

    Update

---
+++ 34/f9/6c/06/132170d88dd6812deaf229e0162693d5244128b2b74dd307aacf31e2082cd79b153610f5188a76d7e7f527952e24cbe90b4713545b01d0135824ca61/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &wr = s.addLibrary("giovannidicanio.winreg", "3.1.0");
+    wr += Git("https://github.com/GiovanniDicanio/WinReg", "v{v}");
+    wr.setRootDirectory("WinReg");
+    //wr.ApiName = "WINREG_API";
+    wr += "WinReg.hpp";
+    wr += "advapi32.lib"_slib;
+}
