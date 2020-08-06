82182154f85a94ae190efeb9c433142a71b84fff
CppanBot <cppanbot@gmail.com>
2020-08-06 13:35:19 +0000

    Update

---
+++ 63/56/8d/12/4f92ba803cf8ba48bb00ede0b2b796a94a8e8ba84a3c5f12584fa3f903ba04d5d5544967ffafac3b0208d879bb2b12ffc701b9c67a41203fd036c21f/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &json = s.addTarget<LibraryTarget>("nlohmann.json", "3.9.1");
+    json += Git("https://github.com/nlohmann/json", "v{v}");
+
+    json += "include/.*"_rr;
+    json += "nlohmann_json.natvis";
+    json.Public += "nlohmann_json.natvis";
+}
