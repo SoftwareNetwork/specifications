b605adf98653a25f867fadc5e6a75826f76066f3
CppanBot <cppanbot@gmail.com>
2020-08-17 21:50:19 +0000

    Update

---
+++ 23/be/65/a6/005359818a91cfc49027a8679f00cd7632c8857dab6ba29da636e6d4d657d5dfa1112c7c25585de65368bb656573a3f25465ceecea908f6373c859ae/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &asio = s.addTarget<LibraryTarget>("chriskohlhoff.asio", "1.18.0");
+    asio += Git("https://github.com/chriskohlhoff/asio", "asio-{M}-{m}-{p}");
+
+    asio.setRootDirectory("asio");
+    asio += "include/.*\\.[hi]pp"_rr;
+    asio.Public += "ASIO_STANDALONE"_d;
+}
