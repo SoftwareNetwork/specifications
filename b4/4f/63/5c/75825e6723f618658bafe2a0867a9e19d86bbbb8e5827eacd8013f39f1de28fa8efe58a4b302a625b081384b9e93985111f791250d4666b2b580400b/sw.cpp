fc61e0bc4bf71b7117ffc50d82c3866e8578d79d
CppanBot <cppanbot@gmail.com>
2020-08-18 23:50:19 +0000

    Update

--- b4/4f/63/5c/75825e6723f618658bafe2a0867a9e19d86bbbb8e5827eacd8013f39f1de28fa8efe58a4b302a625b081384b9e93985111f791250d4666b2b580400b/sw.cpp
+++ b4/4f/63/5c/75825e6723f618658bafe2a0867a9e19d86bbbb8e5827eacd8013f39f1de28fa8efe58a4b302a625b081384b9e93985111f791250d4666b2b580400b/sw.cpp
@@ -25,8 +25,56 @@ void build(Solution &s)
         hwloc.Public += "_USRDLL=_WINDLL"_d;
         hwloc.configureFile("contrib/windows/hwloc_config.h", "hwloc/autogen/config.h");
         hwloc.configureFile("contrib/windows/private_config.h", "private/autogen/config.h");
-        hwloc.configureFile("contrib/windows/static-components.h", "static-components.h");
+    }
+    else
+    {
+        hwloc.ApiName = "HWLOC_DECLSPEC";
+
+        /*hwloc.writeFileOnce("private/autogen/config.h", R"(
+#pragma once
+
+#include <stdint.h>
+)");*/
+
+        hwloc.writeFileOnce("hwloc/autogen/config.h", R"(
+#pragma once
+
+#define HWLOC_VERSION "2.3.0"
+#define HWLOC_VERSION_MAJOR 2
+#define HWLOC_VERSION_MINOR 3
+#define HWLOC_VERSION_RELEASE 0
+#define HWLOC_VERSION_GREEK ""
+
+#define __hwloc_restrict
+#define __hwloc_inline __inline
+
+#define __hwloc_attribute_unused
+#define __hwloc_attribute_malloc
+#define __hwloc_attribute_const
+#define __hwloc_attribute_pure
+#define __hwloc_attribute_deprecated
+#define __hwloc_attribute_may_alias
+#define __hwloc_attribute_warn_unused_result
+
+#define hwloc_pid_t pid_t
+#define hwloc_thread_t pthread_t
+
+#include <stdint.h>
+typedef uint64_t hwloc_uint64_t;
+
+/* Whether we need to re-define all the hwloc public symbols or not */
+#define HWLOC_SYM_TRANSFORM 0
+
+/* The hwloc symbol prefix */
+#define HWLOC_SYM_PREFIX hwloc_
+
+/* The hwloc symbol prefix in all caps */
+#define HWLOC_SYM_PREFIX_CAPS HWLOC_
+)");
     }
 
     hwloc.Public += "org.sw.demo.xmlsoft.libxml2"_dep;
+
+    // common
+    hwloc.configureFile("contrib/windows/static-components.h", "static-components.h");
 }
