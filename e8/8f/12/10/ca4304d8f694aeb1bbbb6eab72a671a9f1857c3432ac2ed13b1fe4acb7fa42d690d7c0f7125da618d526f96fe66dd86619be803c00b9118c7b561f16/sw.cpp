86b5eb0e0f27d2415dd5a133a338be0555bea93c
CppanBot <cppanbot@gmail.com>
2020-09-07 13:19:06 +0000

    Update

--- e8/8f/12/10/ca4304d8f694aeb1bbbb6eab72a671a9f1857c3432ac2ed13b1fe4acb7fa42d690d7c0f7125da618d526f96fe66dd86619be803c00b9118c7b561f16/sw.cpp
+++ e8/8f/12/10/ca4304d8f694aeb1bbbb6eab72a671a9f1857c3432ac2ed13b1fe4acb7fa42d690d7c0f7125da618d526f96fe66dd86619be803c00b9118c7b561f16/sw.cpp
@@ -2,18 +2,15 @@ void build(Solution &s)
 {
     auto &fmt = s.addTarget<LibraryTarget>("fmt", "7.0.3");
     fmt += Git("https://github.com/fmtlib/fmt", "{v}");
-    //fmt.ExportAllSymbols = true;
 
-    fmt += cpp11;
+    fmt += cpp14;
 
     fmt.Private += sw::Shared, "FMT_EXPORT"_d;
     fmt.Public += sw::Shared, "FMT_SHARED"_d;
 
-    if (fmt.getCompilerType() == CompilerType::ClangCl ||
+    if (fmt.getCompilerType() == CompilerType::GNU ||
         fmt.getCompilerType() == CompilerType::Clang)
     {
-        fmt.Public += "FMT_USE_EXTERN_TEMPLATES=0"_d;
-        if (fmt.getCompilerType() == CompilerType::Clang)
-            fmt.ExportAllSymbols = true;
+        fmt.ExportAllSymbols = true;
     }
 }
