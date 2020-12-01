bd69813b33fd09471668b2501d356b367cbc6840
CppanBot <cppanbot@gmail.com>
2020-12-01 11:51:32 +0000

    Update

---
+++ 11/31/97/dc/2a9c97f345919fde7e7c0719a07f47c49bb57ea1de153f0ba5a3991b8347613bbf16f6cc9ed92b117ad8d81e1212ac0d043bb858f966451bc9a0ee01/sw.cpp
@@ -0,0 +1,18 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("libsigpp", "3.0.6");
+    t += Git("https://github.com/libsigcplusplus/libsigcplusplus");
+
+    t +=
+        "sigc\\+\\+/.*\\.cc"_rr,
+        "sigc\\+\\+/.*\\.h"_rr;
+
+    t += "SIGC_BUILD"_d;
+    t.Public += sw::Shared, "SIGC_DLL"_d;
+
+    t.Variables["SIGCXX_DISABLE_DEPRECATED"] = 1;
+    t.Variables["SIGCXX_MAJOR_VERSION"] = t.Variables["PACKAGE_MAJOR_VERSION"];
+    t.Variables["SIGCXX_MINOR_VERSION"] = t.Variables["PACKAGE_MINOR_VERSION"];
+    t.Variables["SIGCXX_MICRO_VERSION"] = t.Variables["PACKAGE_PATCH_VERSION"];
+    t.configureFile("sigc++config.h.in", "sigc++config.h", ConfigureFlags::EnableUndefReplacements);
+}
--- b9/63/80/50/0a1e2b6045c4117a377459a97438a021c953ee84d7b70aa291c89380eabb6798bbfcbb1121095d7a98c6098a13efbb1a1512991c8035c53beaa68fd8/sw.cpp
+++ b9/63/80/50/0a1e2b6045c4117a377459a97438a021c953ee84d7b70aa291c89380eabb6798bbfcbb1121095d7a98c6098a13efbb1a1512991c8035c53beaa68fd8/sw.cpp
@@ -45,11 +45,12 @@ void build(Solution &s)
         }
 
         nettle += "NETTLE_USE_MINI_GMP=1"_v;
-        nettle += "MAJOR_VERSION=3"_v;
-        nettle += "MINOR_VERSION=3"_v;
+        nettle += sw::Variable("MAJOR_VERSION=" + std::to_string(nettle.getPackage().getVersion().getMajor()));
+        nettle += sw::Variable("MINOR_VERSION=" + std::to_string(nettle.getPackage().getVersion().getMinor()));
         const auto GMP_NUMB_BITS = nettle.Variables["SIZEOF_UNSIGNED_LONG"] * 8;
         nettle.Variables["GMP_NUMB_BITS"] = std::to_string(GMP_NUMB_BITS);
         nettle.configureFile("version.h.in", "version.h");
+        nettle.configureFile("version.h.in", "nettle/version.h");
         nettle.writeFileOnce(nettle.BinaryPrivateDir / "config.h", "");
         nettle.writeFileOnce("nettle-stdint.h", "#include <stdint.h>");
         nettle.replaceInFileOnce(nettle.SourceDir / "pss-mgf1.h",
