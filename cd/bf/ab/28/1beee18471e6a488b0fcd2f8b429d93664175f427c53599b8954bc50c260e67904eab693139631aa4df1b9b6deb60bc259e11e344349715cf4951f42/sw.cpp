f693dde6fdf63b8b89559f90f4520a22b317d3b4
CppanBot <cppanbot@gmail.com>
2020-12-08 11:51:38 +0000

    Update

---
+++ cd/bf/ab/28/1beee18471e6a488b0fcd2f8b429d93664175f427c53599b8954bc50c260e67904eab693139631aa4df1b9b6deb60bc259e11e344349715cf4951f42/sw.cpp
@@ -0,0 +1,34 @@
+void build(Solution &s)
+{
+    auto &jasper = s.addTarget<LibraryTarget>("mdadams.jasper", "2.0.22");
+    jasper += Git("https://github.com/mdadams/jasper", "version-{v}");
+
+    jasper.SourceDir /= "src/libjasper";
+    jasper.setChecks("jas");
+    jasper +=
+        ".*\\.c"_rr,
+        ".*\\.h"_rr,
+        ".*\\.in"_rr;
+
+    if (jasper.getBuildSettings().TargetOS.Type == OSType::Windows ||
+        jasper.getBuildSettings().TargetOS.Type == OSType::Mingw)
+        jasper += "WIN32"_def;
+
+    jasper += sw::Shared, "JAS_BUILDING_DLL"_d;
+    jasper.Public += sw::Shared, "JAS_DLL"_d;
+    jasper.Public += sw::Shared, "JAS_HAVE_VISIBILITY"_d;
+
+    jasper.Public += "org.sw.demo.jpeg"_dep;
+
+    jasper.configureFile("include/jasper/jas_config.h.in", "jasper/jas_config.h");
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("jas");
+    s.checkIncludeExists("io.h");
+    s.checkIncludeExists("fcntl.h");
+
+    for (auto &check : s.all)
+        check->Prefixes.insert("JAS_");
+}
