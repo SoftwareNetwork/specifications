8d13192c290913f38cc6d00313c8f724f84c755c
CppanBot <cppanbot@gmail.com>
2021-01-07 09:53:59 +0000

    Update

---
+++ 4a/af/74/51/eaf6fcd96c6dd10a6799311ac6b827573565f3176372491a707417da5d8b3c68e67300a9f6cdec76b2bac94e052243356eda02d892b8b20e45a72a9c/sw.cpp
@@ -0,0 +1,15 @@
+void build(Solution &s)
+{
+    auto &rhash = s.addTarget<LibraryTarget>("aleksey14.rhash", "1.4.1");
+    rhash += Git("https://github.com/rhash/RHash", "v{v}");
+
+    rhash.ApiNames.insert("RHASH_API");
+    rhash +=
+        "librhash/.*\\.c"_rr,
+        "librhash/.*\\.h"_rr;
+
+    rhash -= "librhash/test.*"_rr;
+    rhash.Public += "librhash"_id;
+
+    rhash += Definition("RHASH_XVERSION=" + rhash.Variables["PACKAGE_VERSION_NUM"].toString());
+}
