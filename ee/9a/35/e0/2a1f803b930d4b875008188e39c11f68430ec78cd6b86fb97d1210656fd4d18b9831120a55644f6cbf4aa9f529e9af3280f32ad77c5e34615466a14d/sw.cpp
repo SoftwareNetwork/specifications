3392789dbf86a2f1a9346530a1333eb0a5fa4012
CppanBot <cppanbot@gmail.com>
2021-01-17 14:49:01 +0000

    Update

---
+++ ee/9a/35/e0/2a1f803b930d4b875008188e39c11f68430ec78cd6b86fb97d1210656fd4d18b9831120a55644f6cbf4aa9f529e9af3280f32ad77c5e34615466a14d/sw.cpp
@@ -0,0 +1,31 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<Library>("zeromq.libzmq", "4.3.4");
+    t += Git("https://github.com/zeromq/libzmq", "v{v}");
+
+    t += sw::Shared, "DLL_EXPORT"_def;
+    t.Public += sw::Static, "ZMQ_STATIC"_def;
+
+    t.Variables["@ZMQ_CACHELINE_SIZE@"] = 64;
+
+    t.Variables["ZMQ_USE_CV_IMPL_STL11"] = 1;
+    //t.Variables["ZMQ_USE_CV_IMPL_WIN32API"] = 1;
+    //t.Variables["ZMQ_USE_CV_IMPL_PTHREADS"] = 1;
+
+    t.Variables["ZMQ_POLL_BASED_ON_POLL"] = 1;
+
+    if (t.getBuildSettings().TargetOS.is(OSType::Windows))
+    {
+        t.Variables["ZMQ_IOTHREAD_POLLER_USE_SELECT"] = 1;
+        t.Variables["ZMQ_HAVE_WINDOWS"] = 1;
+
+        t += "ws2_32.lib"_slib, "Iphlpapi.lib"_slib, "Advapi32.lib"_slib;
+    }
+    else
+    {
+        t.Variables["ZMQ_IOTHREAD_POLLER_USE_EPOLL"] = 1;
+        t.Variables["ZMQ_IOTHREAD_POLLER_USE_EPOLL_CLOEXEC"] = 1;
+    }
+
+    t.configureFile("builds/cmake/platform.hpp.in", "platform.hpp");
+}
