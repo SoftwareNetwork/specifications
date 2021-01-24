d2948dd155ec4498b7bce861a4eb9107bf6be33e
CppanBot <cppanbot@gmail.com>
2021-01-24 14:12:47 +0000

    Update

---
+++ 78/89/7e/fc/f0706e1967adf0eb9e9c35debf68d87e8a73f747c6985a496a20e97ea592a251b38dd983682fb5a78e0b24e1030e88df35bea0fe5da34a41eb681bd6/sw.cpp
@@ -0,0 +1,21 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("DaanDeMeyer", "14.2.2");
+    p += Git("https://github.com/DaanDeMeyer/reproc", "v{v}");
+
+    auto &t = p.addLibrary("reproc");
+    t.ApiName = "REPROC_EXPORT";
+    t.setRootDirectory("reproc");
+    t.writeFileOnce("reproc/export.h");
+    if (t.getBuildSettings().TargetOS.is(OSType::Windows))
+        t -= ".*posix.*"_rr;
+    else
+        t -= ".*windows.*"_rr;
+    t += "HAVE_ATTRIBUTE_LIST"_def;
+
+    auto &tpp = p.addLibrary("reprocpp");
+    tpp.ApiName = "REPROCXX_EXPORT";
+    tpp.setRootDirectory("reproc++");
+    tpp.writeFileOnce("reproc++/export.hpp");
+    tpp.Public += t;
+}
