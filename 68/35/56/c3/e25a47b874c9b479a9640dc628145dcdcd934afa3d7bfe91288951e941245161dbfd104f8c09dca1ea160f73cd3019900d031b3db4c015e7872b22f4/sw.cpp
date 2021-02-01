a52f6b154b2ff18b7e55715b8faa59de45034f69
CppanBot <cppanbot@gmail.com>
2021-02-01 10:24:11 +0000

    Update

---
+++ 68/35/56/c3/e25a47b874c9b479a9640dc628145dcdcd934afa3d7bfe91288951e941245161dbfd104f8c09dca1ea160f73cd3019900d031b3db4c015e7872b22f4/sw.cpp
@@ -0,0 +1,42 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibrary>("facebook.folly", "2021.2.1.0");
+    t += Git("https://github.com/facebook/folly", "v{M}.{m:02}.{p:02}.{t:02}");
+
+    //t.CPPVersion = CPPLanguageStandard::CPP14;
+    t.CPPVersion = CPPLanguageStandard::CPP17;
+
+    t += "folly/.*\\.cpp"_rr;
+    t += "folly/.*\\.h"_rr;
+
+    t -= ".*tools.*"_rr;
+    t -= ".*experimental.*"_rr;
+    t -= ".*[tT]est.*"_rr;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t -= "folly/Demangle.cpp";
+        t -= "folly/detail/Demangle.cpp";
+        t -= "folly/Subprocess.cpp";
+        t -= "folly/python/.*"_rr;
+    }
+
+    t.Public += "_ENABLE_EXTENDED_ALIGNED_STORAGE=1"_def;
+    t.Public += "FOLLY_HAVE_LIBGFLAGS=1"_def;
+
+    t.Public += "org.sw.demo.google.glog"_dep;
+    t.Public += "org.sw.demo.google.double_conversion"_dep;
+    t.Public += "org.sw.demo.boost.crc"_dep;
+    t.Public += "org.sw.demo.boost.intrusive"_dep;
+    t.Public += "org.sw.demo.boost.thread"_dep;
+    t.Public += "org.sw.demo.boost.multi_index"_dep;
+    t.Public += "org.sw.demo.boost.context"_dep;
+    t.Public += "org.sw.demo.openssl.ssl"_dep;
+    t.Public += "org.sw.demo.libevent"_dep;
+    //t.Public += "org.sw.demo.python.lib"_dep;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+        t += "NOMINMAX"_d;
+
+    t.writeFileOnce("folly/folly-config.h");
+}
