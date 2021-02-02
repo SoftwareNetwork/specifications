b3531566d5e2734068ff2f48b468dd07a51f6115
CppanBot <cppanbot@gmail.com>
2021-02-02 01:24:09 +0000

    Update

---
+++ e2/34/74/09/92efadcce6840a518cb2d4bf8c39d7a5a023ffa625d4315961d60c765e739285bd0be0178b2b830dcfa7ad89e14efe7b016f48bf09befa5a8ab41556/sw.cpp
@@ -0,0 +1,10 @@
+void build(Solution &s)
+{
+    auto &re2 = s.addTarget<StaticLibraryTarget>("google.re2", "2021.2.2");
+    re2 += Git("https://github.com/google/re2", "{M}-{m:02}-{p:02}");
+
+    re2 += cpp11;
+    re2.ImportFromBazel = true;
+    re2.BazelTargetName = "re2";
+    re2.Public += "."_idir;
+}
