6a40746397fa5cc5dbc7ceddaad572a3348383d0
CppanBot <cppanbot@gmail.com>
2021-03-03 23:42:23 +0000

    Update

--- 5f/46/f8/6e/6ec72ad658280b3cb0ad4217ae2183d6823e322786ecc268bf9462be34ebd8ce57a2ccbf7b38477449713566c3c8b3bcbaa2510bec07c027bc7e31ec/sw.cpp
+++ 5f/46/f8/6e/6ec72ad658280b3cb0ad4217ae2183d6823e322786ecc268bf9462be34ebd8ce57a2ccbf7b38477449713566c3c8b3bcbaa2510bec07c027bc7e31ec/sw.cpp
@@ -3,8 +3,9 @@ void build(Solution &s)
     auto &t = s.addExecutable("dmage.co2mon", "master");
     t += Git("https://github.com/dmage/co2mon");
 
-    auto &lib = t.addStaticLibrary("lib");
+    auto &lib = t.addLibrary("lib");
     {
+        lib.ExportAllSymbols = true;
         lib.setRootDirectory("libco2mon");
         lib.Public += "org.sw.demo.libusb.hidapi"_dep;
     }
