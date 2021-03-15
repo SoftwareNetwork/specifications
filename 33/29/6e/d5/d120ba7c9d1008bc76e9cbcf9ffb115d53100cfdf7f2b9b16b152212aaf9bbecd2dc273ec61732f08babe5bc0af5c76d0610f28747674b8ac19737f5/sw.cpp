2e1fcc0cedb577f8487591ed17d99c2823011f67
CppanBot <cppanbot@gmail.com>
2021-03-15 23:32:06 +0000

    Update

--- 33/29/6e/d5/d120ba7c9d1008bc76e9cbcf9ffb115d53100cfdf7f2b9b16b152212aaf9bbecd2dc273ec61732f08babe5bc0af5c76d0610f28747674b8ac19737f5/sw.cpp
+++ 33/29/6e/d5/d120ba7c9d1008bc76e9cbcf9ffb115d53100cfdf7f2b9b16b152212aaf9bbecd2dc273ec61732f08babe5bc0af5c76d0610f28747674b8ac19737f5/sw.cpp
@@ -3,12 +3,19 @@ void build(Solution &s)
     auto &t = s.addStaticLibrary("quickfix.quickfix", "1.15.1");
     t += Git("https://github.com/quickfix/quickfix", "v{v}");
 
+    t += "src/config_windows.h";
     t.setRootDirectory("src/C++");
     t += ".*\\.cpp"_r;
     t += ".*\\.hp?p?"_r;
     t -= "double-conversion/.*"_r;
     t -= "fix.*"_rr;
 
+    t.Public += "HAVE_STD_UNIQUE_PTR"_def;
+    t.Public += "HAVE_STD_SHARED_PTR"_def;
+
     if (t.getBuildSettings().TargetOS.is(OSType::Windows))
         t += "ws2_32.lib"_slib;
+
+    if (!fs::exists(t.SourceDir / "quickfix"))
+        fs::create_directory_symlink(t.SourceDir / ".", t.SourceDir / "quickfix");
 }
