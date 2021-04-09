8105735370d24a7ea82a06504d1f90e945a4ed11
CppanBot <cppanbot@gmail.com>
2021-04-09 19:25:51 +0000

    Update

---
+++ 47/90/db/fa/4b8015107eccf129e796e6f53346313d765f22b0242e0c17eb705ab8795bd1c68127f13046b87f0fbada336ac3a21e8cd7353c594052d968e2f5beb2/sw.cpp
@@ -0,0 +1,40 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.c_cal", "0.5.3");
+    t += Git("https://github.com/awslabs/aws-c-cal", "v{v}");
+
+    t -=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t += "source/.*"_r;
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_CAL_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_CAL_USE_IMPORT_EXPORT"_d;
+
+    t -= "org.sw.demo.amazon.awslabs.lc-main"_dep;
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t.Public += "WIN32"_def;
+        t += "source/windows/.*"_rr;
+        t += "bcrypt.lib"_slib;
+        /*t += "advapi32.lib"_slib;
+        t += "Crypt32.lib"_slib;
+        t += "Secur32.lib"_slib;
+        t += "Shlwapi.lib"_slib;
+        t += "ws2_32.lib"_slib;*/
+    }
+    else
+    {
+        t += "source/unix/.*"_rr;
+        t += "dl"_slib;
+        t += "org.sw.demo.amazon.awslabs.lc-main"_dep;
+    }
+
+    //t += "org.sw.demo.openssl.crypto"_dep;
+    t.Public += "org.sw.demo.amazon.awslabs.c_common"_dep;
+}
