34e215b14f7f240f45b2600b11e480cd39954666
CppanBot <cppanbot@gmail.com>
2021-04-26 16:19:31 +0000

    Update

---
+++ 89/2a/91/22/04b01fa882117c0cc9df210bdd58fbe184fdda88b81f4baaac14e101eef138b13e17716a0d5f1f2ff8a414a5e8bc1700920f661bc03f018706566cc8/sw.cpp
@@ -0,0 +1,48 @@
+void build(Solution &s)
+{
+    auto &tgbot = s.addLibrary("tgbot", "1.1.1");
+    tgbot += Git("https://github.com/egorpugin/tgbot");
+
+    auto &apitool = tgbot.addExecutable("apitool");
+    {
+        auto &t = apitool;
+        t.PackageDefinitions = true;
+        t += cpp20;
+        t += "tools/.*"_rr;
+        t += "pub.egorpugin.primitives.emitter"_dep;
+        t += "pub.egorpugin.primitives.http"_dep;
+        t += "pub.egorpugin.primitives.xml"_dep;
+        t += "pub.egorpugin.primitives.sw.main"_dep;
+        t += "org.sw.demo.nlohmann.json"_dep;
+        t += "org.sw.demo.imageworks.pystring"_dep;
+    }
+
+    //
+    {
+        tgbot += cpp20;
+
+        tgbot.ApiName = "TGBOT_API";
+
+        tgbot += "include/.*"_rr;
+        tgbot += "src/.*"_rr;
+
+        if (tgbot.getCompilerType() == CompilerType::MSVC)
+            tgbot.CompileOptions.push_back("/Zc:__cplusplus");
+
+        tgbot += "org.sw.demo.nlohmann.json"_dep;
+        tgbot += "org.sw.demo.badger.curl.libcurl"_dep;
+
+        {
+            auto c = tgbot.addCommand();
+            c << cmd::prog(apitool)
+                << cmd::wdir(tgbot.BinaryDir)
+                << cmd::in("TelegramBotAPI.html")
+                << cmd::end()
+                << cmd::out("reflection.inl.h")
+                << cmd::out("types.inl.h")
+                << cmd::out("methods.inl.h")
+                << cmd::out("methods.inl.cpp", cmd::Skip)
+                ;
+        }
+    }
+}
