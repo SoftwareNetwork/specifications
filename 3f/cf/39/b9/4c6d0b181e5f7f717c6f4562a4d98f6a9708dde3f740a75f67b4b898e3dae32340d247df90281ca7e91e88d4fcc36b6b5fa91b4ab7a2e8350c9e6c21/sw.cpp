492d38310f3a5895adf8ae80205b37af8a651bc3
CppanBot <cppanbot@gmail.com>
2021-05-16 10:16:21 +0000

    Update

---
+++ 3f/cf/39/b9/4c6d0b181e5f7f717c6f4562a4d98f6a9708dde3f740a75f67b4b898e3dae32340d247df90281ca7e91e88d4fcc36b6b5fa91b4ab7a2e8350c9e6c21/sw.cpp
@@ -0,0 +1,7 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibrary>("martinus.robin_hood_hashing", "3.11.2");
+    t += Git("https://github.com/martinus/robin-hood-hashing");
+    t += "src/include/.*\\.h"_rr;
+    t += "src/include"_idir;
+}
