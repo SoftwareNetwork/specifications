09f5b9936302206a8e4b58bf1e945dfccdea05b9
CppanBot <cppanbot@gmail.com>
2021-05-21 07:16:30 +0000

    Update

---
+++ 9a/8f/3b/ae/e0fb305158346b7e73da7508fd6e1fd7b1b3c3d0d4308bfe8881449c2beaa487a24cfcec31562fe086dd9b237d83a9b04d64c6ec7c9f9b334ff4f5b4/sw.cpp
@@ -0,0 +1,18 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("alandefreitas.matplotplusplus", "1.1.0");
+    t += Git("https://github.com/alandefreitas/matplotplusplus", "v{v}");
+
+    t += cpp17;
+
+    t += "source/matplot/.*"_rr;
+    t -= "source/matplot/backend/opengl.*"_rr;
+
+    t += "NOMINMAX"_def;
+    t += "gdi32.lib"_slib;
+    t += "Shell32.lib"_slib;
+    t += "user32.lib"_slib;
+
+    t += "org.sw.demo.olvb.nodesoup-master"_dep;
+    t += "org.sw.demo.dtschump.CImg"_dep;
+}
