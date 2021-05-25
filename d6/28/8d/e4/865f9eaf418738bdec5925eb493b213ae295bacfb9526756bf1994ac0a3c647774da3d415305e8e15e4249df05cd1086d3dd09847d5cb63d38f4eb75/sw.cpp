b99b2d4e8be7c35419be1090ed0212ac79114f38
CppanBot <cppanbot@gmail.com>
2021-05-25 20:54:22 +0000

    Update

---
+++ d6/28/8d/e4/865f9eaf418738bdec5925eb493b213ae295bacfb9526756bf1994ac0a3c647774da3d415305e8e15e4249df05cd1086d3dd09847d5cb63d38f4eb75/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("zmartzone.liboauth2", "1.4.2");
+    t += Git("https://github.com/zmartzone/liboauth2", "v{v}");
+
+    t += "org.sw.demo.badger.curl.libcurl"_dep;
+    t += "org.sw.demo.akheron.jansson"_dep;
+}
