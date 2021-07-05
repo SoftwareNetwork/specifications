fa54c9cbc88625c61a5189e4f59395fe2655d752
CppanBot <cppanbot@gmail.com>
2021-07-05 15:36:56 +0000

    Update

---
+++ d0/e8/32/48/d7c7439a9a4253d9435ade8f2bb5cc3f5c073d187a09fed45e7442ea5b86010b37cb8659486b881a3f1fb8eb3633447f930e9b965b61971c0242eea4/sw.cpp
@@ -0,0 +1,41 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("OSGeo.libgeotiff", "1.7.0");
+    t += Git("https://github.com/OSGeo/libgeotiff");
+
+    {
+        t.setRootDirectory("libgeotiff");
+        t.setChecks("libgeotiff");
+
+        t.ApiName = "GTIF_DLL";
+        t += "libxtiff/.*\\.[hc]"_r;
+        t += ".*\\.[hc]"_r;
+        t += ".*\\.inc"_r;
+
+        t += "GTIFAtof=GTIFAtof"_def;
+
+        t.Public += "libxtiff"_idir;
+        t.Public += "."_idir;
+
+        t.Public += "org.sw.demo.tiff"_dep;
+        t.Public += "org.sw.demo.OSGeo.PROJ"_dep;
+
+        t.configureFile("geo_config.h.in", "geo_config.h");
+    }
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("libgeotiff");
+    s.checkIncludeExists("stdlib.h");
+    s.checkIncludeExists("string.h");
+    s.checkIncludeExists("strings.h");
+    s.checkSourceCompiles("STDC_HEADERS", R"sw_xxx(
+    #include <stdlib.h>
+    #include <stdarg.h>
+    #include <string.h>
+    #include <float.h>
+    int main() {return 0;}
+    )sw_xxx");
+
+}
