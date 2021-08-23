ab1eb81079c27e57aab56ddb1ec6f57a51f20b2e
CppanBot <cppanbot@gmail.com>
2021-08-23 11:54:38 +0000

    Update

--- aa/4e/cc/9d/afd9d49e3b64ef04253e23730a37639ddcca358e300477a7bd007c4709a85d67674686ee94a630612eafca42a79092d4658fedb5fa3954615fc85a73/sw.cpp
+++ aa/4e/cc/9d/afd9d49e3b64ef04253e23730a37639ddcca358e300477a7bd007c4709a85d67674686ee94a630612eafca42a79092d4658fedb5fa3954615fc85a73/sw.cpp
@@ -1,6 +1,6 @@
 void build(Solution &s)
 {
-    auto &t = s.addTarget<LibraryTarget>("yourWaifu.sleepy_discord", "master");
+    auto &t = s.addStaticLibrary("yourWaifu.sleepy_discord", "master");
     t += Git("https://github.com/yourWaifu/sleepy-discord");
 
     t += cpp17;
@@ -11,9 +11,12 @@ void build(Solution &s)
     t.Public += "include"_idir;
     t += "include/sleepy_discord"_idir;
 
-    t += "NONEXISTENT_WEBSOCKETPP"_def;
+    //t += "NONEXISTENT_WEBSOCKETPP"_def;
+    t += "NONEXISTENT_UWEBSOCKETS"_def;
+    t += "NONEXISTANT_GIT_INFO"_def;
 
-    t.Public += "org.sw.demo.uNetworking.uWebSockets"_dep;
+    //t.Public += "org.sw.demo.uNetworking.uWebSockets"_dep;
+    t.Public += "org.sw.demo.zaphoyd.websocketpp"_dep;
     t.Public += "org.sw.demo.whoshuu.cpr"_dep;
     t.Public += "org.sw.demo.xiph.opus"_dep;
     t.Public += "org.sw.demo.chriskohlhoff.asio"_dep;
