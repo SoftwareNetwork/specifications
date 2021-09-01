5bd1b21b1748826c16c4e4ed3d5e1375e7c347ca
CppanBot <cppanbot@gmail.com>
2021-09-01 18:54:39 +0000

    Update

---
+++ 31/39/90/34/f1d1bab2bd8aeee018e3a7dabafb17bf4cef26c008fe66cb6ff6aeb1ac9c3d487d4ea2fb0a39c8ea3a81feb7b4f1f8defcc4380e6d314a6abedf90e2/sw.cpp
@@ -0,0 +1,18 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("jinja2cpp", "1.2.1");
+    t += Git("https://github.com/jinja2cpp/Jinja2Cpp");
+
+    t += cpp17;
+
+    t += "org.sw.demo.fmt"_dep;
+    t += "org.sw.demo.boost.algorithm"_dep;
+    t += "org.sw.demo.boost.filesystem"_dep;
+    t += "org.sw.demo.boost.iterator"_dep;
+    t += "org.sw.demo.boost.unordered"_dep;
+    t += "org.sw.demo.boost.variant"_dep;
+    t += "org.sw.demo.martinmoene.expected_lite"_dep;
+    t += "org.sw.demo.martinmoene.optional_lite"_dep;
+    t += "org.sw.demo.martinmoene.string_view_lite"_dep;
+    t += "org.sw.demo.martinmoene.variant_lite"_dep;
+}
