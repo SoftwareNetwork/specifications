a02af288abd80024ee82f69805c61ea69a57e254
CppanBot <cppanbot@gmail.com>
2021-09-16 20:37:12 +0000

    Update

--- 17/e9/58/09/06b01c4741508685c4e625ec229d4575a6f3f404cc7e57ef70bae91c41f3536ec6132f36bef2b16d3e5528d8a2491e493988a9234f0c189ccec2cc0c/sw.cpp
+++ 17/e9/58/09/06b01c4741508685c4e625ec229d4575a6f3f404cc7e57ef70bae91c41f3536ec6132f36bef2b16d3e5528d8a2491e493988a9234f0c189ccec2cc0c/sw.cpp
@@ -3,6 +3,8 @@ void build(Solution &s)
     auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.c_mqtt", "0.7.7");
     t += Git("https://github.com/awslabs/aws-c-mqtt", "v{v}");
 
+    t.Public += "AWS_MQTT_WITH_WEBSOCKETS"_def;
+
     t.Private += sw::Shared, "AWS_MQTT_EXPORTS"_d;
     t.Public += sw::Shared, "AWS_MQTT_USE_IMPORT_EXPORT"_d;
 
