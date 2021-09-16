2a6739b342dcbf9cf895475f40eefef6ab5a781c
CppanBot <cppanbot@gmail.com>
2021-09-16 19:03:39 +0000

    Update

---
+++ 7f/fc/c9/bf/76d7ed2cd359c112dde5e184083a9f97a0472702612d203dc2303c51868f306495f9311d7afa0d46252a19fe8d3cba35b24cd1cf36fceeac0e297440/sw.cpp
@@ -0,0 +1,24 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.c_compression", "0.2.14");
+    t += Git("https://github.com/awslabs/aws-c-compression", "v{v}");
+
+    t -=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t += "source/.*"_r;
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_COMPRESSION_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_COMPRESSION_USE_IMPORT_EXPORT"_d;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t.Public += "WIN32"_def;
+    }
+    t.Public += "org.sw.demo.amazon.awslabs.c_common"_dep;
+}
