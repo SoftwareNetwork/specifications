a159d48fee756b9cced2ab403037544059659c5e
CppanBot <cppanbot@gmail.com>
2021-09-26 11:11:33 +0000

    Update

---
+++ 7b/ce/1c/c5/fc889d7dcde5a44fe7bc9424c37471c1c50e1b78739a5b3e546e29b8646c033c6c4fceb8d5d2fa1195a6172515e3661fcc0531db8497ee9fe8f9cd00/sw.cpp
@@ -0,0 +1,6 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibrary>("biojppm.rapidyaml", "0.2.1");
+    t += Git("https://github.com/biojppm/rapidyaml", "v{v}");
+    t.Public += "org.sw.demo.biojppm.c4core-master"_dep;
+}
