f5af2d3721f521dec7cad2a11a3e5d2bb4328992
CppanBot <cppanbot@gmail.com>
2021-10-19 01:24:36 +0000

    Update

---
+++ a9/2e/9f/df/b21486b874aac61330151021671d23f190c011411fa0491657db55f9dabef448ac4e7ad07634e7374ee51c6c36991a9c5e0b587a407fadf8cbfd0c26/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("xorg.proto.xcb", "1.14.1");
+    t += Git("https://gitlab.freedesktop.org/xorg/proto/xcbproto", "xcb-proto-{M}.{m}{po}");
+
+    t += "src/.*\\.xml"_r;
+    t += "src/.*\\.xsd"_r;
+    t += "xcbgen/.*\\.py"_r;
+}
