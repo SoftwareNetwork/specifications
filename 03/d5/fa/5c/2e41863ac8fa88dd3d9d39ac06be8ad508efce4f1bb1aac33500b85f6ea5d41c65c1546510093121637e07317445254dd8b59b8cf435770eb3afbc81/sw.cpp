9445a42cb2e4479dd2aef7a60ac01fe1aa02f1d4
CppanBot <cppanbot@gmail.com>
2021-10-29 22:01:27 +0000

    Update

---
+++ 03/d5/fa/5c/2e41863ac8fa88dd3d9d39ac06be8ad508efce4f1bb1aac33500b85f6ea5d41c65c1546510093121637e07317445254dd8b59b8cf435770eb3afbc81/sw.cpp
@@ -0,0 +1,21 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.c_http", "0.6.9");
+    t += Git("https://github.com/awslabs/aws-c-http", "v{v}");
+
+    t -=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t += "source/.*"_r;
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_HTTP_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_HTTP_USE_IMPORT_EXPORT"_d;
+
+    t.Public += "org.sw.demo.amazon.awslabs.c_io"_dep;
+    t.Public += "org.sw.demo.amazon.awslabs.c_compression"_dep;
+}
