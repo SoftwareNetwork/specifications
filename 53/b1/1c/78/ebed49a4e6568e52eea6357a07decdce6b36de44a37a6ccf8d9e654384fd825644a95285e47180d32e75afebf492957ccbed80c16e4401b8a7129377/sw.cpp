48d530622c22a34870e76c7a35a7813a0967af23
CppanBot <cppanbot@gmail.com>
2021-11-12 16:01:37 +0000

    Update

---
+++ 53/b1/1c/78/ebed49a4e6568e52eea6357a07decdce6b36de44a37a6ccf8d9e654384fd825644a95285e47180d32e75afebf492957ccbed80c16e4401b8a7129377/sw.cpp
@@ -0,0 +1,38 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("webmproject", "1.0.0.28");
+    p += Git("https://github.com/webmproject/libwebm", "libwebm-{v}");
+
+    auto install = [](auto &t)
+    {
+        if (t.DryRun)
+            return;
+        auto d = t.BinaryDir / "libwebm";
+        fs::create_directories(d);
+        for (auto &[p, sf] : t[".*\\.h"_rr])
+        {
+            auto f = p;
+            t.check_absolute(f);
+            fs::copy_file(f, d / f.filename(), fs::copy_options::update_existing);
+        }
+    };
+
+    auto &mkvparser = p.addStaticLibrary("mkvparser");
+    mkvparser += "mkvparser/.*"_rr;
+    mkvparser += "common/webmids.h";
+    install(mkvparser);
+
+    auto &mkvmuxer = p.addStaticLibrary("mkvmuxer");
+    mkvmuxer += "mkvmuxer/.*"_rr;
+    mkvmuxer.Public += mkvparser;
+    install(mkvmuxer);
+
+    auto &webm = p.addStaticLibrary("webm");
+    //webm.ExportAllSymbols = true;
+    webm += "common/file_util.cc";
+    webm += "common/file_util.h";
+    webm += "common/hdr_util.cc";
+    webm += "common/hdr_util.h";
+    webm.Public += mkvmuxer;
+    install(webm);
+}
