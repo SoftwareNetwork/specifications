7dedb6148c2649821cbb8bd76c8a9a9b050f8d48
CppanBot <cppanbot@gmail.com>
2021-12-04 23:53:05 +0000

    Update

---
+++ 2e/bd/a0/0b/cc6b96a2616c8131079d024ea1c6fa227743a5000debee71f54def3c734b9bc5a6b0ad0b89636d2afc867c7d2c1018cb03ae9234b13062d6b7dc9d7f/sw.cpp
@@ -0,0 +1,42 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("arvidn.libtorrent", "2.0.5");
+    t += Git("https://github.com/arvidn/libtorrent", "v{v}");
+
+    t += cpp14;
+
+    t += "TORRENT_BUILDING_LIBRARY"_d;
+    t += sw::Shared, "TORRENT_BUILDING_SHARED"_d;
+
+    t += "BOOST_ASIO_HAS_STD_CHRONO"_d;
+    t += "BOOST_ASIO_ENABLE_CANCELIO"_d;
+    t += "BOOST_ASIO_NO_DEPRECATED"_d;
+
+    t.Public += "TORRENT_USE_LIBCRYPTO"_d;
+    //t.Public += "TORRENT_USE_SSL"_d;
+    t.Public += "TORRENT_USE_OPENSSL"_d;
+    t.Public += "TORRENT_SSL_PEERS"_d;
+    //t.Public += "OPENSSL_NO_SSL2"_d;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t.Public += "_WIN32_WINNT=0x0600"_d;
+        t.Public += "NOMINMAX"_d;
+        t.Public += "WIN32_LEAN_AND_MEAN"_d;
+        t.Public += "iphlpapi.lib"_slib;
+        t.CompileOptions.push_back("/bigobj");
+    }
+
+    t.Public += "org.sw.demo.boost.asio"_dep;
+    t.Public += "org.sw.demo.boost.crc"_dep;
+    t.Public += "org.sw.demo.boost.date_time"_dep;
+    t.Public += "org.sw.demo.boost.scope_exit"_dep;
+    t.Public += "org.sw.demo.boost.system"_dep;
+    t.Public += "org.sw.demo.boost.logic"_dep;
+    t.Public += "org.sw.demo.boost.multi_index"_dep;
+    t.Public += "org.sw.demo.boost.multiprecision"_dep;
+    t.Public += "org.sw.demo.boost.variant"_dep;
+
+    t.Public += "org.sw.demo.openssl.ssl"_dep;
+    t.Public += "org.sw.demo.arvidn.try_signal-master"_dep;
+}
