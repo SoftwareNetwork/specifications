9b44d20fb3fb146a287ce258141976534d20c2dc
CppanBot <cppanbot@gmail.com>
2021-12-16 23:30:02 +0000

    Update

---
+++ 37/d8/48/58/f2d5607a7174bdd53d8966940f038d00313051cbb057e5aa69f09cad3ca32e19cb3042a9d016b9eca47fb723170979f5c95514846f7ff215a5d3c364/sw.cpp
@@ -0,0 +1,34 @@
+void build(Build &b)
+{
+    auto &p = b.addProject("google.protocolbuffers.upb", "0.0.1");
+    p += Git("https://github.com/protocolbuffers/upb", "", "main");
+
+    //auto &wyhash = p.addStaticLibrary("third_party.wyhash");
+    //wyhash += "third_party/wyhash/wyhash.h";
+
+    auto &port = p.addStaticLibrary("port");
+    port += "upb/port_def.inc";
+    port += "upb/port_undef.inc";
+
+    auto &upb = p.addStaticLibrary("upb");
+    upb += "upb/decode.*"_rr;
+    upb += "upb/encode.*"_rr;
+    upb += "upb/msg.*"_rr;
+    upb -= "upb/msg_test.*"_rr;
+    upb += "upb/table.*"_rr;
+    upb += "upb/upb.*"_rr;
+    //upb += wyhash;
+    upb.Public += port;
+
+    auto &reflection = p.addStaticLibrary("reflection");
+    reflection += "upb/def.*"_rr;
+    reflection += "upb/reflection.*"_rr;
+    reflection += "cmake/google/.*"_rr;
+    reflection.Public += "."_idir;
+    reflection.Public += "cmake"_idir;
+    reflection.Public += upb;
+
+    auto &textformat = p.addStaticLibrary("textformat");
+    textformat += "upb/text_encode.*"_rr;
+    textformat.Public += reflection;
+}
