d665ad4438dcd209cf74f06fca884e3e61e588df
CppanBot <cppanbot@gmail.com>
2021-12-21 11:46:02 +0000

    Update

---
+++ 42/b0/8c/01/bac79e0f0aa29bd869045a19c2930508946fb482810d09938adab500cc072fb7e1b6eb10cc061632a7c173513d701d574411c53c9e2684066c644961/sw.cpp
@@ -0,0 +1,29 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("facebook.zstd", "1.5.1");
+    p += Git("https://github.com/facebook/zstd", "v{v}");
+
+    auto &lib = p.addLibrary("zstd");
+    lib +=
+        "lib/common/.*"_rr,
+        "lib/compress/.*"_rr,
+        "lib/dictBuilder/.*"_rr,
+        "lib/decompress/.*"_rr,
+        "lib/deprecated/.*"_rr,
+        "lib/.*\\.h"_r
+        ;
+    lib +=
+        "lib/common"_idir,
+        "lib/compress"_idir,
+        "lib/dictBuilder"_idir,
+        "lib/decompress"_idir,
+        "lib/deprecated"_idir
+        ;
+    lib.Public += "lib"_idir;
+
+    if (lib.getBuildSettings().TargetOS.is(OSType::Windows))
+    {
+        lib += sw::Shared, "ZSTD_DLL_EXPORT"_def;
+        lib.Interface += sw::Shared, "ZSTD_DLL_IMPORT"_def;
+    }
+}
