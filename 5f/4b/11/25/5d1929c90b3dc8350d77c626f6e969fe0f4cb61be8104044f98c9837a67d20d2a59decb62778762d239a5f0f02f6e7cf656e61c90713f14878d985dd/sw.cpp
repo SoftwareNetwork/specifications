05d82363f93a3ddbb05d7de06ae89249954974ae
CppanBot <cppanbot@gmail.com>
2021-12-29 19:58:45 +0000

    Update

--- 5f/4b/11/25/5d1929c90b3dc8350d77c626f6e969fe0f4cb61be8104044f98c9837a67d20d2a59decb62778762d239a5f0f02f6e7cf656e61c90713f14878d985dd/sw.cpp
+++ 5f/4b/11/25/5d1929c90b3dc8350d77c626f6e969fe0f4cb61be8104044f98c9837a67d20d2a59decb62778762d239a5f0f02f6e7cf656e61c90713f14878d985dd/sw.cpp
@@ -39,8 +39,8 @@ void build(Solution &s)
                 ;
         }
 
-        //if (utils.getBuildSettings().TargetOS.Type == OSType::Windows)
-        //{
+        if (utils.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
             auto gperf_cmd = [&utils](const String &s)
             {
                 auto c = utils.addCommand();
@@ -55,12 +55,12 @@ void build(Solution &s)
             };
             gperf_cmd("mime_type_to_extension");
             gperf_cmd("extension_to_mime_type");
-        /*}
+        }
         else
         {
             //set(MIME_TYPE_TO_EXTENSION_CMD ${gperf} -m100 auto/mime_type_to_extension.gperf | grep -v __gnu_inline__ > auto/mime_type_to_extension.cpp)
             //set(EXTENSION_TO_MIME_TYPE_CMD ${gperf} -m100 auto/extension_to_mime_type.gperf | grep -v __gnu_inline__ > auto/extension_to_mime_type.cpp)
-        }*/
+        }
     }
 
     auto &actor = td.addTarget<StaticLibraryTarget>("actor");
@@ -75,8 +75,7 @@ void build(Solution &s)
     {
         db += "tddb/td/db/.*"_rr;
         db.Public += "tddb"_idir;
-        //db.Public += "org.sw.demo.sqlcipher.sqlcipher"_dep;
-        db.Public += "org.sw.demo.sqlite3"_dep;
+        db.Public += "org.sw.demo.sqlcipher.sqlcipher"_dep;
         db.Public += actor;
 
         for (auto &f : { "tddb/td/db/detail/RawSqliteDb.cpp", "tddb/td/db/SqliteDb.cpp", "tddb/td/db/SqliteStatement.cpp" })
@@ -125,7 +124,7 @@ void build(Solution &s)
     {
         if (t.getCompilerType() == CompilerType::MSVC)
             t.CompileOptions.push_back("-bigobj");
-        //else
+        //else if (t.getCompilerType() != CompilerType::GNU)
             //t.CompileOptions.push_back("-Wa,-mbig-obj");
     };
 
@@ -252,12 +251,6 @@ void build(Solution &s)
         core.Public += memprof;
         core.Public += net;
         core.Public += db;
-
-        // disable sqlcipher
-        core.patch("td/telegram/Td.cpp",
-            "DbKey::raw_key(\"cucumber\")",
-            "td::DbKey::empty()"
-        );
     }
 
     auto &client = td.addTarget<StaticLibraryTarget>("client");
