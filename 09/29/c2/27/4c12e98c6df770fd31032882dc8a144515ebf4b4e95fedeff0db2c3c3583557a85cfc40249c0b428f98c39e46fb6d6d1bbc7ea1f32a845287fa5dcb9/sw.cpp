eec5c3f61d36f8240de3e00373bc1fbc83310b40
CppanBot <cppanbot@gmail.com>
2022-01-02 17:58:01 +0000

    Update

---
+++ 09/29/c2/27/4c12e98c6df770fd31032882dc8a144515ebf4b4e95fedeff0db2c3c3583557a85cfc40249c0b428f98c39e46fb6d6d1bbc7ea1f32a845287fa5dcb9/sw.cpp
@@ -0,0 +1,26 @@
+void build(Solution &s)
+{
+    auto &fmt = s.addTarget<LibraryTarget>("fmt", "8.1.0");
+    fmt += Git("https://github.com/fmtlib/fmt", "{v}");
+
+    fmt += cpp14;
+
+    fmt -= "src/fmt.cc";
+
+    fmt.Private += sw::Shared, "FMT_EXPORT"_d;
+    fmt.Public += sw::Shared, "FMT_SHARED"_d;
+
+    if (fmt.getCompilerType() == CompilerType::GNU ||
+        fmt.getCompilerType() == CompilerType::Clang ||
+        fmt.getCompilerType() == CompilerType::AppleClang)
+    {
+        fmt.ExportAllSymbols = true;
+    }
+
+    if (fmt.getCompilerType() == CompilerType::ClangCl ||
+        fmt.getCompilerType() == CompilerType::Clang ||
+        fmt.getCompilerType() == CompilerType::AppleClang)
+    {
+        fmt.Public += "FMT_USE_EXTERN_TEMPLATES=0"_d;
+    }
+}
