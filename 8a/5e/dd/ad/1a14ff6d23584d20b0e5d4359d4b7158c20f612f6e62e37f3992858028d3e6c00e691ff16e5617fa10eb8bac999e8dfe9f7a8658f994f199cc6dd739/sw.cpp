28ff87d9ff49d7d5a8480c26ce9038f9a243d353
CppanBot <cppanbot@gmail.com>
2022-01-07 15:07:54 +0000

    Update

--- 8a/5e/dd/ad/1a14ff6d23584d20b0e5d4359d4b7158c20f612f6e62e37f3992858028d3e6c00e691ff16e5617fa10eb8bac999e8dfe9f7a8658f994f199cc6dd739/sw.cpp
+++ 8a/5e/dd/ad/1a14ff6d23584d20b0e5d4359d4b7158c20f612f6e62e37f3992858028d3e6c00e691ff16e5617fa10eb8bac999e8dfe9f7a8658f994f199cc6dd739/sw.cpp
@@ -6,8 +6,13 @@ void build(Solution &s)
     auto &date = p.addTarget<StaticLibraryTarget>("date");
     date += "include/date/date.h";
 
-    auto &date_full = p.addTarget<StaticLibraryTarget>("date_full");
-    date_full += cpp11;
-    date_full += "org.sw.demo.badger.curl.libcurl"_dep;
+    auto &date_full = p.addLibrary("date_full");
+    date_full += cpp17;
+    //date_full += "org.sw.demo.badger.curl.libcurl"_dep;
     date_full -= "src/ios.mm";
+    date_full.Public += "HAS_REMOTE_API=0"_def;
+    if (date_full.getBuildSettings().TargetOS.Type != OSType::Windows)
+        date_full.Public += "USE_OS_TZDB=1"_def;
+    date_full += sw::Shared, "DATE_BUILD_DLL"_def;
+    date_full.Interface += sw::Shared, "DATE_USE_DLL"_def;
 }
