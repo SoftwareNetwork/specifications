f558a82ee6fb5896b0bf13df8c536a4cd8ff1592
CppanBot <cppanbot@gmail.com>
2022-01-29 21:46:12 +0000

    Update

---
+++ f5/a4/92/46/24e792c7757aab22e616a142fd1d451014eaa42446a6abd069f48cc2e4ef830998a4e571fa66b1cf7d3d1f1fd77c3a9193e5f959bfc3da770ce94762/sw.cpp
@@ -0,0 +1,7 @@
+void build(Solution &s)
+{
+    auto &charls = s.addTarget<LibraryTarget>("vbaderks.charls", "2.3.2");
+    charls += Git("https://github.com/team-charls/charls");
+
+    charls += "CHARLS_LIBRARY_BUILD"_def;
+}
