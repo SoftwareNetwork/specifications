2ea0301945d8cfb149b50570c521dcee586b6b65
CppanBot <cppanbot@gmail.com>
2022-01-31 16:46:23 +0000

    Update

--- 9e/f7/c4/04/c6ba5f7e36e93a7573afb89b8cfb0bfe737abb97739e571d0a062dfdf573e58e7b91232be2414070a990202719ee551c9e90c203799222a6f7bda2a5/sw.cpp
+++ 9e/f7/c4/04/c6ba5f7e36e93a7573afb89b8cfb0bfe737abb97739e571d0a062dfdf573e58e7b91232be2414070a990202719ee551c9e90c203799222a6f7bda2a5/sw.cpp
@@ -3,5 +3,6 @@ void build(Solution &s)
     auto &t = s.addTarget<StaticLibraryTarget>("blake2.blake2", "2019.07.24");
     t += Git("https://github.com/BLAKE2/BLAKE2", "{M}{m:02}{p:02}");
     t += "ref/blake.*"_r;
+    t.Public += "ref"_idir;
     //t += "sse/blake.*"_r;
 }
