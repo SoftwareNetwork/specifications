8b6359c9952b11603a35b396b2ab4c6726081145
CppanBot <cppanbot@gmail.com>
2022-02-07 18:36:50 +0000

    Update

---
+++ 70/76/d0/3b/27a1f70250fb3ee391dabe4f9a510a6da2a76730c09ea2de7bf21e850feaa834d0525a39f4aaad6ffcfaea8e5ea6b1fd1ec308e8c1a045e45f3f2656/sw.cpp
@@ -0,0 +1,42 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.c_io", "0.10.18");
+    t += Git("https://github.com/awslabs/aws-c-io", "v{v}");
+
+    t -=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t += "source/.*"_r;
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_IO_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_IO_USE_IMPORT_EXPORT"_d;
+
+    t -= "org.sw.demo.amazon.awslabs.s2n"_dep;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        t += "source/windows/.*"_rr;
+        t.Public += "AWS_USE_IO_COMPLETION_PORTS"_d;
+        t += "advapi32.lib"_slib;
+        t += "Crypt32.lib"_slib;
+        t += "Secur32.lib"_slib;
+        t += "Shlwapi.lib"_slib;
+        t += "ws2_32.lib"_slib;
+        t += "Ncrypt.lib"_slib;
+    }
+    else
+    {
+        t += "source/linux/.*"_rr;
+        t += "source/posix/.*"_rr;
+        t += "dl"_slib;
+        t += "source/s2n/.*"_r;
+        t += "org.sw.demo.amazon.awslabs.s2n"_dep;
+    }
+
+    t.Public += "org.sw.demo.amazon.awslabs.c_cal"_dep;
+}
