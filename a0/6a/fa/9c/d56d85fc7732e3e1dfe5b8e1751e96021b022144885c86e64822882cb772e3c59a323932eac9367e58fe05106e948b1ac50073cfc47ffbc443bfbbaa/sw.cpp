6e3e261a92efa7e09a385fa81caa251f913a52b8
CppanBot <cppanbot@gmail.com>
2022-02-21 19:01:13 +0000

    Update

---
+++ a0/6a/fa/9c/d56d85fc7732e3e1dfe5b8e1751e96021b022144885c86e64822882cb772e3c59a323932eac9367e58fe05106e948b1ac50073cfc47ffbc443bfbbaa/sw.cpp
@@ -0,0 +1,10 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.c_s3", "0.1.35");
+    t += Git("https://github.com/awslabs/aws-c-s3", "v{v}");
+
+    t.Private += sw::Shared, "AWS_S3_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_S3_USE_IMPORT_EXPORT"_d;
+
+    t.Public += "org.sw.demo.amazon.awslabs.c_auth"_dep;
+}
