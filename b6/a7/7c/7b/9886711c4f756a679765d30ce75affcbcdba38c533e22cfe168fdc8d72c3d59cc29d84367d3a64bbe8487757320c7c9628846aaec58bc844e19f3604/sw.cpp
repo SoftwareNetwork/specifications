f54b83b40e217959a7066ddd80d11cd99a4d44ba
CppanBot <cppanbot@gmail.com>
2022-02-28 22:53:08 +0000

    Update

---
+++ b6/a7/7c/7b/9886711c4f756a679765d30ce75affcbcdba38c533e22cfe168fdc8d72c3d59cc29d84367d3a64bbe8487757320c7c9628846aaec58bc844e19f3604/sw.cpp
@@ -0,0 +1,32 @@
+void build(Solution &s)
+{
+    auto &dbus = s.addLibrary("freedesktop.dbus", "1.14.0");
+    dbus += RemoteFile("https://dbus.freedesktop.org/releases/dbus/dbus-{v}.tar.xz");
+
+    dbus.ApiNames.insert({ "DBUS_EXPORT", "DBUS_PRIVATE_EXPORT" });
+    dbus += "cmake/config.h.cmake";
+    dbus += "dbus/.*"_rr;
+    dbus -= "dbus/.*test.*\\.c"_rr;
+
+    dbus += "DBUS_COMPILATION"_def;
+
+    dbus -= "dbus/.*unix.*\\.c"_rr;
+    dbus -= "dbus/.*pthread.*\\.c"_rr;
+    dbus -= "dbus/.*wince.*\\.c"_rr;
+    dbus -= "dbus/.*epoll.*\\.c"_rr;
+
+    dbus.Variables["DBUS_INT16_TYPE"] = "short";
+    dbus.Variables["DBUS_INT32_TYPE"] = "int";
+    dbus.Variables["DBUS_INT64_TYPE"] = "long long";
+    dbus.Variables["DBUS_INT64_CONSTANT"] = "\"LL\"";
+    dbus.Variables["DBUS_UINT64_CONSTANT"] = "\"ULL\"";
+
+    dbus.Variables["DBUS_VERSION"] = dbus.Variables["PACKAGE_VERSION"];
+    dbus.Variables["DBUS_MAJOR_VERSION"] = dbus.Variables["PACKAGE_VERSION_MAJOR"];
+    dbus.Variables["DBUS_MINOR_VERSION"] = dbus.Variables["PACKAGE_VERSION_MINOR"];
+    dbus.Variables["DBUS_MICRO_VERSION"] = dbus.Variables["PACKAGE_VERSION_PATCH"];
+
+    // TODO: add config vars
+    dbus.configureFile("cmake/config.h.cmake", dbus.BinaryPrivateDir / "config.h");
+    dbus.configureFile("dbus/dbus-arch-deps.h.in", "dbus/dbus-arch-deps.h");
+}
