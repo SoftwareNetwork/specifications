5a96c220fc07530b664a2cc8710b727523b5823a
CppanBot <cppanbot@gmail.com>
2022-04-05 15:41:33 +0000

    Update

---
+++ 69/dc/8e/a2/5a08f788007bb35ef1d438ef64efbb5180fcf9a6e92847bdadd0c427c97288583bb904e4c8a8b41cda6df32c67aaf77b86beffdbfda538c9ab22b242/sw.cpp
@@ -0,0 +1,23 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("madler.zlib", "1.2.12");
+    t += Git("https://github.com/madler/zlib", "v{v}");
+
+    //t += c89;
+    t.setChecks("zlib", true);
+
+    t += "[^/]*\\.[hc]"_rr;
+    t += sw::Shared, "ZLIB_DLL"_def;
+
+    if (!t.getBuildSettings().TargetOS.is(OSType::Windows))
+    {
+        t.ExportAllSymbols = true;
+        t += "HAVE_HIDDEN"_def;
+    }
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("zlib");
+    s.checkIncludeExists("unistd.h");
+}
