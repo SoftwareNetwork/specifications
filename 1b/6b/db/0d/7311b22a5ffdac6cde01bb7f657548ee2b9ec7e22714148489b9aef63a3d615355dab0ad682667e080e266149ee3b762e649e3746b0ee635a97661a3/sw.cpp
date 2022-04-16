a69e404af789eaefd0a678ccc9f52495c223142b
CppanBot <cppanbot@gmail.com>
2022-04-16 16:42:10 +0000

    Update

---
+++ 1b/6b/db/0d/7311b22a5ffdac6cde01bb7f657548ee2b9ec7e22714148489b9aef63a3d615355dab0ad682667e080e266149ee3b762e649e3746b0ee635a97661a3/sw.cpp
@@ -0,0 +1,33 @@
+void build(Solution &s)
+{
+    auto &libtorrent = s.addTarget<LibraryTarget>("arvidn.libtorrent", "1.2.15");
+    libtorrent += Git("https://github.com/arvidn/libtorrent", "v{v}");
+
+    libtorrent +=
+        "ed25519/src/.*"_rr,
+        "include/.*"_rr,
+        "src/.*"_rr;
+
+    libtorrent.Private += "TORRENT_BUILDING_LIBRARY"_d;
+    libtorrent.Public += "TORRENT_USE_ICONV"_d;
+    libtorrent.Public += "TORRENT_USE_LIBCRYPTO"_d;
+    libtorrent.Public += "TORRENT_USE_OPENSSL"_d;
+    if (libtorrent.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        libtorrent.Private += "BOOST_ASIO_ENABLE_CANCELIO"_d;
+        libtorrent.Public += "_WIN32_WINNT=0x0601"_d;
+        libtorrent.Public += "NOMINMAX"_d;
+        libtorrent.Public += "WIN32_LEAN_AND_MEAN"_d;
+        libtorrent.Public += "iphlpapi.lib"_slib;
+    }
+    libtorrent.Private += sw::Shared, "TORRENT_BUILDING_SHARED"_d;
+
+    libtorrent.Public += "org.sw.demo.boost.asio"_dep;
+    libtorrent.Public += "org.sw.demo.gnu.iconv.libiconv"_dep;
+    libtorrent.Public += "org.sw.demo.boost.crc"_dep;
+    libtorrent.Public += "org.sw.demo.boost.date_time"_dep;
+    libtorrent.Public += "org.sw.demo.openssl.ssl"_dep;
+    libtorrent.Public += "org.sw.demo.boost.scope_exit"_dep;
+    libtorrent.Public += "org.sw.demo.boost.system"_dep;
+    libtorrent.Public += "org.sw.demo.boost.multiprecision"_dep;
+}
