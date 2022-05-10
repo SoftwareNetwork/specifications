f00e6d104ff0132b4ba958d3d43ef9aa795c044b
CppanBot <cppanbot@gmail.com>
2022-05-10 12:13:54 +0000

    Update

--- 50/b6/bd/4b/25951a0db98463493312d0f7f539cb80ac6a8edf7e80b420a5bd2c0ba96a3d47111b373ce4f5d0c65254982d5cafc380d84872256f52dac1346f27fb/sw.cpp
+++ 50/b6/bd/4b/25951a0db98463493312d0f7f539cb80ac6a8edf7e80b420a5bd2c0ba96a3d47111b373ce4f5d0c65254982d5cafc380d84872256f52dac1346f27fb/sw.cpp
@@ -1,6 +1,8 @@
 void build(Solution &s)
 {
-    auto &websocketpp = s.addTarget<LibraryTarget>("zaphoyd.websocketpp", "0.8.2");
-    websocketpp += Git("https://github.com/zaphoyd/websocketpp");
-    websocketpp += "websocketpp/.*\\.hpp"_rr;
+    auto &t = s.addTarget<LibraryTarget>("zaphoyd.websocketpp", "0.8.2");
+    t += Git("https://github.com/zaphoyd/websocketpp");
+    t += "websocketpp/.*\\.hpp"_rr;
+    t.patch("websocketpp/endpoint.hpp", "~endpoint<connection,config>", "~endpoint");
+    t.patch("websocketpp/logger/basic.hpp", "<concurrency,names>", "");
 }
