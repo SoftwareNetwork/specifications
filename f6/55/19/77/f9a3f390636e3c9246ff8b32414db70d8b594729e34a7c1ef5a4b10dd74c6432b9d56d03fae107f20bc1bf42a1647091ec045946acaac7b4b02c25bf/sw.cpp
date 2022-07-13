66a1c527625fdf3c53fc27e362822c9617cb9070
CppanBot <cppanbot@gmail.com>
2022-07-13 15:10:07 +0000

    Update

---
+++ f6/55/19/77/f9a3f390636e3c9246ff8b32414db70d8b594729e34a7c1ef5a4b10dd74c6432b9d56d03fae107f20bc1bf42a1647091ec045946acaac7b4b02c25bf/sw.cpp
@@ -0,0 +1,35 @@
+void build(Solution &s)
+{
+    auto &abseil = s.addStaticLibrary("google.abseil", "2022.06.23.0");
+    abseil += Git("https://github.com/abseil/abseil-cpp", "{M}{m:02}{p:02}.{t}");
+
+    {
+        auto &t = abseil;
+        t += cpp17;
+        SwapAndRestore sr(t.SourceDir, t.SourceDir / "absl");
+        t += ".*\\.cc"_rr;
+        t += ".*\\.h"_rr;
+        t += ".*\\.inc"_rr;
+        //t -= ".*_benchmark.*cc"_rr;
+        // this includes removal of nanobenchmark
+        // nanobenchmark.c does not build in x86
+        t -= ".*benchmark.*cc"_rr;
+        t -= ".*_test.*cc"_rr;
+        t -= ".*_generator.*cc"_rr;
+        t -= ".*_gentables.*cc"_rr;
+        //t -= ".*_nonprod.*cc"_rr;
+        t -= ".*/test_.*cc"_rr;
+        t -= "hash/internal/print_hash_of.cc";
+        t -= "random/benchmarks.cc";
+        //t -= "random/mocking_bit_gen.cc";
+        //t -= "synchronization/internal/waiter.cc";
+        t += "random/internal/distribution_test_util.cc";
+
+        if (abseil.getBuildSettings().TargetOS.is(OSType::Windows) || abseil.getBuildSettings().TargetOS.is(OSType::Mingw))
+        {
+            abseil.Public += "NOMINMAX"_def;
+            abseil += "Dbghelp.lib"_slib;
+        }
+        abseil += "CoreFoundation"_framework;
+    }
+}
