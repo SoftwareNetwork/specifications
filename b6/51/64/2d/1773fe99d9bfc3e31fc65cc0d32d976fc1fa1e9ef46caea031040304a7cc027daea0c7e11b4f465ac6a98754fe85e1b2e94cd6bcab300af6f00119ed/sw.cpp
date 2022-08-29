80020cd48d0c1084f13697b0d3b00d986c849a08
CppanBot <cppanbot@gmail.com>
2022-08-29 19:17:25 +0000

    Update

---
+++ b6/51/64/2d/1773fe99d9bfc3e31fc65cc0d32d976fc1fa1e9ef46caea031040304a7cc027daea0c7e11b4f465ac6a98754fe85e1b2e94cd6bcab300af6f00119ed/sw.cpp
@@ -0,0 +1,7 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("marzer.tomlplusplus", "3.2.0");
+    t += Git("https://github.com/marzer/tomlplusplus", "v{v}");
+    t += "include/.*"_rr;
+    t.Public += "toml++.natvis";
+}
