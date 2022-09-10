d7ea14331b629f75e29c6bb958b1d3843d69eb83
CppanBot <cppanbot@gmail.com>
2022-09-10 12:09:00 +0000

    Update

---
+++ af/1d/42/46/5d10aaabb1a0eb2cd9e50715d8c951bd5fefdad8be6c229510dd194ef2027b8ddad5e04456d3a24fc201a9f848b4116f00097a32186bb936cf007032/sw.cpp
@@ -0,0 +1,19 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("brainboxdotcc.dpp", "10.0.19");
+    t += Git("https://github.com/brainboxdotcc/DPP", "v{v}");
+
+    t += cpp17;
+    t += "include/dpp/.*"_rr;
+    t += "src/dpp/.*"_rr;
+    t.Public += sw::Static, "DPP_STATIC"_def;
+    t += "DPP_BUILD"_def;
+    t.Public += "include"_idir;
+    //t += "include/dpp"_idir;
+    t += "org.sw.demo.madler.zlib"_dep;
+    t += "org.sw.demo.openssl.ssl"_dep;
+    //t += "org.sw.demo.nlohmann.json"_dep;
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("/bigobj");
+    t.pushBackToFileOnce("include/dpp/nlohmann/json_fwd.hpp", "#include \"json.hpp\"");
+}
