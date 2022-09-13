d7232ace1f1a1db9ef10196604bd49aab69f8571
CppanBot <cppanbot@gmail.com>
2022-09-13 17:09:01 +0000

    Update

---
+++ ac/36/c5/b2/4372285d5e033ddf3cbea901dd1e52f28ce71b978df2221abfe4fcef0b3da3cdcc37591faa06b944634b2b86fc77c6d7348103fef43151e8dc3cd1cf/sw.cpp
@@ -0,0 +1,23 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.s2n", "1.3.21");
+    t += Git("https://github.com/awslabs/s2n", "v{v}");
+
+    t += "S2N_API"_api;
+
+    t += "api/.*"_rr;
+    t += "crypto/.*"_rr;
+    t += "error/.*"_rr;
+    t += "stuffer/.*"_rr;
+    t += "tls/.*"_rr;
+    t += "utils/.*"_rr;
+    t += "pq-crypto/.*"_rr;
+
+    t.Public += "api"_idir;
+    t += "."_idir;
+
+    t.patch("api/s2n.h", "#    define S2N_API", "//#     define S2N_API");
+    t.patch("utils/s2n_asn1_time.c", "__tm_gmtoff", "tm_gmtoff");
+
+    t.Public += "org.sw.demo.openssl.crypto"_dep;
+}
