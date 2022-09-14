299ba298206c2fcf8aad74b892af258f1dd75a25
CppanBot <cppanbot@gmail.com>
2022-09-14 11:09:00 +0000

    Update

---
+++ 76/a3/2c/de/2b82c31b9a666b55259fdf693c9c092b02389bbece2891658d724801fae594939a757226e14de5d195fdab653eebbb8ff89ff383bf63d0cd14f825ae/sw.cpp
@@ -0,0 +1,15 @@
+void build(Solution &s)
+{
+    auto &webp = s.addTarget<LibraryTarget>("webmproject.webp", "1.2.3");
+    webp += Git("https://github.com/webmproject/libwebp", "v{v}");
+
+    webp.ApiName = "WEBP_API";
+    webp +=
+        "src/.*\\.c"_rr,
+        "src/.*\\.h"_rr;
+
+    webp += "."_id;
+    webp.Public += "src"_id;
+
+    webp.replaceInFileOnce("src/webp/types.h", "#ifndef WEBP_EXTERN", "#define WEBP_EXTERN extern " + webp.ApiName + "\n#ifndef WEBP_EXTERN");
+}
