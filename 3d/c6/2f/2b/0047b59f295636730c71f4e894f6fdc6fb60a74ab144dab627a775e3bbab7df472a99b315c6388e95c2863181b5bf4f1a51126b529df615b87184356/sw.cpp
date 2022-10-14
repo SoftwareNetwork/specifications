e7cd95e451acee0e855a6106b702b1a358005986
CppanBot <cppanbot@gmail.com>
2022-10-14 12:09:02 +0000

    Update

---
+++ 3d/c6/2f/2b/0047b59f295636730c71f4e894f6fdc6fb60a74ab144dab627a775e3bbab7df472a99b315c6388e95c2863181b5bf4f1a51126b529df615b87184356/sw.cpp
@@ -0,0 +1,45 @@
+void build(Solution &s)
+{
+    auto &libharu = s.addTarget<LibraryTarget>("libharu", "2.4.3");
+    libharu += Git("https://github.com/libharu/libharu", "v{v}");
+
+    libharu.setChecks("libharu");
+
+    libharu +=
+        "include/.*"_rr,
+        "src/.*"_rr;
+
+    libharu.Private +=
+        "src"_id;
+
+    libharu.Public +=
+        "include"_id;
+
+    if (libharu.getBuildSettings().TargetOS.Type == OSType::Windows)
+    {
+        libharu.Private += sw::Shared, "HPDF_DLL_MAKE_CDECL"_d;
+        libharu.Interface += sw::Shared, "HPDF_DLL_CDECL"_d;
+    }
+    else
+    {
+        libharu.ApiName = "SW_LIBHARU_API";
+        libharu += "HPDF_SHARED_MAKE"_d;
+        libharu.Interface += "HPDF_SHARED"_d;
+        libharu.replaceInFileOnce("include/hpdf.h", "extern A", "SW_LIBHARU_API A");
+    }
+
+    libharu.Public += "org.sw.demo.glennrp.png"_dep;
+    libharu.configureFile("include/hpdf_config.h.cmake", "hpdf_config.h");
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("libharu");
+    s.checkIncludeExists("stdint.h");
+    s.checkIncludeExists("strings.h");
+    s.checkIncludeExists("string.h");
+    s.checkIncludeExists("unistd.h");
+    s.checkTypeSize("size_t");
+    s.checkTypeSize("void *");
+    s.checkLibraryFunctionExists("m", "floor");
+}
