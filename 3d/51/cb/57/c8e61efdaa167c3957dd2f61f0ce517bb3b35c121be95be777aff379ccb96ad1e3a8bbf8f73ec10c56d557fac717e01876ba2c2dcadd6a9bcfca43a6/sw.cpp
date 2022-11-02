77baf5f2b143db6f49ae70eacd954d59d9bc9025
CppanBot <cppanbot@gmail.com>
2022-11-02 12:09:31 +0000

    Update

---
+++ 3d/51/cb/57/c8e61efdaa167c3957dd2f61f0ce517bb3b35c121be95be777aff379ccb96ad1e3a8bbf8f73ec10c56d557fac717e01876ba2c2dcadd6a9bcfca43a6/sw.cpp
@@ -0,0 +1,6 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("msgpack", "4.1.3");
+    t += Git("https://github.com/msgpack/msgpack-c", "cpp-{v}");
+    t.ApiName = "MSGPACK_DLLEXPORT";
+}
