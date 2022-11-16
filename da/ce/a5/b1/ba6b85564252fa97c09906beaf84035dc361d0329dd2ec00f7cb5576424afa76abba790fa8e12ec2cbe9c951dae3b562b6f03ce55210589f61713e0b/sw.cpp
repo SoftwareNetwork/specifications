afd021fc509a515f202efbfad0a3b3f40b6de0ad
CppanBot <cppanbot@gmail.com>
2022-11-16 20:21:22 +0000

    Update

---
+++ da/ce/a5/b1/ba6b85564252fa97c09906beaf84035dc361d0329dd2ec00f7cb5576424afa76abba790fa8e12ec2cbe9c951dae3b562b6f03ce55210589f61713e0b/sw.cpp
@@ -0,0 +1,25 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("catchorg.catch2", "3.2.0");
+    t += Git("https://github.com/catchorg/Catch2", "v{v}");
+
+    t += cpp17;
+    t.WholeArchive = true;
+    t += "src/.*"_rr;
+
+    //t.Variables["CATCH_CONFIG_ANDROID_LOGWRITE"] = "";
+    //t.Variables["CATCH_CONFIG_COLOUR_WIN32"] = "";
+    //t.Variables["CATCH_CONFIG_COUNTER"] = "";
+
+    //t.Variables["CATCH_CONFIG_CPP11_TO_STRING"] = "";
+    //t.Variables["CATCH_CONFIG_CPP17_BYTE"] = "";
+    //t.Variables["CATCH_CONFIG_CPP17_OPTIONAL"] = "";
+    //t.Variables["CATCH_CONFIG_CPP17_STRING_VIEW"] = "";
+    //t.Variables["CATCH_CONFIG_CPP17_UNCAUGHT_EXCEPTIONS"] = "";
+    //t.Variables["CATCH_CONFIG_CPP17_VARIANT"] = "";
+
+    t.Variables["CATCH_CONFIG_DEFAULT_REPORTER"] = "console";
+    t.Variables["CATCH_CONFIG_CONSOLE_WIDTH"] = "80";
+    //t.Variables["CATCH_CONFIG_FALLBACK_STRINGIFIER"] = "";
+    t.configureFile("src/catch2/catch_user_config.hpp.in", "catch2/catch_user_config.hpp");
+}
