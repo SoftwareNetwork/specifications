0e86f70aebd1b9dc2b3698045c01c93cf751ce68
CppanBot <cppanbot@gmail.com>
2022-11-23 00:19:47 +0000

    Update

---
+++ fa/79/c9/18/493dc3b64f4b729b3ae7f20b8cfd952ca5dadd3754d91853ec3c997bd55aea5644e04c5ebbdb1d6ce048cf85e6a3f2e1ba9fc593d14a2a7db89c8fdb/sw.cpp
@@ -0,0 +1,26 @@
+void build(Solution &s)
+{
+    auto &flatbuffers = s.addTarget<StaticLibraryTarget>("google.flatbuffers", "22.11.22");
+    flatbuffers += Git("https://github.com/google/flatbuffers", "v{v}");
+    {
+        flatbuffers +=
+            "include/.*"_rr,
+            "src/code_generators.cpp",
+            "src/idl_gen_fbs.cpp",
+            "src/idl_gen_text.cpp",
+            "src/idl_parser.cpp",
+            "src/reflection.cpp",
+            "src/util.cpp";
+    }
+
+    auto &flatc = flatbuffers.addTarget<ExecutableTarget>("flatc");
+    {
+        flatc +=
+            "grpc/src/compiler/.*"_rr,
+            "src/.*"_rr;
+        flatc -= "src/flathash.cpp";
+        flatc.Public +=
+            "grpc"_id;
+        (flatc + flatbuffers)->IncludeDirectoriesOnly = true;
+    }
+}
