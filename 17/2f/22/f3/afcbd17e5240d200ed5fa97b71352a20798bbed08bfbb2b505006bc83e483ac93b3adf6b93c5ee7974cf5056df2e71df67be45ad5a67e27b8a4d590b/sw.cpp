aa5cbbd227e76e5707fe54ad71b0ac05464a4cb2
CppanBot <cppanbot@gmail.com>
2022-11-26 12:19:40 +0000

    Update

---
+++ 17/2f/22/f3/afcbd17e5240d200ed5fa97b71352a20798bbed08bfbb2b505006bc83e483ac93b3adf6b93c5ee7974cf5056df2e71df67be45ad5a67e27b8a4d590b/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("BLAKE3_team.BLAKE3", "1.3.3");
+    t += Git("https://github.com/BLAKE3-team/BLAKE3");
+
+    t.setRootDirectory("c");
+    t += "blake3.*"_r;
+    t -= "blake3_neon.c";
+}
