b279edacac439fd2b6f75d9df9a6740c01e055bc
CppanBot <cppanbot@gmail.com>
2022-12-20 01:19:40 +0000

    Update

---
+++ f5/4e/2e/ed/fee46c7bf9881543096307d517f1b1f297765f9f84a05faadcf8afab8327900c442ac34e34a4af8ed6c1d9345b111fe20a3e1da9058592e1534a0e94/sw.cpp
@@ -0,0 +1,12 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("whoshuu.cpr", "1.9.3");
+    t += Git("https://github.com/whoshuu/cpr");
+
+    t += cpp17;
+
+    t += "include/.*"_rr;
+    t += "cpr/.*"_rr;
+
+    t.Public += "org.sw.demo.badger.curl.libcurl"_dep;
+}
