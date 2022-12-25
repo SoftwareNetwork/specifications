503c9a16736b6995afaa3e2dab4a48666e2a3df2
CppanBot <cppanbot@gmail.com>
2022-12-25 13:19:48 +0000

    Update

---
+++ cc/bc/01/e2/efebee91d9ddb1bb93a76e767b565d10a003b6f765dcfbf2cda90795b3cae3ae99cb8c93aa170985b1f12fbd11e1cc30e657ba77aa927928b90c0a11/sw.cpp
@@ -0,0 +1,31 @@
+void build(Solution &s)
+{
+    auto &libogg = s.addTarget<LibraryTarget>("xiph.libogg", "1.3.5");
+    libogg += RemoteFile("https://downloads.xiph.org/releases/ogg/libogg-{v}.tar.gz");
+
+    libogg +=
+        "include/.*\\.h"_rr,
+        "include/.*\\.in"_rr,
+        "src/.*\\.[hc]"_rr,
+        "win32/ogg.def";
+
+    libogg.Private +=
+        "src"_id;
+    libogg.Public +=
+        "include"_id;
+
+    libogg.writeFileOnce("ogg/config_types.h", R"(
+        #pragma once
+
+        #  include <stdint.h>
+
+        typedef int16_t ogg_int16_t;
+        typedef uint16_t ogg_uint16_t;
+        typedef int32_t ogg_int32_t;
+        typedef uint32_t ogg_uint32_t;
+        typedef int64_t ogg_int64_t;
+        )"
+    );
+
+    libogg.patch("win32/ogg.def", "LIBRARY ogg", "LIBRARY");
+}
