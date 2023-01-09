06a9228109f0eda2eea43042b80140f448692375
CppanBot <cppanbot@gmail.com>
2023-01-09 19:06:18 +0000

    Update

---
+++ b5/bf/18/a0/aecb35fff5a122c7737a2bbcc56403e65221535b3df7ca78b2a98dd6665cdc0ebaa975bb14e6d3a12cf441de86e61e1286d94e7e338801806f05d097/sw.cpp
@@ -0,0 +1,6 @@
+void build(Solution &s)
+{
+    auto &args = s.addTarget<LibraryTarget>("taywee.args", "6.4.5");
+    args += Git("https://github.com/Taywee/args", "{v}");
+    args += "args.hxx";
+}
