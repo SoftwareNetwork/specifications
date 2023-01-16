eaada4eebb29ac47fd036740eed7302aad5a0095
CppanBot <cppanbot@gmail.com>
2023-01-16 22:06:22 +0000

    Update

---
+++ 7f/75/df/07/1810cc7a08d39300401484f00bf6cbc38874e12e9093c5b4e1d75047bcd6cebc366a2311e10d1d2a49ad8a6f51d8209a6b8204add513cf68b61eae62/sw.cpp
@@ -0,0 +1,25 @@
+void build(Solution &s)
+{
+    auto &mpfr = s.addTarget<LibraryTarget>("gnu.mpfr", "4.2.0");
+    mpfr += RemoteFile("https://www.mpfr.org/mpfr-current/mpfr-{v}.tar.xz");
+
+    mpfr.setRootDirectory("src");
+    mpfr += ".*"_rr;
+
+    mpfr -=
+        "jyn_asympt.c",
+        "round_raw_generic.c";
+
+    mpfr.Private += "MPFR_TUNE_CASE=\"\""_d;
+    mpfr.Public += "org.sw.demo.gnu.gmp.lib"_dep;
+
+    if (mpfr.getBuildSettings().TargetOS.is(ArchType::x86))
+        mpfr += "x86"_idir;
+    else if (mpfr.getBuildSettings().TargetOS.is(ArchType::x86_64))
+        mpfr += "x86_64/core2"_idir;
+
+    if (mpfr.Variables["WORDS_BIGENDIAN"] == 1)
+        mpfr += "HAVE_BIG_ENDIAN"_def;
+    else
+        mpfr += "HAVE_LITTLE_ENDIAN"_def;
+}
