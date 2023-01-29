26869bbe1caf193469d5dc888af4f1bdbc488522
CppanBot <cppanbot@gmail.com>
2023-01-29 13:06:13 +0000

    Update

---
+++ 93/44/71/2c/5e1f2754715c5b0534c43e3aec8d8dfffa69bb83d8145b3816f899b56a5d543876dd658eadd63cb2d0d7a12f1a7d9a6efafc3e1815bc554b81a24f04/sw.cpp
@@ -0,0 +1,6 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("pantor.inja", "3.4.0");
+    t += Git("https://github.com/pantor/inja", "v{v}");
+    t.Public += "org.sw.demo.nlohmann.json"_dep;
+}
