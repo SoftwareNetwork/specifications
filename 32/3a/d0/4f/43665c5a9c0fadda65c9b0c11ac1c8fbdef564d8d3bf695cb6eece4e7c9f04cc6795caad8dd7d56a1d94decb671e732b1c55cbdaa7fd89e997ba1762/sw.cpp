89215c055fa6e40db3b8b2133a68a598da29bed0
CppanBot <cppanbot@gmail.com>
2023-02-07 16:06:22 +0000

    Update

---
+++ 32/3a/d0/4f/43665c5a9c0fadda65c9b0c11ac1c8fbdef564d8d3bf695cb6eece4e7c9f04cc6795caad8dd7d56a1d94decb671e732b1c55cbdaa7fd89e997ba1762/sw.cpp
@@ -0,0 +1,6 @@
+void build(Solution &s)
+{
+    auto &cpp_httplib = s.addTarget<LibraryTarget>("yhirose.cpp_httplib", "0.12.0");
+    cpp_httplib += Git("https://github.com/yhirose/cpp-httplib", "v{v}");
+    cpp_httplib += "httplib.h";
+}
