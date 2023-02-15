594085a6097d18629c61b4b3cbc3885bd3979c5d
CppanBot <cppanbot@gmail.com>
2023-02-15 21:06:27 +0000

    Update

---
+++ e5/71/b6/24/63c2b4781a86b0749c77921051c9df0433b89a938dd2e8555947ee05e677fc300310f46ea64bd27591af5c24e2db5ef401e308729c205dac58d0b822/sw.cpp
@@ -0,0 +1,6 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibraryTarget>("jarro2783.cxxopts", "3.1.1");
+    t += Git("https://github.com/jarro2783/cxxopts", "v{v}");
+    t += "include/.*"_rr;
+}
