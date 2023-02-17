130d44667ec7a8d4345c70d1f1468e06efb445da
CppanBot <cppanbot@gmail.com>
2023-02-17 02:06:22 +0000

    Update

--- 02/b7/0f/2d/de5144a03812430be4aa5b95455b33551b9a15a9c5c768db750d67c07fd3933c1cf720e3abca1d629494f9f8d76ed63402f1c5a1b0c642e4c2974889/sw.cpp
+++ 02/b7/0f/2d/de5144a03812430be4aa5b95455b33551b9a15a9c5c768db750d67c07fd3933c1cf720e3abca1d629494f9f8d76ed63402f1c5a1b0c642e4c2974889/sw.cpp
@@ -9,21 +9,22 @@ void build(Solution &s)
         SwapAndRestore sr(t.SourceDir, t.SourceDir / "absl");
         t += ".*\\.cc"_rr;
         t += ".*\\.h"_rr;
-        //t += ".*\\.inc"_rr;
+        t += ".*\\.inc"_rr;
         //t -= ".*_benchmark.*cc"_rr;
         // this includes removal of nanobenchmark
         // nanobenchmark.c does not build in x86
         t -= ".*benchmark.*cc"_rr;
         t -= ".*_test.*cc"_rr;
-        //t -= ".*_generator.*cc"_rr;
-        //t -= ".*_gentables.*cc"_rr;
+        t -= ".*_mock.*cc"_rr;
+        t -= ".*_generator.*cc"_rr;
+        t -= ".*_gentables.*cc"_rr;
         //t -= ".*_nonprod.*cc"_rr;
-        //t -= ".*/test_.*cc"_rr;
-        //t -= "hash/internal/print_hash_of.cc";
-        //t -= "random/benchmarks.cc";
+        t -= ".*/test_.*cc"_rr;
+        t -= "hash/internal/print_hash_of.cc";
+        t -= "random/benchmarks.cc";
         //t -= "random/mocking_bit_gen.cc";
         //t -= "synchronization/internal/waiter.cc";
-        //t += "random/internal/distribution_test_util.cc";
+        t += "random/internal/distribution_test_util.cc";
 
         if (abseil.getBuildSettings().TargetOS.is(OSType::Windows) || abseil.getBuildSettings().TargetOS.is(OSType::Mingw))
         {
