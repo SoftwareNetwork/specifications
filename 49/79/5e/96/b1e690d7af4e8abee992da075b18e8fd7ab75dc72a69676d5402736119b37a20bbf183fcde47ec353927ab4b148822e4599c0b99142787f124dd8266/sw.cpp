e91ed3f5b072a04b935dca1b6eb321a3b323d387
CppanBot <cppanbot@gmail.com>
2023-02-17 20:24:46 +0000

    Update

--- 49/79/5e/96/b1e690d7af4e8abee992da075b18e8fd7ab75dc72a69676d5402736119b37a20bbf183fcde47ec353927ab4b148822e4599c0b99142787f124dd8266/sw.cpp
+++ 49/79/5e/96/b1e690d7af4e8abee992da075b18e8fd7ab75dc72a69676d5402736119b37a20bbf183fcde47ec353927ab4b148822e4599c0b99142787f124dd8266/sw.cpp
@@ -4,6 +4,7 @@ void build(Solution &s)
     p += Git("https://github.com/protocolbuffers/utf8_range");
 
     auto &r = p.addStaticLibrary("utf8_range");
+    r += cpp17;
     r +=
         "naive.c",
         "range2-neon.c",
@@ -12,9 +13,11 @@ void build(Solution &s)
         ;
 
     auto &v = p.addStaticLibrary("utf8_validity");
+    v += cpp17;
     v +=
         "utf8_validity.cc",
         "utf8_validity.h"
         ;
     v.Public += "org.sw.demo.google.abseil"_dep;
+    //v.CompileOptions.push_back("/arch:AVX");
 }
