414b7f62c84fdcc01e693fa0fa3a450f27b648ea
CppanBot <cppanbot@gmail.com>
2023-04-06 15:45:06 +0000

    Update

---
+++ 89/e9/d5/36/7546ff8e364318e47aa92bcb7ca6e4a1de11d5cfb17e566b9bc2e1e9e2260a17ec4b65d635999e399d1dee215f9517dfd8769f15bc3fdbda834fb1a5/sw.cpp
@@ -0,0 +1,25 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("sqlite3", "3.41.1");
+    {
+        auto tv = time(0);
+        t += RemoteFile("http://www.sqlite.org/" + std::to_string(localtime(&tv)->tm_year + 1900) + "/sqlite-amalgamation-{M}{m:02}{p:02}00.zip");
+        t += "sqlite3.[hc]"_r;
+        t += "sqlite3ext.h";
+        t.ApiName = "SQLITE_API";
+
+        t += "SQLITE_ENABLE_COLUMN_METADATA"_def;
+        t += "SQLITE_ENABLE_FTS5"_def;
+
+        if (!t.getBuildSettings().TargetOS.is(OSType::Windows) && !t.getBuildSettings().TargetOS.is(OSType::Mingw))
+        {
+            t += "dl"_slib;
+            if (!t.getBuildSettings().TargetOS.Android)
+                t += "pthread"_slib;
+        }
+    }
+
+    auto &sh = t.addExecutable("shell");
+    sh += "shell.c";
+    sh += t;
+}
