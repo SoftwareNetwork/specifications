ba960f416e4728a43aa3e41af16a7bdd82006ec3
CppanBot <cppanbot@gmail.com>
2023-08-15 10:45:47 +0000

    Update

---
+++ 54/71/bb/74/a33bdd3cda312abc280d86b5f91dbd8f799cbb4b39c14b6a558a6b14a8ef64240fa4c6d713d5792e93ef1404d1cc53db03c5ca73e0dbd4b795fa23c2/sw.cpp
@@ -0,0 +1,19 @@
+void build(Solution &s)
+{
+    auto &yaml_cpp = s.addTarget<LibraryTarget>("jbeder.yaml_cpp", "0.8.0");
+    yaml_cpp += Git("https://github.com/jbeder/yaml-cpp");
+
+    yaml_cpp += "include/.*"_rr;
+    yaml_cpp += "src/.*"_rr;
+
+    yaml_cpp += "YAML_CPP_API"_api;
+    yaml_cpp += cpp11;
+    yaml_cpp.writeFileOnce("include/yaml-cpp/dll.h");
+
+    yaml_cpp.Public += "org.sw.demo.boost.iterator"_dep;
+    yaml_cpp.Public += "org.sw.demo.boost.smart_ptr"_dep;
+
+    yaml_cpp.writeFileOnce("include/yaml-cpp/dll.h", "");
+    yaml_cpp.patch("include/yaml-cpp/node/detail/node.h", "static std::atomic<size_t> m_amount;", "static YAML_CPP_API std::atomic<size_t> m_amount;");
+    yaml_cpp.patch("src/node_data.cpp", "std::atomic<size_t> node::m_amount{0};", "YAML_CPP_API std::atomic<size_t>  node::m_amount{0};");
+}
