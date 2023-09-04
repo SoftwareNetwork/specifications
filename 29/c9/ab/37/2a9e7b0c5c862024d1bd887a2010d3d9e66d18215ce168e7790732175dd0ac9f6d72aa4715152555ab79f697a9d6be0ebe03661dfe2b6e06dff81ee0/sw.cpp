0d8a770b871c39db7b6d3a6f2c5ef539de59a346
CppanBot <cppanbot@gmail.com>
2023-09-04 16:54:59 +0000

    Update

---
+++ 29/c9/ab/37/2a9e7b0c5c862024d1bd887a2010d3d9e66d18215ce168e7790732175dd0ac9f6d72aa4715152555ab79f697a9d6be0ebe03661dfe2b6e06dff81ee0/sw.cpp
@@ -0,0 +1,72 @@
+#pragma sw require header org.sw.demo.lexxmark.winflexbison.bison
+
+void build(Solution &s)
+{
+    auto &thrift = s.addTarget<LibraryTarget>("apache.thrift", "0.19.0");
+    thrift += Git("https://github.com/apache/thrift", "v{v}");
+
+    auto &c = thrift.addTarget<ExecutableTarget>("compiler");
+    {
+        c.setRootDirectory("compiler/cpp");
+
+        c += "src/.*"_rr;
+        c -= "src/thrift/logging.cc";
+        //c.configureFile("src/thrift/version.h.in", "thrift/version.h");
+
+        gen_bison("org.sw.demo.lexxmark.winflexbison"_dep, c, "src/thrift/thrifty.yy", "thrift/thrifty.cc");
+        gen_flex("org.sw.demo.lexxmark.winflexbison"_dep, c, "src/thrift/thriftl.ll");
+
+        if (c.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            c += "Ole32.lib"_slib;
+        }
+    }
+
+    // thrift
+    {
+        thrift.PackageDefinitions = true;
+        thrift.setRootDirectory("lib/cpp");
+        thrift.setChecks("thrift");
+
+        thrift +=
+            "src/thrift/.*\\.cpp"_rr,
+            "src/thrift/.*\\.h"_rr,
+            "src/thrift/.*\\.tcc"_rr;
+
+        thrift += sw::Shared, "thrift_EXPORTS"_def;
+        thrift.Public += sw::Static, "THRIFT_STATIC_DEFINE"_def;
+
+        thrift.Public += "org.sw.demo.boost.locale"_dep;
+        thrift.Public += "org.sw.demo.boost.scope_exit"_dep;
+        thrift.Public += "org.sw.demo.boost.system"_dep;
+        thrift.Public += "org.sw.demo.boost.thread"_dep;
+        //thrift.Public += "org.sw.demo.libressl.ssl"_dep;
+        thrift.Public += "org.sw.demo.openssl.ssl"_dep;
+        thrift.Public += "org.sw.demo.madler.zlib"_dep;
+        thrift.Public += "org.sw.demo.libevent"_dep;
+
+        if (thrift.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            thrift.configureFile("src/thrift/windows/config.h", "thrift/config.h");
+            thrift -= "src/thrift/VirtualProfiling.cpp";
+            thrift += "Shlwapi.lib"_slib;
+            thrift += "Ole32.lib"_slib;
+        }
+        else
+        {
+            thrift -= "src/thrift/windows/.*"_rr;
+        }
+
+        // remove non blocking (needs unix?)
+        thrift -= "src/thrift/server/TNonblockingServer.cpp";
+        thrift -= "src/thrift/qt/.*"_rr;
+    }
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("thrift");
+    s.checkIncludeExists("stdlib.h");
+    s.checkTypeSize("size_t");
+    s.checkTypeSize("void *");
+}
---
+++ ec/47/f8/71/0b49e6b7b5cba9bd272fbe13b2e2a07f557b1381f9e84a4f003299b6944e846c9177dc7aaaf88ae9154b6a9f1f207f36f0b63a2c35ba192ae08c71a1/sw.cpp
@@ -0,0 +1,11 @@
+void build(Solution &s)
+{
+    auto &re2 = s.addTarget<StaticLibraryTarget>("google.re2", "2023.9.1");
+    re2 += Git("https://github.com/google/re2", "{M}-{m:02}-{p:02}");
+
+    re2 += cpp11;
+    re2.ImportFromBazel = true;
+    re2.BazelTargetName = "re2";
+    re2.Public += "."_idir;
+    re2 += "NOMINMAX"_def;
+}
