d25e7d5cde73f0e5bcd9dbe648d144ff2fd0a5ea
CppanBot <cppanbot@gmail.com>
2023-09-22 01:09:30 +0000

    Update

--- aa/e8/4e/27/f03edaf70818b6ea334bcbc87a806950e0dba7888c2f833a6b672968df15ed7f345c1b8224e64e55a7609e2c159be8faaf6c08a41001bb8ea8dc98e7/sw.cpp
+++ aa/e8/4e/27/f03edaf70818b6ea334bcbc87a806950e0dba7888c2f833a6b672968df15ed7f345c1b8224e64e55a7609e2c159be8faaf6c08a41001bb8ea8dc98e7/sw.cpp
@@ -4,4 +4,6 @@ void build(Solution &s)
     rapidjson += Git("https://github.com/miloyip/rapidjson", "v{v}");
     rapidjson += "include/.*"_rr;
     rapidjson.Public += "RAPIDJSON_HAS_STDSTRING"_d;
+    if (rapidjson.getBuildSettings().TargetOS.Arch == ArchType::aarch64)
+        rapidjson.Public += "RAPIDJSON_ENDIAN=RAPIDJSON_LITTLEENDIAN"_def;
 }
