86c4c3e535689dac56008e45dbdc0e06b44c5cc7
CppanBot <cppanbot@gmail.com>
2023-10-12 15:09:51 +0000

    Update

---
+++ 5e/10/fd/b2/d5705c0bb9b331e2226cc0daa4e95ef528a5f122d3c5e39f3c3f4ec9ed2f13e6216182179dc4c26207b3f660cd18a1e83f344f59c27eec1f6b2b8070/sw.cpp
@@ -0,0 +1,14 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("gpuopen.D3D12MemoryAllocator", "2.0.1");
+    t += Git("https://github.com/GPUOpen-LibrariesAndSDKs/D3D12MemoryAllocator", "v{v}");
+
+    t += "include/.*"_rr;
+    t += "src/D3D12MemAlloc.cpp";
+    t.Public += "src/D3D12MemAlloc.natvis";
+
+    t += sw::Shared, "D3D12MA_EXPORTS"_def;
+    t.Interface += sw::Shared, "D3D12MA_IMPORTS"_def;
+
+    t += "uuid.lib"_slib;
+}
