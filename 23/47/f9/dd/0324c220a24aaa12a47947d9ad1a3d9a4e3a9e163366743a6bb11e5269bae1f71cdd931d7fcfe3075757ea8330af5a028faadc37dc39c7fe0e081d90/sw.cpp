a0678cf343986febee3b282d425a00449ec2bf47
CppanBot <cppanbot@gmail.com>
2023-10-17 03:09:35 +0000

    Update

---
+++ 23/47/f9/dd/0324c220a24aaa12a47947d9ad1a3d9a4e3a9e163366743a6bb11e5269bae1f71cdd931d7fcfe3075757ea8330af5a028faadc37dc39c7fe0e081d90/sw.cpp
@@ -0,0 +1,36 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("xorg.proto", "2023.2.0");
+    t += Git("https://gitlab.freedesktop.org/xorg/proto/xorgproto", "xorgproto-{M}.{m}");
+
+    t.setChecks("x");
+
+    t += "include/.*\\.h"_rr;
+
+    if (t.Variables["HAVE_FD_SET_FDS_BITS"] == 1)
+        t.Variables["USE_FDS_BITS"] = "fds_bits";
+    else if (t.Variables["HAVE_FD_SET___FDS_BITS"] == 1)
+        t.Variables["USE_FDS_BITS"] = "__fds_bits";
+    else if (!t.DryRun && t.getBuildSettings().TargetOS.is(OSType::Linux))
+        throw std::runtime_error("Your fd_set is too weird.");
+    t.configureFile("include/X11/Xpoll.h.in", "X11/Xpoll.h");
+}
+
+void check(Checker &c)
+{
+    auto &s = c.addSet("x");
+
+    auto add_headers = [](auto &c)
+    {
+        for (auto &h : {
+            "sys/param.h",
+            "sys/select.h",
+            "sys/types.h",
+            "sys/time.h",
+            })
+            c.Parameters.Includes.push_back(h);
+    };
+
+    add_headers(s.checkStructMemberExists("fd_set", "fds_bits"));
+    add_headers(s.checkStructMemberExists("fd_set", "__fds_bits"));
+}
---
+++ d4/b6/0e/b8/002942f8468a67705f75ce924ea42eea83e41baf921eb7e59faa60efcb4021f30aeb5c9e3ba9d4010bb8fb902563b0f0c017b622953a5f97915a5eb6/sw.cpp
@@ -0,0 +1,106 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("intel", "2021.10.0");
+    p += Git("https://github.com/intel/tbb", "v{v}");
+
+    auto make_def_fn = [](auto &t, auto &&name)
+    {
+        String arch;
+        if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+            arch += "win";
+        else if (t.getBuildSettings().TargetOS.isApple())
+            arch += "mac";
+        else
+            arch += "lin";
+        if (t.getBuildSettings().TargetOS.is(ArchType::x86_64))
+            arch += "64";
+        else
+            arch += "32";
+        arch += "-";
+        arch += name;
+        arch += ".def";
+        return arch;
+    };
+
+    auto &tbb = p.addTarget<LibraryTarget>("tbb");
+    {
+        tbb +=
+            "include/.*"_rr,
+            "src/tbb/.*\\.cpp"_rr,
+            "src/tbb/.*\\.h"_rr;
+        tbb ^= "include/tbb/tbbmalloc_proxy.h";
+        tbb -= "src/tbb/.*\\.def"_rr;
+        tbb.patch("src/tbb/governor.cpp", "return pteb->StackBase;", "return (std::uintptr_t)pteb->StackBase;");
+
+        tbb += "org.sw.demo.open_mpi.hwloc"_dep;
+
+        tbb.Private += "__TBB_BUILD"_d;
+        tbb.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        tbb.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        if (tbb.getBuildSettings().TargetOS.Type != OSType::Windows)
+            tbb.Public += "USE_PTHREAD"_d;
+        else
+            tbb.Public += "USE_WINTHREAD"_d;
+
+        tbb += path("src") / "tbb" / "def" / make_def_fn(tbb, "tbb");
+    }
+
+    // tbbbind is not separate lib
+    //tbb -= "src/tbb/tbb_bind.cpp";
+
+    auto &tbbmalloc = tbb.addTarget<SharedLibraryTarget>("malloc");
+    {
+        tbbmalloc +=
+            "include/.*"_rr,
+            "src/tbbmalloc/.*\\.cpp"_rr,
+            "src/tbbmalloc/.*\\.def"_rr,
+            "src/tbbmalloc/.*\\.h"_rr;
+        tbbmalloc -=
+            "src/tbbmalloc/.*\\.def"_rr;
+
+        // disable autolink
+        tbbmalloc.patch("include/tbb/tbbmalloc_proxy.h", "#pragma comment", "//#pragma  comment");
+
+        tbbmalloc.Private += "__TBBMALLOC_BUILD"_d;
+        tbbmalloc.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        tbbmalloc.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        if (tbbmalloc.getBuildSettings().Native.LibrariesType == LibraryType::Static)
+            tbbmalloc += "__TBB_SOURCE_DIRECTLY_INCLUDED"_def;
+        if (tbbmalloc.getBuildSettings().TargetOS.Type != OSType::Windows)
+            tbbmalloc.Public += "USE_PTHREAD"_d;
+        else
+            tbbmalloc.Public += "USE_WINTHREAD"_d;
+
+        tbbmalloc += path("src") / "tbbmalloc" / "def" / make_def_fn(tbb, "tbbmalloc");
+    }
+
+    auto &tbbmalloc_proxy = tbbmalloc.addTarget<SharedLibraryTarget>("proxy");
+    {
+        tbbmalloc_proxy +=
+            "include/.*"_rr,
+            "src/tbbmalloc_proxy.*"_rr;
+        tbbmalloc_proxy -=
+            "src/tbbmalloc_proxy/.*\\.def"_rr;
+        tbbmalloc_proxy ^= "include/tbb/tbbmalloc_proxy.h";
+
+        tbbmalloc_proxy.Public += "__TBB_NO_IMPLICIT_LINKAGE"_d;
+        tbbmalloc_proxy.Public += "__TBBMALLOC_NO_IMPLICIT_LINKAGE"_d;
+        if (tbbmalloc_proxy.getBuildSettings().TargetOS.Type != OSType::Windows)
+            tbbmalloc_proxy.Public += "USE_PTHREAD"_d;
+        else
+            tbbmalloc_proxy.Public += "USE_WINTHREAD"_d;
+
+        tbbmalloc_proxy.Public += tbbmalloc;
+
+        if (tbbmalloc_proxy.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            if (tbbmalloc_proxy.getBuildSettings().TargetOS.is(ArchType::x86_64))
+                tbbmalloc_proxy.Interface.LinkOptions.push_back("-INCLUDE:__TBB_malloc_proxy");
+            else if (tbbmalloc_proxy.getBuildSettings().TargetOS.is(ArchType::x86))
+                tbbmalloc_proxy.Interface.LinkOptions.push_back("-INCLUDE:___TBB_malloc_proxy");
+        }
+
+        // only linux present
+        //tbbmalloc_proxy += path("src") / "tbbmalloc" / "def" / make_def_fn(tbb, "proxy");
+    }
+}
