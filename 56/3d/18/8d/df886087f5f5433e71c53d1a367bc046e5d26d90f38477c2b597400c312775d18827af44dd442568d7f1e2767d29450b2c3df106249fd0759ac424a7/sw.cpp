0862f453d22829c0d914bd9ec45a4bd30fe376e1
CppanBot <cppanbot@gmail.com>
2023-12-07 11:47:36 +0000

    Update

---
+++ 56/3d/18/8d/df886087f5f5433e71c53d1a367bc046e5d26d90f38477c2b597400c312775d18827af44dd442568d7f1e2767d29450b2c3df106249fd0759ac424a7/sw.cpp
@@ -0,0 +1,24 @@
+void build(Solution &s)
+{
+    auto &t = s.addStaticLibrary("opentelemetry", "1.13.0");
+    t += Git("https://github.com/open-telemetry/opentelemetry-cpp", "v{v}");
+
+    t += cpp11;
+    t += "api/include/.*"_rr;
+    t += "sdk/include/.*"_rr;
+    t += "sdk/src/.*"_rr;
+
+    if (t.getBuildSettings().TargetOS.is(OSType::Windows)) {
+        t -= "sdk/src/common/platform/fork_unix.cc";
+    } else {
+        t -= "sdk/src/common/platform/fork_windows.cc";
+    }
+
+    t.Public += "api/include"_idir;
+    t.Public += "sdk/include"_idir;
+    t += "sdk"_idir;
+
+    auto &e = t.addStaticLibrary("exporters.ostream");
+    e.setRootDirectory("exporters/ostream");
+    e.Public += t;
+}
