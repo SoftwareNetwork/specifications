49f3601541b6bbad6bc5b5d8b9a0f38721f99f1e
CppanBot <cppanbot@gmail.com>
2023-12-17 15:47:17 +0000

    Update

---
+++ 57/47/f1/e8/95ec55fa7aa98409702cfe7d65ec72679193764aa02b01184aaee0d4707e8b4f416ebd796bff99e8c017230e7d2602fe9717667cc44f3a50bc05d7cc/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("ned14.outcome", "2.2.8");
+    t += Git("https://github.com/ned14/outcome", "v{v}");
+
+    t.Public += "OUTCOME_DISABLE_ABI_PERMUTATION"_def;
+    t.Public += "org.sw.demo.ned14.quickcpplib-master"_dep;
+}
