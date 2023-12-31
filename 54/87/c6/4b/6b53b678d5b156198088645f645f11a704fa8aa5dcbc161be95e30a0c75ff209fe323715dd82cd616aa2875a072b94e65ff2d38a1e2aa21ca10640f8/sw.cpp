84d6dc6aa575fb404aa1256f25c26707f93a0972
CppanBot <cppanbot@gmail.com>
2023-12-31 23:56:43 +0000

    Update

---
+++ 54/87/c6/4b/6b53b678d5b156198088645f645f11a704fa8aa5dcbc161be95e30a0c75ff209fe323715dd82cd616aa2875a072b94e65ff2d38a1e2aa21ca10640f8/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("khronos.vulkan.headers", "1.3.274");
+    t += Git("https://github.com/KhronosGroup/Vulkan-Headers", "v{v}");
+
+    t += "include/.*"_rr;
+    t += "registry/.*"_rr;
+}
