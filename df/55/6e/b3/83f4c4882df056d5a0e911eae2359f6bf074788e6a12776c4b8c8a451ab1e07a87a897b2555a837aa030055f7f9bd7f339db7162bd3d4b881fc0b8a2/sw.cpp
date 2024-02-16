eebf391e242aeb99a39cc4dad3cae44fca845a53
CppanBot <cppanbot@gmail.com>
2024-02-16 20:57:22 +0000

    Update

---
+++ df/55/6e/b3/83f4c4882df056d5a0e911eae2359f6bf074788e6a12776c4b8c8a451ab1e07a87a897b2555a837aa030055f7f9bd7f339db7162bd3d4b881fc0b8a2/sw.cpp
@@ -0,0 +1,27 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("census.opencensus", "0.3.0");
+    p += Git("https://github.com/census-instrumentation/opencensus-cpp", "v{v}");
+
+    auto add_dir = [](auto &t, const path &dir)
+    {
+        t += FileRegex(path("opencensus") / dir, ".*\\.h", false);
+        t.AllowEmptyRegexes = true;
+        t += FileRegex(path("opencensus") / dir / "internal", ".*", false);
+        t -= FileRegex(path("opencensus") / dir / "internal", ".*_test\\.cc", false);
+        t -= FileRegex(path("opencensus") / dir / "internal", ".*_benchmark\\.cc", false);
+        t -= FileRegex(path("opencensus") / dir / "internal", ".*_fuzzer\\.cc", false);
+        t.AllowEmptyRegexes = false;
+    };
+
+    auto &cpp = p.addStaticLibrary("cpp");
+    cpp += cpp17;
+    add_dir(cpp, "common");
+    add_dir(cpp, "context");
+    add_dir(cpp, "stats");
+    add_dir(cpp, "tags");
+    add_dir(cpp, "trace");
+    add_dir(cpp, "trace/exporter");
+    add_dir(cpp, "trace/propagation");
+    cpp.Public += "org.sw.demo.google.abseil"_dep;
+}
