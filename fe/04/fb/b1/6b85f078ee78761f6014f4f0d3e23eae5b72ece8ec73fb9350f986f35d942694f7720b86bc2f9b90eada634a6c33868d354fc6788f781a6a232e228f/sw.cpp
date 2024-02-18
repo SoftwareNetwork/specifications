8bb6a9533c5ff7dbde165b67084587b26f9aa050
CppanBot <cppanbot@gmail.com>
2024-02-18 23:57:19 +0000

    Update

---
+++ fe/04/fb/b1/6b85f078ee78761f6014f4f0d3e23eae5b72ece8ec73fb9350f986f35d942694f7720b86bc2f9b90eada634a6c33868d354fc6788f781a6a232e228f/sw.cpp
@@ -0,0 +1,18 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("google.highway", "1.1.0");
+    t += Git("https://github.com/google/highway");
+
+    t += cpp17;
+    t -= "hwy/.*"_rr;
+    t += "hwy/aligned_allocator.cc";
+    //t += "hwy/nanobenchmark.cc";
+    t += "hwy/targets.cc";
+    t += "hwy/per_target.cc";
+
+    t += "HWY_DLLEXPORT"_api;
+
+    t.Public += sw::Shared, "HWY_SHARED_DEFINE"_def;
+    /*t += sw::Shared, "hwy_EXPORTS"_def;
+    t += sw::Shared, "hwy_contrib_EXPORTS"_def;*/
+}
