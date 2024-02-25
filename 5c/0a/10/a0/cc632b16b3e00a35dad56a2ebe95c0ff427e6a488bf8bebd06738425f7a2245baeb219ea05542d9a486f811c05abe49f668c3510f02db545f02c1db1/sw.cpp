34f78e1883bee00d366f219fbc1a41d7e4a3a73b
CppanBot <cppanbot@gmail.com>
2024-02-25 17:57:02 +0000

    Update

--- 5c/0a/10/a0/cc632b16b3e00a35dad56a2ebe95c0ff427e6a488bf8bebd06738425f7a2245baeb219ea05542d9a486f811c05abe49f668c3510f02db545f02c1db1/sw.cpp
+++ 5c/0a/10/a0/cc632b16b3e00a35dad56a2ebe95c0ff427e6a488bf8bebd06738425f7a2245baeb219ea05542d9a486f811c05abe49f668c3510f02db545f02c1db1/sw.cpp
@@ -1,9 +1,11 @@
 void build(Solution &s)
 {
-    auto &t = s.addStaticLibrary("alandefreitas.matplotplusplus", "1.2.1");
+    auto &t = s.addLibrary("alandefreitas.matplotplusplus", "1.2.1");
     t += Git("https://github.com/alandefreitas/matplotplusplus", "v{v}");
 
     t += cpp17;
+
+    t += "MATPLOT_EXPORTS"_api;
 
     t += "source/matplot/.*"_rr;
     t -= "source/matplot/backend/opengl.*"_rr;
@@ -15,4 +17,6 @@ void build(Solution &s)
 
     t += "org.sw.demo.olvb.nodesoup-master"_dep;
     t += "org.sw.demo.dtschump.CImg"_dep;
+
+    t.writeFileOnce("matplot/detail/exports.h");
 }
