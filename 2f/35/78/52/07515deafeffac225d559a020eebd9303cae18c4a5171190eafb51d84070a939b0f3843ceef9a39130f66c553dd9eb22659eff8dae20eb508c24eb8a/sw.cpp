e4eec6d401d0184db5acf490144f51c96119460e
CppanBot <cppanbot@gmail.com>
2024-02-27 01:57:12 +0000

    Update

--- 2f/35/78/52/07515deafeffac225d559a020eebd9303cae18c4a5171190eafb51d84070a939b0f3843ceef9a39130f66c553dd9eb22659eff8dae20eb508c24eb8a/sw.cpp
+++ 2f/35/78/52/07515deafeffac225d559a020eebd9303cae18c4a5171190eafb51d84070a939b0f3843ceef9a39130f66c553dd9eb22659eff8dae20eb508c24eb8a/sw.cpp
@@ -1,6 +1,7 @@
 void build(Solution &s)
 {
-   auto &t = s.add<PythonLibrary>("jinja2", "3.1.3");
+   auto &t = s.add<PythonLibrary>("python.pypi.jinja2", "3.1.3");
    t += Git("https://github.com/pallets/jinja");
    t += "src/.*"_rr;
+   t += "org.sw.demo.python.pypi.markupsafe"_dep;
 }
