4aff75d265ebb1c862e5b17e19f7918e2a1ab73f
CppanBot <cppanbot@gmail.com>
2024-03-06 21:32:00 +0000

    Update

---
+++ b4/c8/90/5d/a37f3e7bc6e1d0c6423b71eb2cccabbf3711a1fe522be88a576af52908960e0dfb5d375b96e5a90dd0add0e0c4dcfd3b5c7ab5b2c2849904b49a6156/sw.cpp
@@ -0,0 +1,14 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("Genivia.REflex", "4.1.0");
+    p += Git("https://github.com/Genivia/RE-flex", "v{v}");
+
+    auto &lib = p.addStaticLibrary("lib");
+    lib += "include/.*"_rr;
+    lib += "lib/.*"_rr;
+    lib += "unicode/.*"_rr;
+
+    auto &exe = p.addExecutable("exe");
+    exe += "src/.*"_rr;
+    exe += lib;
+}
--- c8/c5/25/72/2cc7309c63f60a8e87f9f0894732fb6163f18a19ae2e28e7f52bfbed77cb9482715b1f1f27da3e939f2b600913c7c6840450d4c6a233d507ea067644/sw.cpp
+++ c8/c5/25/72/2cc7309c63f60a8e87f9f0894732fb6163f18a19ae2e28e7f52bfbed77cb9482715b1f1f27da3e939f2b600913c7c6840450d4c6a233d507ea067644/sw.cpp
@@ -2,4 +2,5 @@ void build(Solution &s)
 {
     auto &t = s.addTarget<LibraryTarget>("htacg.tidy_html5", "5.8.0");
     t += Git("https://github.com/htacg/tidy-html5");
+    t += "DISABLE_DEBUG_LOG"_def;
 }
