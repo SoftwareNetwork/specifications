9740f21e9b0ab5c9a47d8d4889a3f8a8a95590f3
CppanBot <cppanbot@gmail.com>
2024-03-10 10:32:07 +0000

    Update

---
+++ 42/43/25/5a/59bb7eac0a41365b952135c95c5662feb5965d487c182af2f2948e8dada1a93fe785de6aed2b63b1992d371536c1bf2fedd733c7413f32c9b31b5a42/sw.cpp
@@ -0,0 +1,16 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("uNetworking.uWebSockets", "20.61.0");
+    t += Git("https://github.com/uNetworking/uWebSockets", "v{v}");
+
+    t += "src/.*"_rr;
+    String f;
+    for (auto &&[p,_] : t["src/.*"_rr]) {
+        f += "#include <" + normalize_path(p).string() + ">\n";
+    }
+    t.writeFileOnce("uWS/uWS.h", f);
+
+    //t += cpp20; // we are header only
+    t.Public += "org.sw.demo.uNetworking.uSockets"_dep;
+    t.Public += "org.sw.demo.madler.zlib"_dep;
+}
