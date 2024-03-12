f94a2b8c91372b6e6d8ab2c7eaacadbca8fe071a
CppanBot <cppanbot@gmail.com>
2024-03-12 10:32:04 +0000

    Update

--- 2a/05/2a/f7/d9b68629ce6de28b1386ce4549d8e5a568f9a7bc320021696da9ddeeeaadbbac3b0f1496a1ef8de44d1e96a1e950d02ada90a33669d7e1bbc3be74e0/sw.cpp
+++ 2a/05/2a/f7/d9b68629ce6de28b1386ce4549d8e5a568f9a7bc320021696da9ddeeeaadbbac3b0f1496a1ef8de44d1e96a1e950d02ada90a33669d7e1bbc3be74e0/sw.cpp
@@ -2,4 +2,6 @@ void build(Solution &s)
 {
     auto &t = s.addStaticLibrary("mohabouje.WinToast", "1.3.0");
     t += Git("https://github.com/mohabouje/WinToast");
+    t.Public += "com.Microsoft.Windows.SDK.winrt"_dep;
+    t += "uuid.lib"_slib;
 }
