af9a8e8b3a2161974ccab4f524d8f9e8cb875359
CppanBot <cppanbot@gmail.com>
2024-03-20 09:17:45 +0000

    Update

---
+++ de/fc/5d/0d/fe29257e37d7d2b3d310dae5e2ae21db6a29cf559d9c7b5287f3ab98acd0d3476f33610355428d3345d95d5f5a7474b612445fb56998302bfd1e9314/sw.cpp
@@ -0,0 +1,30 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.crt_cpp", "0.26.4");
+    t += Git("https://github.com/awslabs/aws-crt-cpp", "v{v}");
+
+    t -=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t += "source/.*"_rr;
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_CRT_CPP_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_CRT_CPP_USE_IMPORT_EXPORT"_d;
+
+    t.Public += "org.sw.demo.amazon.awslabs.c_auth"_dep;
+    t.Public += "org.sw.demo.amazon.awslabs.c_event_stream"_dep;
+    t.Public += "org.sw.demo.amazon.awslabs.c_mqtt"_dep;
+    t.Public += "org.sw.demo.amazon.awslabs.c_s3"_dep;
+    t.Public += "org.sw.demo.amazon.awslabs.c_sdkutils"_dep;
+
+    t.Variables["AWS_CRT_CPP_VERSION"] = t.Variables["PACKAGE_VERSION"];
+    t.Variables["PROJECT_VERSION_MAJOR"] = t.Variables["PACKAGE_VERSION_MAJOR"];
+    t.Variables["PROJECT_VERSION_MINOR"] = t.Variables["PACKAGE_VERSION_MINOR"];
+    t.Variables["PROJECT_VERSION_PATCH"] = t.Variables["PACKAGE_VERSION_PATCH"];
+    t.configureFile("include/aws/crt/Config.h.in", "aws/crt/Config.h");
+}
