9d982ab0aa64f2b49bc78735e7ff57ece008ec64
CppanBot <cppanbot@gmail.com>
2024-03-27 20:17:53 +0000

    Update

---
+++ 95/19/17/fd/f255db7b2b138981bdb419f4eb7559d5aa87683fa5de245ee2c347eb263e794102015f96e3d14e7958aae60efe13d612b91435a5eb7fc20644825c11/sw.cpp
@@ -0,0 +1,9 @@
+void build(Solution &s)
+{
+    auto &libtommath = s.addTarget<StaticLibraryTarget>("libtom.libtommath", "1.3.0");
+    libtommath += Git("https://github.com/libtom/libtommath", "v{v}");
+
+    libtommath +=
+        ".*\\.h"_rr,
+        "bn.*\\.c"_rr;
+}
