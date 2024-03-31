9cfe9467885a3744bd01dab7f193ac1b5a085603
CppanBot <cppanbot@gmail.com>
2024-03-31 16:17:40 +0000

    Update

---
+++ 91/13/eb/8c/861c2dc75c016a7af180f075d22c722137ea1b308060772ba018a6ce65f9a9ce2863c240b4d626e70140afa5b372250158ee8fb547e44aad26aad92a/sw.cpp
@@ -0,0 +1,13 @@
+void build(Solution &s)
+{
+    auto &re2 = s.addTarget<StaticLibraryTarget>("google.re2", "2024.4.1");
+    re2 += Git("https://github.com/google/re2", "{M}-{m:02}-{p:02}");
+
+    re2 += cpp17;
+    re2.ExportAllSymbols = true;
+    re2.ImportFromBazel = true;
+    re2.BazelTargetName = "re2";
+    re2.Public += "."_idir;
+    re2 += "NOMINMAX"_def;
+    re2 += "org.sw.demo.google.abseil"_dep;
+}
