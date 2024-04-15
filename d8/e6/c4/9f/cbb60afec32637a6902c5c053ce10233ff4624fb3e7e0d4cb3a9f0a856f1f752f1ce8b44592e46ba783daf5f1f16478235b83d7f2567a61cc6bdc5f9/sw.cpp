7b3fad38fe139252f124045c41db9c711024ca47
CppanBot <cppanbot@gmail.com>
2024-04-15 12:56:17 +0000

    Update

--- d8/e6/c4/9f/cbb60afec32637a6902c5c053ce10233ff4624fb3e7e0d4cb3a9f0a856f1f752f1ce8b44592e46ba783daf5f1f16478235b83d7f2567a61cc6bdc5f9/sw.cpp
+++ d8/e6/c4/9f/cbb60afec32637a6902c5c053ce10233ff4624fb3e7e0d4cb3a9f0a856f1f752f1ce8b44592e46ba783daf5f1f16478235b83d7f2567a61cc6bdc5f9/sw.cpp
@@ -3,7 +3,7 @@ void build(Solution &s)
     auto &t = s.addTarget<LibraryTarget>("brainboxdotcc.dpp", "10.0.29");
     t += Git("https://github.com/brainboxdotcc/DPP", "v{v}");
 
-    t += cpp17;
+    t += cpp20;
     t += "include/dpp/.*"_rr;
     t += "src/dpp/.*"_rr;
     t.Public += sw::Static, "DPP_STATIC"_def;
@@ -12,8 +12,9 @@ void build(Solution &s)
     //t += "include/dpp"_idir;
     t += "org.sw.demo.madler.zlib"_dep;
     t += "org.sw.demo.openssl.ssl"_dep;
-    //t += "org.sw.demo.nlohmann.json"_dep;
+    t.Public += "DPP_USE_EXTERNAL_JSON"_def;
+    t.Public += "org.sw.demo.nlohmann.json.natvis"_dep;
     if (t.getCompilerType() == CompilerType::MSVC)
         t.CompileOptions.push_back("/bigobj");
-    t.pushBackToFileOnce("include/dpp/nlohmann/json_fwd.hpp", "#include \"json.hpp\"");
+    //t.pushBackToFileOnce("include/dpp/nlohmann/json_fwd.hpp", "#include \"json.hpp\"");
 }
