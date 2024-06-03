3504b8d0488c02318f05b0397239127a43ce50a7
CppanBot <cppanbot@gmail.com>
2024-06-03 16:31:56 +0000

    Update

---
+++ fc/f5/5c/6c/9d070be6a6ef0fb38fa00089bed6c5ba41e93050e01202ce943e0253cc1a3c0f8921026badc050b60886e46877defcbf7d14b07afd4edc7b90a7d78e/sw.cpp
@@ -0,0 +1,29 @@
+void build(Solution &s)
+{
+    auto &vorbis = s.addProject("xiph.vorbis", "1.3.7");
+    vorbis += RemoteFile("https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-{v}.tar.xz");
+
+    auto &libvorbis = vorbis.addTarget<LibraryTarget>("libvorbis");
+    libvorbis +=
+        "include/.*\\.h"_rr,
+        "lib/.*\\.c"_rr,
+        "lib/.*\\.h"_rr,
+        "win32/.*\\.def"_rr;
+    libvorbis -= "win32/vorbisenc.def";
+    libvorbis -= "win32/vorbisfile.def";
+    libvorbis -=
+        "lib/barkmel.c",
+        "lib/psytune.c",
+        "lib/tone.c",
+        "lib/vorbisfile.c";
+    libvorbis.Public += "org.sw.demo.xiph.libogg-1"_dep;
+
+    auto &vorbisfile = vorbis.addTarget<LibraryTarget>("vorbisfile");
+    vorbisfile +=
+        "include/.*\\.h"_rr,
+        "lib/misc.h",
+        "lib/os.h",
+        "lib/vorbisfile.c",
+        "win32/vorbisfile.def";
+    vorbisfile.Public += libvorbis;
+}
