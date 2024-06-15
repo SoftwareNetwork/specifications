714cfee410a048702ed048d0c84060fff8da0777
CppanBot <cppanbot@gmail.com>
2024-06-15 16:10:54 +0000

    Update

--- 0d/17/73/f2/7c3379dd2d3d9840e729531f0a382078c7599ef6efb75e5661e837427a3ce6339e4a7d97f460da0d9c2752896d7f2253349a321af2166260c9b64251/sw.cpp
+++ 0d/17/73/f2/7c3379dd2d3d9840e729531f0a382078c7599ef6efb75e5661e837427a3ce6339e4a7d97f460da0d9c2752896d7f2253349a321af2166260c9b64251/sw.cpp
@@ -1,9 +1,20 @@
 void build(Solution &s)
 {
-    auto &t = s.addStaticLibrary("BLAKE3_team.BLAKE3", "1.5.1");
+    auto &t = s.addLibrary("BLAKE3_team.blake3", "1.5.1");
     t += Git("https://github.com/BLAKE3-team/BLAKE3");
 
     t.setRootDirectory("c");
-    t += "blake3.*"_r;
-    t -= "blake3_neon.c";
+    t += "BLAKE3_API"_api;
+    t -= ".*"_r;
+    t += "blake3.c";
+    t += "blake3_dispatch.c";
+    t += "blake3_portable.c";
+
+    t += "BLAKE3_NO_SSE2"_def;
+    t += "BLAKE3_NO_SSE41"_def;
+    t += "BLAKE3_NO_AVX2"_def;
+    t += "BLAKE3_NO_AVX512"_def;
+
+    //t -= "blake3_neon.c";
+    t += "BLAKE3_USE_NEON=0"_def;
 }
