d9b146ed8d5e2d50cc6b394a57208c179f318cae
CppanBot <cppanbot@gmail.com>
2025-02-10 23:29:56 +0000

    Update

--- 69/18/95/86/b894feeb7b671c9348cee1edddd6c17d25c8b16606f505a57eb09f7da5c732dcd92c7a7c1d40dd3f0ff3ec7854dad5c8a7f29b73a74ab75731709ef5/sw.cpp
+++ 69/18/95/86/b894feeb7b671c9348cee1edddd6c17d25c8b16606f505a57eb09f7da5c732dcd92c7a7c1d40dd3f0ff3ec7854dad5c8a7f29b73a74ab75731709ef5/sw.cpp
@@ -60,6 +60,7 @@ void build(Solution &s)
         t += "src/core/lib/.*"_rr;
         t += "src/core/ext/upb-gen/.*"_rr;
         t += "src/core/ext/upbdefs-gen/.*"_rr;
+        t += "src/core/call/.*"_rr;
         t += "src/core/client_channel/.*"_rr;
         t += "src/core/config/.*"_rr;
         t += "src/core/filter/.*"_rr;
