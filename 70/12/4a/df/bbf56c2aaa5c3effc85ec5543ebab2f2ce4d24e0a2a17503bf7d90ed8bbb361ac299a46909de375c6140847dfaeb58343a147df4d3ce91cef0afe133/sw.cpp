ab80a8ffb6ba97a0097eafbb847f7b22507ef07a
CppanBot <cppanbot@gmail.com>
2025-02-10 12:29:57 +0000

    Update

---
+++ 70/12/4a/df/bbf56c2aaa5c3effc85ec5543ebab2f2ce4d24e0a2a17503bf7d90ed8bbb361ac299a46909de375c6140847dfaeb58343a147df4d3ce91cef0afe133/sw.cpp
@@ -0,0 +1,21 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.c_auth", "0.8.4");
+    t += Git("https://github.com/awslabs/aws-c-auth", "v{v}");
+
+    t -=
+        "include/.*"_rr,
+        "source/.*"_rr;
+    t += "source/.*"_rr;
+
+    if (t.getCompilerType() == CompilerType::MSVC)
+        t.CompileOptions.push_back("-bigobj");
+    //else if (s.Settings.Native.CompilerType == CompilerType::GNU)
+    //t.CompileOptions.push_back("-Wa,-mbig-obj");
+
+    t.Private += sw::Shared, "AWS_AUTH_EXPORTS"_d;
+    t.Public += sw::Shared, "AWS_AUTH_USE_IMPORT_EXPORT"_d;
+
+    t.Public += "org.sw.demo.amazon.awslabs.c_http"_dep;
+    t.Public += "org.sw.demo.amazon.awslabs.c_sdkutils"_dep;
+}
