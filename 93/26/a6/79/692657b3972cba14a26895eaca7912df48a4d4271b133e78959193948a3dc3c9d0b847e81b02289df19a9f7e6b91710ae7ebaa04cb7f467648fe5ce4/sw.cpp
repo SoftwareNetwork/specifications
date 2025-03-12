680d177d431007b285b1120e5812b99681cc374c
CppanBot <cppanbot@gmail.com>
2025-03-12 17:42:00 +0000

    Update

---
+++ 93/26/a6/79/692657b3972cba14a26895eaca7912df48a4d4271b133e78959193948a3dc3c9d0b847e81b02289df19a9f7e6b91710ae7ebaa04cb7f467648fe5ce4/sw.cpp
@@ -0,0 +1,28 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<LibraryTarget>("amazon.awslabs.s2n", "1.5.14");
+    t += Git("https://github.com/awslabs/s2n", "v{v}");
+
+    t += "S2N_API"_api;
+
+    t += "api/.*"_rr;
+    t += "crypto/.*"_rr;
+    t += "error/.*"_rr;
+    t += "stuffer/.*"_rr;
+    t += "tls/.*"_rr;
+    t += "utils/.*"_rr;
+    //t += "pq-crypto/.*"_rr;
+
+    t.Public += "api"_idir;
+    t += "."_idir;
+
+    t.patch("api/s2n.h", "#    define S2N_API", "//#     define S2N_API");
+    //t.patch("utils/s2n_asn1_time.c", "__tm_gmtoff", "tm_gmtoff");
+
+    if (t.getBuildSettings().TargetOS.Type != OSType::Windows)
+    {
+        t += "pthread"_slib;
+    }
+
+    t.Public += "org.sw.demo.openssl.crypto"_dep;
+}
---
+++ b3/d7/79/8d/700cb143e71091949c31379617813e5e3ca964e47717f6ddf5930fc7377f8a151c59e0d9bbe2d78f36c4c8ee8ec4581f0820ffde4ef2e4242fe709bc/sw.cpp
@@ -0,0 +1,8 @@
+void build(Solution &s)
+{
+    auto &t = s.addTarget<StaticLibraryTarget>("baldurk.renderdoc", "1.37.0");
+    t += Git("https://github.com/baldurk/renderdoc", "v{M}.{m}");
+
+    t += "renderdoc/api/.*"_rr;
+    t.Public += "renderdoc/api/app"_idir;
+}
