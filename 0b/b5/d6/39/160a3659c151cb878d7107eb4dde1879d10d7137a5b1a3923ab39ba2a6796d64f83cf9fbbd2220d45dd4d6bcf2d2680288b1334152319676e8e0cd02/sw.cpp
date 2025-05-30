437e3ca984cbef94e24ebf02e31dc0b5b1abce0c
CppanBot <cppanbot@gmail.com>
2025-05-30 13:53:04 +0000

    Update

---
+++ 0b/b5/d6/39/160a3659c151cb878d7107eb4dde1879d10d7137a5b1a3923ab39ba2a6796d64f83cf9fbbd2220d45dd4d6bcf2d2680288b1334152319676e8e0cd02/sw.cpp
@@ -0,0 +1,22 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("howardhinnant.date", "3.0.4");
+    p += Git("https://github.com/HowardHinnant/date", "v{v}");
+
+    auto &date = p.addTarget<StaticLibraryTarget>("date");
+    date += "include/date/date.h";
+
+    auto &date_full = p.addLibrary("date_full");
+    date_full += cpp17;
+    //date_full += "org.sw.demo.badger.curl.libcurl"_dep;
+    date_full -= "src/ios.mm";
+    date_full.Public += "HAS_REMOTE_API=0"_def;
+    if (date_full.getBuildSettings().TargetOS.Type != OSType::Windows) {
+        date_full.Public += "USE_OS_TZDB=1"_def;
+    } else {
+        date_full += "Ole32.lib"_slib;
+        date_full += "Shell32.lib"_slib;
+    }
+    date_full += sw::Shared, "DATE_BUILD_DLL"_def;
+    date_full.Interface += sw::Shared, "DATE_USE_DLL"_def;
+}
---
+++ 53/67/13/bd/edca0eb062386faf203ebeb30177ec1e9b03034f01169d1172fd415388db37de0dfb8532db0018f5e5c2df5e3ca1d40f946032595699d986e6bdfe72/sw.cpp
@@ -0,0 +1,38 @@
+void build(Solution &s)
+{
+    auto &t = s.addLibrary("swami.libinstpatch", "1.1.7");
+    t += Git("https://github.com/swami/libinstpatch", "v{v}");
+
+    t += "libinstpatch/.*"_rr;
+    t -= "libinstpatch/gir_prog.c";
+    t -= "libinstpatch/IpatchState.*"_rr;
+
+    if (t.getBuildSettings().TargetOS.Type == OSType::Windows)
+        t += "WIN32"_def;
+
+    t.configureFile("config.h.cmake", t.BinaryPrivateDir / "config.h");
+    t.configureFile("libinstpatch/libinstpatch.h.in", t.BinaryPrivateDir / "libinstpatch.h");
+    t.configureFile("libinstpatch/libinstpatch.h.in", "libinstpatch/libinstpatch.h");
+
+    t.Variables["IPATCH_VERSION"] = "\"" + t.Variables["PACKAGE_VERSION"].toString() + "\"";
+    t.Variables["IPATCH_VERSION_MAJOR"] = t.Variables["PACKAGE_VERSION_MAJOR"];
+    t.Variables["IPATCH_VERSION_MINOR"] = t.Variables["PACKAGE_VERSION_MINOR"];
+    t.Variables["IPATCH_VERSION_MICRO"] = t.Variables["PACKAGE_VERSION_PATCH"];
+    t.configureFile("libinstpatch/version.h.in", t.BinaryPrivateDir / "version.h");
+    t.configureFile("libinstpatch/version.h.in", "libinstpatch/version.h");
+
+    t += "org.sw.demo.gnome.glib.gobject"_dep;
+    t.Public += "org.sw.demo.erikd.libsndfile"_dep;
+
+    for (auto ext : { "h"s, "c"s })
+    {
+        // glib.genmarshal
+        auto c = t.addCommand();
+        c << cmd::prog("org.sw.demo.python.exe-3"_dep)
+            << t.getFile("org.sw.demo.gnome.glib.gobject"_dep, "gobject/glib-genmarshal.in")
+            << "--output" << cmd::out("marshals." + ext)
+            << (ext == "h" ? "--header" : "--body")
+            << cmd::in("libinstpatch/marshals.list")
+            ;
+    }
+}
