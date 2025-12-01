92f170e556e3d41bdccc8d37db841bfc883247ba
CppanBot <cppanbot@gmail.com>
2025-12-01 16:13:00 +0000

    Update

---
+++ 1a/ff/aa/5c/9e3ca30795860d99a35e18bb0b68e2337b9004b28a2ffda7244aed07ab7198764e39c43fe8e27f2cb91d0da6cbd8a2292b6c88c8728a887fb435d73c/sw.cpp
@@ -0,0 +1,20 @@
+void build(Solution &s)
+{
+    auto &p = s.addProject("qtproject.qt.labs.vstools.natvis", "3.3.0");
+    p += Git("https://github.com/qt-labs/vstools", "v{v}");
+
+    auto add_natvis = [&](int v) {
+        path dir = "QtVsTools.Package";
+        auto s = std::to_string(v);
+        auto tgt = "qt" + s;
+        auto natvis = tgt + ".natvis";
+        auto xml = natvis + ".xml";
+        auto &t = p.addLibrary(tgt);
+        t += dir / xml;
+        t.configureFile(dir / xml, natvis);
+        t.patch(natvis, "##NAMESPACE##", "");
+        t.Public += path{natvis};
+    };
+    add_natvis(5);
+    add_natvis(6);
+}
