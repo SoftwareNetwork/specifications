60c90ba3eaa046f90ca4c9c26935433ab87e7958
CppanBot <cppanbot@gmail.com>
2026-03-05 23:19:51 +0000

    Update

--- 22/73/5a/19/53fab5b141529a6a0aefb10003a9c3f94c3f48677c71146eef46fec18688a61c7cce65872b1073e058fe76f2cf183e0ebc28a92f8ae427ea304a3171/sw.cpp
+++ 22/73/5a/19/53fab5b141529a6a0aefb10003a9c3f94c3f48677c71146eef46fec18688a61c7cce65872b1073e058fe76f2cf183e0ebc28a92f8ae427ea304a3171/sw.cpp
@@ -92,19 +92,28 @@ struct NasmCompiler : sw::NativeCompiler,
 void build(Solution &s)
 {
     auto &nasm = s.addTarget<ExecutableTarget>("nasm", "3.1");
-    nasm += RemoteFile("https://www.nasm.us/pub/nasm/releasebuilds/{M}.{m:02}/nasm-{M}.{m:02}.tar.gz");
+    //nasm += RemoteFile("https://www.nasm.us/pub/nasm/releasebuilds/{M}.{m:02}/nasm-{M}.{m:02}rc2.tar.gz"); // with generated files (no perl)
+    nasm += Git("https://github.com/netwide-assembler/nasm", "nasm-{M}.{m:02}"); // requires perl to generate files
+    //nasm += Git("https://github.com/netwide-assembler/nasm", "nasm-{M}.{m:02}rc2"); // requires perl to generate files
 
-    nasm.setChecks("nasm");
+    //nasm += c23; // after sw update is pushed
+    nasm.CVersion = CLanguageStandard::C23;
+
+    nasm.setChecks("nasm", true);
 
     nasm +=
         ".*\\.h"_rr,
         "asm/.*\\.c"_rr,
-        "output/.*\\.c"_rr,
         "nasmlib/.*\\.c"_rr,
         "common/.*\\.c"_rr,
-        "macros/.*\\.c"_rr,
-        "stdlib/.*\\.c"_rr,
-        "x86/.*\\.c"_rr
+        "stdlib/.*\\.c"_rr//,
+        //"x86/.*\\.c"_rr
+        ;
+    nasm -=
+        "macros/.*"_rr,
+        "output/.*"_rr,
+        "perllib/.*"_rr,
+        "x86/.*"_rr
         ;
     nasm +=
         "."_idir,
@@ -113,15 +122,185 @@ void build(Solution &s)
         "asm"_idir,
         "output"_idir
         ;
+    nasm += "output/.*\\.c"_rr;
 
-    nasm.Public +=
-        "include"_id;
+    auto zlib = (nasm + "org.sw.demo.madler.zlib"_dep);
+
+    auto add_perl_dep = [&](auto &&c, auto &&dep) {
+        auto d = nasm.addProgDependency(dep);
+        auto fn = nasm.getObjFile(d, "bin");
+        c << "-I" + fn.string();
+        std::dynamic_pointer_cast<::sw::driver::Command>(c.getCommand())->addProgramDependency(d);
+        return d;
+    };
+    auto perl_command = [&]() {
+        auto c = nasm.addCommand();
+        c << cmd::prog("org.sw.demo.perl.perl"_dep);
+        c << "-I" + (nasm.SourceDir / "perllib").string();
+        c << "-I" + nasm.SourceDir.string();
+        c << "-I" + nasm.BinaryDir.string();
+        add_perl_dep(c, "org.sw.demo.perl.packages.dist.PathTools.Cwd"_dep);
+        add_perl_dep(c, "org.sw.demo.perl.packages.ext.Fcntl"_dep);
+        add_perl_dep(c, "org.sw.demo.perl.packages.dist.IO"_dep);
+        add_perl_dep(c, "org.sw.demo.perl.packages.cpan.Util"_dep);
+        add_perl_dep(c, "org.sw.demo.perl.packages.ext.File.Glob"_dep);
+        add_perl_dep(c, "org.sw.demo.perl.packages.cpan.Compress.Raw.Zlib"_dep);
+        return c;
+    };
+
+    perl_command()
+        << cmd::in("x86/preinsns.pl")
+        << cmd::in("x86/insns.dat")
+        << cmd::out("x86/insns.xda")
+        ;
+    //
+    struct pair {const char *k,*f;};
+    for (auto &&[k,f] : {
+        pair{"-fc","x86/iflag.c"},
+        pair{"-fh","x86/iflaggen.h"},
+        pair{"-b","x86/insnsb.c"},
+        pair{"-a","x86/insnsa.c"},
+        pair{"-d","x86/insnsd.c"},
+        pair{"-i","x86/insnsi.h"},
+        pair{"-n","x86/insnsn.c"},
+        })
+    perl_command()
+        << cmd::in("x86/insns.pl")
+        << k
+        << cmd::in("x86/insns.xda")
+        << cmd::out(f)
+        ;
+
+    //
+    for (auto &&[k,f] : {
+        pair{"h","asm/pptok.h"},
+        pair{"c","asm/pptok.c"},
+        pair{"ph","asm/pptok.ph"},
+        })
+    perl_command()
+        << cmd::in("asm/pptok.pl")
+        << k
+        << cmd::in("asm/pptok.dat")
+        << cmd::out(f)
+        ;
+
+    for (auto &&[k,f] : {
+        pair{"h","asm/directiv.h"},
+        pair{"c","asm/directbl.c"},
+        })
+    perl_command()
+        << cmd::in("nasmlib/perfhash.pl")
+        << k
+        << cmd::in("asm/directiv.dat")
+        << cmd::out(f)
+        ;
+
+    for (auto &&[k,f] : {
+        pair{"c","x86/regs.c"},
+        pair{"fc","x86/regflags.c"},
+        pair{"dc","x86/regdis.c"},
+        pair{"dh","x86/regdis.h"},
+        pair{"vc","x86/regvals.c"},
+        pair{"h","x86/regs.h"},
+        })
+    perl_command()
+        << cmd::in("x86/regs.pl")
+        << k
+        << cmd::in("x86/regs.dat")
+        << cmd::std_out(f)
+        ;
+
+    for (auto &&[k,f] : {
+        pair{"h","asm/warnings.h"},
+        pair{"c","asm/warnings_c.h"},
+        })
+    perl_command()
+        << cmd::in("asm/warnings.pl")
+        << k
+        << cmd::out(f)
+        << cmd::in("asm/warnings.dat")
+        ;
+
+    for (auto &&[k,f] : {
+        pair{"h","version.h"},
+        pair{"mac","version.mac"},
+        })
+    perl_command()
+        << cmd::in("version.pl")
+        << k
+        << cmd::std_in("version")
+        << cmd::std_out(f)
+        ;
+
+    for (auto &&[k,f] : {
+        pair{"h","asm/tokens.h"},
+        pair{"c","asm/tokhash.c"},
+        })
+    perl_command()
+        << cmd::in("asm/tokhash.pl")
+        << k
+        << cmd::in("x86/insnsn.c")
+        << cmd::in("x86/regs.dat")
+        << cmd::in("asm/tokens.dat")
+        << cmd::std_out(f)
+        ;
+
+    auto m = perl_command()
+        << cmd::wdir(nasm.BinaryDir)
+        << cmd::in("macros/macros.pl")
+        << cmd::in("version.mac")
+        ;
+    for (auto &&f : {
+        "macros/altreg.mac",
+        "macros/fp.mac",
+        "macros/ifunc.mac",
+        "macros/masm.mac",
+        "macros/smartalign.mac",
+        "macros/standard.mac",
+        "macros/vtern.mac",
+    })
+        m << cmd::in(f);
+    for (auto &&f : {
+        "output/outaout.mac",
+        "output/outas86.mac",
+        "output/outbin.mac",
+        "output/outcoff.mac",
+        "output/outdbg.mac",
+        "output/outelf.mac",
+        "output/outmacho.mac",
+        "output/outobj.mac",
+    })
+        m << cmd::in(f);
+    m
+        << cmd::end()
+        << cmd::out("macros/macros.c")
+        ;
+
+    nasm += IncludeDirectory{nasm.BinaryDir / "asm"};
+    nasm += IncludeDirectory{nasm.BinaryDir / "x86"};
+
+    //if (mp.getCompilerType() == CompilerType::GNU) {
+    if (nasm.getBuildSettings().TargetOS.Type != OSType::Windows) {
+        nasm += "HAVE_STDNORETURN_H"_def;
+        //nasm += "HAVE_STDC_INLINE"_def;
+        nasm += "HAVE_STDC_LEADING_ZEROS"_def;
+        nasm += "HAVE___BUILTIN_CLZ"_def;
+        nasm += "HAVE__BUILTIN_CLZLL"_def;
+
+        nasm += "_GNU_SOURCE"_def;
+        // in endian.h
+        nasm += "HAVE_HTOLE16"_def;
+        nasm += "HAVE_HTOLE32"_def;
+        nasm += "HAVE_HTOLE64"_def;
+    }
+
+    nasm.Public += "include"_id;
 
     auto C = std::make_shared<NasmCompiler>();
     C->file = nasm.getOutputFile();
     nasm.setProgram(C);
 
-    nasm += "org.sw.demo.madler.zlib"_dep;
+    nasm.pushFrontToFileOnce("include/compiler.h", "#include <stdint.h>");
 }
 
 void check(Checker &c)
@@ -144,19 +323,31 @@ void check(Checker &c)
     s.checkFunctionExists("_fullpath");
     s.checkFunctionExists("_snprintf");
     s.checkFunctionExists("_vsnprintf");
+    s.checkFunctionExists("mempcpy");
+    s.checkFunctionExists("mempset");
+
+    //s.checkIncludeExists("stdnoreturn.h"); // causes build errors on msvc
+    //s.checkIncludeExists("windows.h");
+    s.checkIncludeExists("endian.h");
+    s.checkIncludeExists("io.h");
+    s.checkIncludeExists("fcntl.h");
     s.checkIncludeExists("inttypes.h");
     s.checkIncludeExists("limits.h");
     s.checkIncludeExists("memory.h");
+    s.checkIncludeExists("stdbit.h");
     s.checkIncludeExists("stdbool.h");
     s.checkIncludeExists("stddef.h");
     s.checkIncludeExists("stdint.h");
+    s.checkIncludeExists("stdio.h");
     s.checkIncludeExists("stdlib.h");
     s.checkIncludeExists("strings.h");
     s.checkIncludeExists("string.h");
     s.checkIncludeExists("sys/param.h");
     s.checkIncludeExists("sys/stat.h");
     s.checkIncludeExists("sys/types.h");
+    s.checkIncludeExists("sys/mman.h");
     s.checkIncludeExists("unistd.h");
+
     s.checkTypeSize("size_t");
     s.checkTypeSize("void *");
     {
@@ -170,10 +361,4 @@ void check(Checker &c)
     #include <float.h>
     int main() {return 0;}
     )sw_xxx");
-    s.checkDeclarationExists("decl");
-    s.checkDeclarationExists("decl");
-    s.checkDeclarationExists("decl");
-    s.checkDeclarationExists("decl");
-    s.checkDeclarationExists("decl");
-    s.checkDeclarationExists("decl");
 }
