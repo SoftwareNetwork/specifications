d98d3090c600af0bd642f28fa9e6098c8160b3ba
CppanBot <cppanbot@gmail.com>
2026-03-06 19:19:52 +0000

    Update

--- 33/70/0c/64/a17b4a36d4a7f8a10f0fef25ef8e13375307827dcf2267a048fde58ca89bbfc9d1cfe66b6e50d0b41a5bbc671efe29e49aa2ab060ba97fec90e6b124/sw.cpp
+++ 33/70/0c/64/a17b4a36d4a7f8a10f0fef25ef8e13375307827dcf2267a048fde58ca89bbfc9d1cfe66b6e50d0b41a5bbc671efe29e49aa2ab060ba97fec90e6b124/sw.cpp
@@ -413,6 +413,8 @@ void build(Solution &s)
                 if (t.getBuildSettings().TargetOS.isApple()) {
                     setup_nvtype();
 
+                    m1["d_querylocale"] = "define";
+                    m1["xlocale_needed"] = "define";
                     m1["d_setlocale_r"] = "define";
                     m1["d_has_C_UTF8"] = "false";
                     m1["d_isinfl"] = "undef";
