4c65bcc24760382896c8662af1bf9b65b9d5c827
CppanBot <cppanbot@gmail.com>
2026-03-16 00:01:36 +0000

    Update

---
+++ 43/14/fd/e4/2b92eab6c615f0c9243ff5676eb5a7f463fcc497e46cb7a54b2b44473d05c99fde0410714ad8af03c18afa75509f530b8fd2005b99f0d36c493fac54/sw.cpp
@@ -0,0 +1,18 @@
+void build(Solution &s)
+{
+    auto &zxing_cpp = s.addTarget<StaticLibraryTarget>("zxing_cpp.zxing_cpp", "3.0.2");
+    zxing_cpp += Git("https://github.com/zxing-cpp/zxing-cpp", "v{v}");
+
+    zxing_cpp += cpp17;
+
+    zxing_cpp += "core/.*"_rr;
+    zxing_cpp -= "core/src/libzint/.*"_rr;
+
+    zxing_cpp += "ZXING_READERS"_def;
+    zxing_cpp += "ZXING_WRITERS"_def;
+    // ZXING_USE_ZINT
+
+    zxing_cpp.Public += "core/src"_id;
+
+    zxing_cpp.configureFile("core/Version.h.in", "Version.h");
+}
