ccace3985ad26036abb778f822effa8a6b6d5d32
CppanBot <cppanbot@gmail.com>
2026-03-18 02:53:05 +0000

    Update

--- 0d/d5/cc/04/ebc8d454d267195ccbc945b2611170bb40e9a9c722c9441199f7c6620acf44647dcc1c5e182ca735dc4ac58d3e82937b1aa53cf809092b1546822ae0/sw.cpp
+++ 0d/d5/cc/04/ebc8d454d267195ccbc945b2611170bb40e9a9c722c9441199f7c6620acf44647dcc1c5e182ca735dc4ac58d3e82937b1aa53cf809092b1546822ae0/sw.cpp
@@ -8,20 +8,32 @@ void build(Solution &s)
     pqxx.ApiName = "PQXX_LIBEXPORT";
     pqxx += cpp20;
 
-    if (pqxx.getBuildSettings().TargetOS.Type == OSType::Windows)
+    if (pqxx.getBuildSettings().TargetOS.Type == OSType::Windows) {
         pqxx += "ws2_32.lib"_slib;
-    else
-        pqxx += "PQXX_HAVE_POLL"_def;
+    } else {
+        pqxx += "PQXX_HAVE_POLL=1"_v;
+        pqxx += "PQXX_HAVE_GCC_VISIBILITY=1"_v;
+    }
 
     pqxx += "org.sw.demo.postgres.pq"_dep;
+
+    pqxx += "HAVE_CXX20=1"_v;
+    pqxx += "PQXX_HAVE_SLEEP_FOR=1"_v;
+    pqxx += "PQXX_HAVE_CHARCONV_INT=1"_v;
+    pqxx += "PQXX_HAVE_CHARCONV_FLOAT=1"_v;
+
+    pqxx.Variables["PACKAGE"] = "\"" + pqxx.Variables["PACKAGE"].toString() + "\"";
+    pqxx.Variables["PACKAGE_BUGREPORT"] = "\"" + pqxx.Variables["PACKAGE_BUGREPORT"].toString() + "\"";
+    pqxx.Variables["PACKAGE_NAME"] = "\"" + pqxx.Variables["PACKAGE_NAME"].toString() + "\"";
+    pqxx.Variables["PACKAGE_STRING"] = "\"" + pqxx.Variables["PACKAGE_STRING"].toString() + "\"";
+    pqxx.Variables["PACKAGE_TARNAME"] = "\"" + pqxx.Variables["PACKAGE_TARNAME"].toString() + "\"";
+    pqxx.Variables["PACKAGE_URL"] = "\"" + pqxx.Variables["PACKAGE_URL"].toString() + "\"";
+    pqxx.Variables["PACKAGE_VERSION"] = "\"" + pqxx.Variables["PACKAGE_VERSION"].toString() + "\"";
 
     pqxx.writeFileOnce("pqxx/config-public-compiler.h");
     pqxx.writeFileOnce("pqxx/config-internal-libpq.h");
     pqxx.writeFileOnce("pqxx/config-internal-compiler.h");
-
-    pqxx += "PQXX_HAVE_SLEEP_FOR"_def;
-    pqxx += "PQXX_HAVE_CHARCONV_INT"_def;
-    pqxx += "PQXX_HAVE_CHARCONV_FLOAT"_def;
+    pqxx.configureFile("include/pqxx/config.h.in", "pqxx/config-compiler.h", ConfigureFlags::EnableUndefReplacements);
 }
 
 void check(Checker &c)
