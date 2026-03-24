a825a022b8c5eabdac0f7368ba23083a10bdf60d
CppanBot <cppanbot@gmail.com>
2026-03-24 14:53:06 +0000

    Update

--- 0f/3f/53/55/71bf5e9680a930ab93ba50fc4ad358943d504465ea571709586214784e48d6ddcddb0667d2330893667a59285f1d1a55cac46cdab1b7850a8b108f1d/sw.cpp
+++ 0f/3f/53/55/71bf5e9680a930ab93ba50fc4ad358943d504465ea571709586214784e48d6ddcddb0667d2330893667a59285f1d1a55cac46cdab1b7850a8b108f1d/sw.cpp
@@ -3,5 +3,6 @@ void build(Solution &s)
     auto &double_conversion = s.addTarget<StaticLibraryTarget>("google.double_conversion", "3.4.0");
     double_conversion += Git("https://github.com/google/double-conversion", "v{v}");
     double_conversion += "double-conversion/.*"_rr;
-    double_conversion.Public += "double-conversion"_idir;
+    double_conversion.Public += "double-conversion"_idir; // what project does need this?
+    double_conversion.Public += "."_idir; // qt is ok with this
 }
