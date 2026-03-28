7b8c29fd40f69e3432047f0ceaffdf5f01c3d374
CppanBot <cppanbot@gmail.com>
2026-03-28 12:01:36 +0000

    Update

--- 82/28/68/0e/0b11e53eda336a39d4b2b22e9a7b881151d7001c2880a0d8cbe32be748b6faf53c7d4e5d938608bf92be39d1ae51de3572700f16bb3c2f27df1976f9/sw.cpp
+++ 82/28/68/0e/0b11e53eda336a39d4b2b22e9a7b881151d7001c2880a0d8cbe32be748b6faf53c7d4e5d938608bf92be39d1ae51de3572700f16bb3c2f27df1976f9/sw.cpp
@@ -1,30 +1,64 @@
 #pragma sw header on
 
+static int cuda_preprocess_file(path i, path o)
+{
+    auto s = read_file(i);
+    //boost::replace_all(s, "#pragma detect_mismatch(\"_ITERATOR_DEBUG_LEVEL", "// detect_mismatch(\"_ITERATOR_DEBUG_LEVEL");
+    //boost::replace_all(s, "#pragma detect_mismatch(\"RuntimeLibrary", "// detect_mismatch(\"RuntimeLibrary");
+    //boost::replace_all(s, "#pragma comment(lib", "// comment(lib");
+    write_file(o, s);
+
+    return 0;
+}
+SW_DEFINE_VISIBLE_FUNCTION_JUMPPAD2(cuda_preprocess_file)
+
 void setup_cuda_for_target(auto &t) {
     std::shared_ptr<sw::Program> cc1(t.findProgramByExtension(".cpp")->clone());
     auto cc = std::static_pointer_cast<sw::NativeCompiler>(cc1);
-    auto ext = ".ii.cpp"s;
+    auto ext1 = ".ii.cpp"s;
+    auto ext2 = ".cpp"s;
     for (auto &&[p,f] : t[".*\\.cu"_rr]) {
         if (f->skip) {
             continue;
         }
         auto n = p.filename().string();
+        // this is a preprocessor step (--cuda), so we must provide everything ourselves (like -D_DEBUG etc.)
+        // for flags see https://learn.microsoft.com/en-us/cpp/build/reference/md-mt-ld-use-run-time-library
         auto c = t.addCommand();
         c
             << cmd::prog("nvcc")
             << cmd::env("Path", cc->file.parent_path().string())
             //<< "-MD"
-            << "-cuda"
+            << "--cuda"
             //<< "-c"
             << cmd::in(p)
             << "-o"
-            << cmd::out(n + ext)
+            << cmd::out(n + ext1)
             ;
+        if (t.getBuildSettings().TargetOS.Type == OSType::Windows) {
+            // != GNU?
+            // if (getCompilerType() == CompilerType::MSVC || getCompilerType() == CompilerType::ClangCl)
+            //c << "-D_CRT_NOPRAGMA_LIBS"; // (prevent pragma comment(lib, ...) - this one is getting undeffed)
+            c << "-D_USE_ANSI_CPP"; // (prevent pragma comment(lib, ...) - prevent whole header)
+            c << "-D_MT"; // (non /LD mode)
+            if (!t.getBuildSettings().Native.MT) {
+                c << "-D_DLL";
+            }
+            if (t.getBuildSettings().Native.ConfigurationType == ConfigurationType::Debug) {
+                c << "-D_DEBUG";
+            }
+        }
         t.add(sw::CallbackType::EndPrepare, [c, &t]() mutable
         {
             t.getMergeObject().addDefinitionsAndIncludeDirectories(*c.getCommand());
         });
-        t += path{n + ext};
+
+        //t.addCommand(SW_VISIBLE_FUNCTION(cuda_preprocess_file))
+        //    << cmd::in(n + ext1)
+        //    << cmd::out(n + ext1 + ext2)
+        //    ;
+        // sed -s '/#line.*/d;/^[[:space:]]*$/d' cuda_dlib.cu.ii.cpp > 1.cpp
+        //t += path{n + ext1 + ext2};
     }
 }
 
@@ -49,7 +83,11 @@ void build(Solution &s)
         if (t.getBuildSettings().TargetOS.Type == OSType::Windows) {
             t.Public += IncludeDirectory{cuda_root_win / "cuda" / ver / "include"s};
             t += LinkDirectory{cuda_root_win / "cuda" / ver / "lib/x64"s};
-            t += "cudart_static.lib"_slib;
+            if (!t.getBuildSettings().Native.MT) {
+                t += "cudart.lib"_slib;
+            } else {
+                t += "cudart_static.lib"_slib;
+            }
             t += "cusolver.lib"_slib;
             t += "curand.lib"_slib;
             t += "cublas.lib"_slib;
