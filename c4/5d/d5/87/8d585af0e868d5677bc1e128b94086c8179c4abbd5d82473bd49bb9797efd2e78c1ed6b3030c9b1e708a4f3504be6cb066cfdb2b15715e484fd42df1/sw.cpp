56575ec69d81dfc5442447ebcec07cb3dfeb3197
CppanBot <cppanbot@gmail.com>
2026-03-29 11:01:36 +0000

    Update

--- c4/5d/d5/87/8d585af0e868d5677bc1e128b94086c8179c4abbd5d82473bd49bb9797efd2e78c1ed6b3030c9b1e708a4f3504be6cb066cfdb2b15715e484fd42df1/sw.cpp
+++ c4/5d/d5/87/8d585af0e868d5677bc1e128b94086c8179c4abbd5d82473bd49bb9797efd2e78c1ed6b3030c9b1e708a4f3504be6cb066cfdb2b15715e484fd42df1/sw.cpp
@@ -3,7 +3,7 @@ void build(Solution &s)
     auto &jsoncpp = s.addTarget<LibraryTarget>("open_source_parsers.jsoncpp", "1.9.7");
     jsoncpp += Git("https://github.com/open-source-parsers/jsoncpp", "{v}");
 
-    jsoncpp += cpp11;
+    jsoncpp += cpp17;
     jsoncpp +=
         "include/.*\\.h"_rr,
         "src/lib_json/.*\\.cpp"_rr,
@@ -12,4 +12,6 @@ void build(Solution &s)
 
     jsoncpp.Interface += sw::Shared, "JSON_DLL"_d;
     jsoncpp += sw::Shared, "JSON_DLL_BUILD"_d;
+
+    jsoncpp.Public += "JSONCPP_HAS_STRING_VIEW"_def;
 }
