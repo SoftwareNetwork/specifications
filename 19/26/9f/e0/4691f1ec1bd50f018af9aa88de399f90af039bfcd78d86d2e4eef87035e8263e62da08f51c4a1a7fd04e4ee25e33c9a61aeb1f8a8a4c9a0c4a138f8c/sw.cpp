129b2b6ac4307d1e2312e1ed5e8ed0c6acb3a1dd
CppanBot <cppanbot@gmail.com>
2026-05-13 14:37:06 +0000

    Update

--- 19/26/9f/e0/4691f1ec1bd50f018af9aa88de399f90af039bfcd78d86d2e4eef87035e8263e62da08f51c4a1a7fd04e4ee25e33c9a61aeb1f8a8a4c9a0c4a138f8c/sw.cpp
+++ 19/26/9f/e0/4691f1ec1bd50f018af9aa88de399f90af039bfcd78d86d2e4eef87035e8263e62da08f51c4a1a7fd04e4ee25e33c9a61aeb1f8a8a4c9a0c4a138f8c/sw.cpp
@@ -1,80 +1,93 @@
 void build(Solution &s)
 {
     auto &p = s.addProject("google.breakpad", "master");
-    p += Git("https://github.com/google/breakpad", "", "master");
+    p += Git("https://github.com/google/breakpad", "", "main");
+
+    auto setup = [](auto &t) {
+        t += cpp20;
+        t.setChecks("breakpad");
+    };
 
     auto &windows = p.addTarget<StaticLibraryTarget>("common.windows");
-    windows.setChecks("breakpad");
-
-    windows +=
-        "src/client/windows/common/.*\\.h"_rr,
-        "src/common/[^/]*\\.h"_rr,
-        "src/common/windows/.*\\.h"_rr,
-        "src/common/windows/guid_string.*"_rr,
-        "src/google_breakpad/.*"_rr;
-
-    windows -=
-        ".*_unittest.cc"_rr;
-
-    if (windows.getBuildSettings().TargetOS.Type == OSType::Windows)
     {
-        windows.Public += "UNICODE"_d;
+        setup(windows);
+
+        windows +=
+            "src/client/windows/common/.*\\.h"_rr,
+            "src/common/[^/]*\\.h"_rr,
+            "src/common/windows/.*\\.h"_rr,
+            "src/common/windows/guid_string.*"_rr,
+            "src/google_breakpad/.*"_rr;
+
+        windows -=
+            ".*_unittest.cc"_rr;
+
+        if (windows.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            windows.Public += "UNICODE"_d;
+        }
     }
 
     //
     auto &client = p.addTarget<StaticLibraryTarget>("client.windows.crash_generation.client");
-    client.setChecks("breakpad");
-
-    client +=
-        "src/client/windows/crash_generation/crash_generation_client.*"_rr;
-
-    if (client.getBuildSettings().TargetOS.Type == OSType::Windows)
     {
-        client.Public += "UNICODE"_d;
-    }
+        setup(client);
 
-    client.Public += windows;
+        client +=
+            "src/client/windows/crash_generation/crash_generation_client.*"_rr;
+
+        if (client.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            client.Public += "UNICODE"_d;
+        }
+
+        client.Public += windows;
+    }
 
     //
     auto &server = p.addTarget<StaticLibraryTarget>("client.windows.crash_generation.server");
-    server.setChecks("breakpad");
-
-    server +=
-        "src/client/windows/crash_generation/client_info.*"_rr,
-        "src/client/windows/crash_generation/crash_generation_server.*"_rr,
-        "src/client/windows/crash_generation/minidump_generator.*"_rr;
-
-    server.Public +=
-        "src"_id;
-    server.Public +=
-        "src/client/windows/crash_generation"_id;
-
-    if (server.getBuildSettings().TargetOS.Type == OSType::Windows)
     {
-        server.Public += "UNICODE"_d;
-    }
+        setup(server);
 
-    server.Public += client;
+        server +=
+            "src/client/windows/crash_generation/client_info.*"_rr,
+            "src/client/windows/crash_generation/crash_generation_server.*"_rr,
+            "src/client/windows/crash_generation/minidump_generator.*"_rr;
+
+        server.Public +=
+            "src"_id;
+        server.Public +=
+            "src/client/windows/crash_generation"_id;
+
+        if (server.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            server.Public += "UNICODE"_d;
+        }
+
+        server.Public += client;
+    }
 
     //
     auto &handler = p.addTarget<StaticLibraryTarget>("client.windows.handler");
-    handler.setChecks("breakpad");
-
-    handler +=
-        "src/client/windows/handler/exception_handler.cc",
-        "src/client/windows/handler/exception_handler.h";
-
-    handler.Public +=
-        "src"_id;
-    handler.Public +=
-        "src/client/windows/handler"_id;
-
-    if (handler.getBuildSettings().TargetOS.Type == OSType::Windows)
     {
-        handler.Public += "UNICODE"_d;
-    }
+        setup(handler);
 
-    handler.Public += client;
+        handler +=
+            "src/client/windows/handler/exception_handler.cc",
+            "src/client/windows/handler/exception_handler.h";
+
+        handler.Public +=
+            "src"_id;
+        handler.Public +=
+            "src/client/windows/handler"_id;
+
+        if (handler.getBuildSettings().TargetOS.Type == OSType::Windows)
+        {
+            handler.Public += "UNICODE"_d;
+        }
+
+        handler.Public += client;
+    }
 }
 
 void check(Checker &c)
