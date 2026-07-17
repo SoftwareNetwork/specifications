5fcb75a87448d77fc62973fb93c9f8756f369cad
CppanBot <cppanbot@gmail.com>
2026-07-17 02:22:28 +0000

    Update

--- 5f/47/fb/09/72cef504afd7d649dac98d1156d2d8178400752dc03db44e4974ab5ced3e2072916a6ffccfd8d6781bba30a5d8b04e7183776cf7e2717c2ca3976beb/sw.cpp
+++ 5f/47/fb/09/72cef504afd7d649dac98d1156d2d8178400752dc03db44e4974ab5ced3e2072916a6ffccfd8d6781bba30a5d8b04e7183776cf7e2717c2ca3976beb/sw.cpp
@@ -1,10 +1,12 @@
 void build(Solution &s)
 {
     auto &t = s.addTarget<LibraryTarget>("ThePhD.sol2", "3.5.0");
-    t += Git("https://github.com/ThePhD/sol2", "v{v}");
+    //t += Git("https://github.com/ThePhD/sol2", "v{v}");
+    t += Git("https://github.com/egorpugin/sol2", "develop");
 
     t += "include/.*"_rr;
     t.Public += "sol2.natvis";
 
-    t.Public += "org.sw.demo.lua-5.4"_dep;
+    // original sol2 does not work with lua 5.5 yet
+    t.Public += "org.sw.demo.lua"_dep;
 }
